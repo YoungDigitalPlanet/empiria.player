1dedf2538e2a0167adb153625b576a5af80ff17e
rrybacki <rrybacki@ydp.com.pl>
2011-12-09 17:31:42 +0000

    external sound processor extension added
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@90901 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/controller/extensions/jswrappers/JsSoundProcessorExtension.java
@@ -0,0 +1,110 @@
+package eu.ydp.empiria.player.client.controller.extensions.jswrappers;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import eu.ydp.empiria.player.client.controller.events.delivery.DeliveryEvent;
+import eu.ydp.empiria.player.client.controller.events.delivery.DeliveryEventType;
+import eu.ydp.empiria.player.client.controller.events.interaction.MediaInteractionSoundEventCallback;
+import eu.ydp.empiria.player.client.controller.events.interaction.MediaInteractionSoundEventCallforward;
+import eu.ydp.empiria.player.client.controller.extensions.ExtensionType;
+import eu.ydp.empiria.player.client.controller.extensions.types.DeliveryEventsListenerExtension;
+
+public class JsSoundProcessorExtension extends JsExtension implements
+		DeliveryEventsListenerExtension {
+
+	protected boolean playing;
+	protected JavaScriptObject socketJs;
+	protected MediaInteractionSoundEventCallback currCallback;
+	
+	@Override
+	public void onDeliveryEvent(DeliveryEvent deliveryEvent) {
+		if (deliveryEvent.getType() == DeliveryEventType.PAGE_UNLOADING){
+			if (playing)
+				soundStop();
+		}
+		if (deliveryEvent.getType() == DeliveryEventType.FEEDBACK_SOUND  ||  deliveryEvent.getType() == DeliveryEventType.MEDIA_SOUND_PLAY){
+
+			if (deliveryEvent.getParams().containsKey("url")  &&  deliveryEvent.getParams().get("url") instanceof String){
+
+				MediaInteractionSoundEventCallback callback = null;
+				if (deliveryEvent.getParams().containsKey("callback")  &&  deliveryEvent.getParams().get("callback") instanceof MediaInteractionSoundEventCallback){
+					callback = ((MediaInteractionSoundEventCallback)deliveryEvent.getParams().get("callback"));
+				}
+				
+				if (playing)
+					soundStop();
+				
+				soundPlayJs(extensionJsObject, deliveryEvent.toJsObject());
+				
+				if (callback != null){
+					
+					currCallback = callback;	
+				
+					playing = true;
+					callback.onPlay();
+					callback.setCallforward(new MediaInteractionSoundEventCallforward() {
+						
+						@Override
+						public void stop() {
+							soundStop();
+						}
+					});
+				}
+				
+				
+			}
+			
+		}
+
+	}
+	
+	protected void soundStop(){
+		playing = false;
+		soundStopJs(extensionJsObject);
+		if (currCallback != null)
+			currCallback.onStop();
+	}
+
+	@Override
+	public ExtensionType getType() {
+		return ExtensionType.EXTENSION_PROCESSOR_SOUND;
+	}
+
+	@Override
+	public void init() {
+		socketJs = createSoundProcessorSocketJs();
+		setSoundProcessorSocketJs(extensionJsObject, socketJs);
+	}
+	
+	private void onSoundFinished(){
+		soundStop();
+	}
+	
+	private native void soundPlayJs(JavaScriptObject extenstionObject, JavaScriptObject eventJsObject)/*-{
+		if (typeof extenstionObject.soundPlay == 'function'){
+			extenstionObject.soundPlay(eventJsObject);
+		}
+	}-*/;
+	
+	private native void soundStopJs(JavaScriptObject extenstionObject)/*-{
+		if (typeof extenstionObject.soundStop == 'function'){
+			extenstionObject.soundStop();
+		}
+	}-*/;
+	
+	private native JavaScriptObject createSoundProcessorSocketJs()/*-{
+		var instance = this;
+		var socket = [];
+		socket.onSoundFinished = function(){
+			return instance.@eu.ydp.empiria.player.client.controller.extensions.jswrappers.JsSoundProcessorExtension::onSoundFinished()();
+		}
+		return socket;
+	}-*/;
+	
+
+	private native void setSoundProcessorSocketJs(JavaScriptObject extension, JavaScriptObject socket)/*-{
+		if (typeof extension.setSoundProcessorSocket == 'function'){
+			extension.setSoundProcessorSocket(socket);
+		}
+	}-*/;
+
+}
