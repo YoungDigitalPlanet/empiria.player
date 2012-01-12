333463425b698836aaf6c4937f79c59c5f0b9683
rrybacki <rrybacki@ydp.com.pl>
2012-01-12 06:22:57 +0000

    modules as extensions task#55827
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@93074 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/controller/feedback/AssessmentFeedbackSocket.java
@@ -0,0 +1,7 @@
+package eu.ydp.empiria.player.client.controller.feedback;
+
+import com.google.gwt.user.client.ui.Widget;
+
+public interface AssessmentFeedbackSocket {
+	public Widget getFeedbackView(int percentageScore);
+}
---
+++ src/eu/ydp/empiria/player/client/controller/feedback/InlineFeedbackSocket.java
@@ -0,0 +1,6 @@
+package eu.ydp.empiria.player.client.controller.feedback;
+
+public interface InlineFeedbackSocket {
+
+	public void addInlineFeedback(InlineFeedback inlineFeedback);
+}
