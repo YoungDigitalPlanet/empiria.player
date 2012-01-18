f63d1097c6f071e9b0b986fbe01000b258f6d648
rrybacki <rrybacki@ydp.com.pl>
2012-01-18 11:51:37 +0000

    modify interfaces
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@93686 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ empiria.player/src/eu/ydp/empiria/player/client/module/ILockable.java
@@ -0,0 +1,7 @@
+package eu.ydp.empiria.player.client.module;
+
+public interface ILockable {
+
+	/** lock activity */
+	public void lock(boolean l);
+}
--- empiria.player/src/eu/ydp/empiria/player/client/module/ModuleJsSocketFactory.java
+++ empiria.player/src/eu/ydp/empiria/player/client/module/ModuleJsSocketFactory.java
@@ -11,18 +11,26 @@ public class ModuleJsSocketFactory {
 
 		jso = addObjectFunctions(jso, module.getClass().getName());
 		
+		if (module instanceof ILockable){
+			jso = addLockableFunctions(jso, module);
+		}
+		
+		if (module instanceof IResetable){
+			jso = addResetableFunctions(jso, module);
+		}
+		
 		if (module instanceof IActivity){
 			jso = addActivityFunctions(jso, module);
 		}
 		
-		if (module instanceof IInteractionModule){
-			jso = addInteractionFunctions(jso, module);
+		if (module instanceof IUniqueModule){
+			jso = addUniqueFunctions(jso, module);
 		}
 		
 		return jso;
 	}
 	
-	public static native JavaScriptObject addObjectFunctions(JavaScriptObject jso, String className)/*-{
+	private static native JavaScriptObject addObjectFunctions(JavaScriptObject jso, String className)/*-{
 		var instanceClassName = className;		
 		jso.getClassName = function(){
 			return instanceClassName;
@@ -30,7 +38,26 @@ public class ModuleJsSocketFactory {
 		return jso; 
 	}-*/;
 	
-	public static native JavaScriptObject addActivityFunctions(JavaScriptObject jso, Object moduleInstance)/*-{
+	private static native JavaScriptObject addLockableFunctions(JavaScriptObject jso, Object moduleInstance)/*-{
+		var instance = moduleInstance;
+		jso.lock = function(){
+			instance.@eu.ydp.empiria.player.client.module.ILockable::lock(Z)(true);
+		}
+		jso.unlock = function(){
+			instance.@eu.ydp.empiria.player.client.module.ILockable::lock(Z)(false);
+		}
+	}-*/;
+	
+	
+	private static native JavaScriptObject addResetableFunctions(JavaScriptObject jso, Object moduleInstance)/*-{
+		var instance = moduleInstance;
+		jso.reset = function(){
+			instance.@eu.ydp.empiria.player.client.module.IResetable::reset()();
+		}
+	}-*/;
+	
+	
+	private static native JavaScriptObject addActivityFunctions(JavaScriptObject jso, Object moduleInstance)/*-{
 		var instance = moduleInstance;
 		jso.showAnswers = function(value){
 			instance.@eu.ydp.empiria.player.client.module.IActivity::showCorrectAnswers(Z)(true);
@@ -45,19 +72,10 @@ public class ModuleJsSocketFactory {
 			instance.@eu.ydp.empiria.player.client.module.IActivity::showCorrectAnswers(Z)(false);
 			instance.@eu.ydp.empiria.player.client.module.IActivity::lock(Z)(false);
 		}
-		jso.lock = function(){
-			instance.@eu.ydp.empiria.player.client.module.IActivity::lock(Z)(true);
-		}
-		jso.unlock = function(){
-			instance.@eu.ydp.empiria.player.client.module.IActivity::lock(Z)(false);
-		}
-		jso.reset = function(){
-			instance.@eu.ydp.empiria.player.client.module.IActivity::reset()();
-		}
 		return jso;
 	}-*/;
 
-	public static native JavaScriptObject addInteractionFunctions(JavaScriptObject jso, Object moduleInstance)/*-{
+	private static native JavaScriptObject addUniqueFunctions(JavaScriptObject jso, Object moduleInstance)/*-{
 		var instance = moduleInstance;
 		jso.getIdentifier = function(){
 			return instance.@eu.ydp.empiria.player.client.module.IInteractionModule::getIdentifier();
