e76ee337da0e6aed78651fd1e7fdc97e91a22f08
rrybacki <rrybacki@ydp.com.pl>
2012-01-18 11:51:37 +0000

    modify interfaces
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@93686 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/module/IActivity.java
+++ src/eu/ydp/empiria/player/client/module/IActivity.java
@@ -3,17 +3,11 @@ package eu.ydp.empiria.player.client.module;
 /**
  * Interface for widgets implementing interactive module functions
  */
-public interface IActivity {
-
-	/** Reset module (as if it was created once again) */
-	public void reset();
+public interface IActivity extends ILockable, IResetable {
 
 	/** Mark wrong and mark correct answers */
 	public void markAnswers(boolean mark);
 	
 	/** Show correct answers */
 	public void showCorrectAnswers(boolean show);
-	
-	/** lock activity */
-	public void lock(boolean l);
 }
