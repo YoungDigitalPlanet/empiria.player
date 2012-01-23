049bbf1912e20b81574b0b4529f4fb00dc94e54b
rrybacki <rrybacki@ydp.com.pl>
2012-01-23 10:02:25 +0000

    remove old navigation
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@94047 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/controller/flow/navigation/NavigationViewSocket.java
+++
@@ -1,9 +0,0 @@
-package eu.ydp.empiria.player.client.controller.flow.navigation;
-
-import com.google.gwt.user.client.ui.Widget;
-
-public interface NavigationViewSocket {
-
-	public Widget getMenuView();
-	public Widget getComboView();
-}
