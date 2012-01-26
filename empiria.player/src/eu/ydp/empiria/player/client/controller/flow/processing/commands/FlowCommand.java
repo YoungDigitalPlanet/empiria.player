8a938228aae063728abc276b6a2a336ebad9ac96
rrybacki <rrybacki@ydp.com.pl>
2012-01-26 07:22:54 +0000

    refactor flow requesting task#55706
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@94426 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- empiria.player/src/eu/ydp/empiria/player/client/controller/flow/processing/commands/FlowCommand.java
+++ empiria.player/src/eu/ydp/empiria/player/client/controller/flow/processing/commands/FlowCommand.java
@@ -22,37 +22,35 @@ public abstract class FlowCommand implements IFlowCommand {
 		
 		IFlowCommand command = null;
 
-		if ("NAVIGATE_NEXT_ITEM".equals(objName)) {
+		if (NavigateNextItem.NAME.equals(objName)) {
 			command = new NavigateNextItem();
-		} else if ("NAVIGATE_PREVIOUS_ITEM".equals(objName)) {
+		} else if (NavigatePreviousItem.NAME.equals(objName)) {
 			command = new NavigatePreviousItem();
-		} else if ("NAVIGATE_FIRST_ITEM".equals(objName)) {
+		} else if (NavigateFirstItem.NAME.equals(objName)) {
 			command = new NavigateFirstItem();
-		} else if ("NAVIGATE_LAST_ITEM".equals(objName)) {
+		} else if (NavigateLastItem.NAME.equals(objName)) {
 			command = new NavigateLastItem();
-		} else if ("NAVIGATE_TOC".equals(objName)) {
+		} else if (NavigateToc.NAME.equals(objName)) {
 			command = new NavigateToc();
-		} else if ("NAVIGATE_TEST".equals(objName)) {
+		} else if (NavigateTest.NAME.equals(objName)) {
 			command = new NavigateTest();
-		} else if ("NAVIGATE_SUMMARY".equals(objName)) {
+		} else if (NavigateSummary.NAME.equals(objName)) {
 			command = new NavigateSummary();
-		} else if ("NAVIGATE_GOTO_ITEM".equals(objName)) {
+		} else if (NavigateGotoItem.NAME.equals(objName)) {
 			command = new NavigateGotoItem(objIndex);
-		} else if ("NAVIGATE_PREVIEW_ITEM".equals(objName)) {
+		} else if (NavigatePreviewItem.NAME.equals(objName)) {
 			command = new NavigatePreviewItem(objIndex);
-		} else if ("CONTINUE".equals(objName)) {
+		} else if (Continue.NAME.equals(objName)) {
 			command = new Continue();
-		} else if ("CHECK".equals(objName)) {
+		} else if (Check.NAME.equals(objName)) {
 			command = new Check();
-		} else if ("RESET".equals(objName)) {
+		} else if (Reset.NAME.equals(objName)) {
 			command = new Reset();
-		} else if ("SHOW_ANSWERS".equals(objName)) {
+		} else if (ShowAnswers.NAME.equals(objName)) {
 			command = new ShowAnswers();
-		//} else if ("HIDE_ANSWERS".equals(objName)) {
-		//	command = new HideAnswers();
-		} else if ("LOCK".equals(objName)) {
+		} else if (Lock.NAME.equals(objName)) {
 			command = new Lock();
-		}else if ("UNLOCK".equals(objName)) {
+		}else if (Unlock.NAME.equals(objName)) {
 			command = new Unlock();
 		}
 		
@@ -72,8 +70,9 @@ public abstract class FlowCommand implements IFlowCommand {
 
 	public final static class NavigateNextItem extends FlowCommand{
 		public NavigateNextItem(){
-			super("NAVIGATE_NEXT_ITEM");
+			super(NAME);
 		}		
+		public static final String NAME = "NAVIGATE_NEXT_ITEM"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.nextPage();
@@ -81,8 +80,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public final static class NavigatePreviousItem extends FlowCommand{
 		public NavigatePreviousItem(){
-			super("NAVIGATE_PREVIOUS_ITEM");
+			super(NAME);
 		}		
+		public static final String NAME = "NAVIGATE_PREVIOUS_ITEM"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.previousPage();
@@ -90,8 +90,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public final static class NavigateFirstItem extends FlowCommand{
 		public NavigateFirstItem(){
-			super("NAVIGATE_FIRST_ITEM");
+			super(NAME);
 		}		
+		public static final String NAME = "NAVIGATE_FIRST_ITEM"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.gotoFirstPage();
@@ -99,8 +100,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public final static class NavigateLastItem extends FlowCommand{
 		public NavigateLastItem(){
-			super("NAVIGATE_LAST_ITEM");
+			super(NAME);
 		}		
+		public static final String NAME = "NAVIGATE_LAST_ITEM"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.gotoLastPage();
@@ -108,8 +110,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public final static class NavigateToc extends FlowCommand{
 		public NavigateToc(){
-			super("NAVIGATE_TOC");
-		}		
+			super(NAME);
+		}
+		public static final String NAME = "NAVIGATE_TOC"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.gotoToc();
@@ -117,8 +120,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public final static class NavigateTest extends FlowCommand{
 		public NavigateTest(){
-			super("NAVIGATE_TEST");
+			super(NAME);
 		}		
+		public static final String NAME = "NAVIGATE_TEST"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.gotoTest();
@@ -126,8 +130,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public final static class NavigateSummary extends FlowCommand{
 		public NavigateSummary(){
-			super("NAVIGATE_SUMMARY");
+			super(NAME);
 		}		
+		public static final String NAME = "NAVIGATE_SUMMARY"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.gotoSummary();
@@ -142,8 +147,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public final static class NavigateGotoItem extends FlowCommandWithIndex{
 		public NavigateGotoItem(int index){
-			super("NAVIGATE_GOTO_ITEM", index);
+			super(NAME, index);
 		}	
+		public static final String NAME = "NAVIGATE_GOTO_ITEM"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.gotoPage(index);
@@ -151,8 +157,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public final static class NavigatePreviewItem extends FlowCommandWithIndex{
 		public NavigatePreviewItem(int index){
-			super("NAVIGATE_PREVIEW_ITEM", index);
+			super(NAME, index);
 		}	
+		public static final String NAME = "NAVIGATE_PREVIEW_ITEM"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.previewPage(index);
@@ -160,8 +167,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public static class Check extends FlowCommand{
 		public Check(){
-			super("CHECK");
+			super(NAME);
 		}
+		public static final String NAME = "CHECK"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.checkPage();
@@ -169,8 +177,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public static class Continue extends FlowCommand{
 		public Continue(){
-			super("CONTINUE");
+			super(NAME);
 		}
+		public static final String NAME = "CONTINUE"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.continuePage();
@@ -178,8 +187,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public static class Reset extends FlowCommand{
 		public Reset(){
-			super("RESET");
+			super(NAME);
 		}
+		public static final String NAME = "RESET"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.resetPage();
@@ -187,8 +197,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public static class ShowAnswers extends FlowCommand{
 		public ShowAnswers(){
-			super("SHOW_ANSWERS");
+			super(NAME);
 		}
+		public static final String NAME = "SHOW_ANSWERS"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.showAnswers();
@@ -196,8 +207,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public static class Lock extends FlowCommand{
 		public Lock(){
-			super("LOCK");
+			super(NAME);
 		}
+		public static final String NAME = "LOCK"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.lockPage();
@@ -205,8 +217,9 @@ public abstract class FlowCommand implements IFlowCommand {
 	}
 	public static class Unlock extends FlowCommand{
 		public Unlock(){
-			super("UNLOCK");
+			super(NAME);
 		}
+		public static final String NAME = "UNLOCK"; 
 		@Override
 		public void execute(FlowCommandsListener listener) {
 			listener.unlockPage();
