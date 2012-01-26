6c6dd2e2c7839910c8c049e8c0922248f18946d5
rrybacki <rrybacki@ydp.com.pl>
2012-01-26 08:09:45 +0000

    unit tests task#55706
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@94435 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- empiria.player/test/eu/ydp/empiria/player/client/controller/extensions/FlowRequestProcessorExtensionTest.java
+++ empiria.player/test/eu/ydp/empiria/player/client/controller/extensions/FlowRequestProcessorExtensionTest.java
@@ -21,7 +21,26 @@ import eu.ydp.empiria.player.client.controller.flow.request.FlowRequestInvoker;
 
 public class FlowRequestProcessorExtensionTest extends ExtensionTestBase {
 
-	protected IFlowCommand[] cmds = {new FlowCommand.NavigateFirstItem()};
+	protected IFlowCommand[] cmdsNavigation = {new FlowCommand.NavigateFirstItem(),
+									 new FlowCommand.NavigateNextItem(),
+									 new FlowCommand.NavigateSummary(),
+									 new FlowCommand.NavigateToc()};
+	
+	protected IFlowCommand[] cmdsActivity = {new FlowCommand.NavigateFirstItem(),
+			 new FlowCommand.Check(),
+			 new FlowCommand.Continue(),
+			 new FlowCommand.ShowAnswers(),
+			 new FlowCommand.Continue(),
+			 new FlowCommand.Check(),
+			 new FlowCommand.ShowAnswers(),
+			 new FlowCommand.Check(),
+			 new FlowCommand.ShowAnswers(),
+			 new FlowCommand.Reset(),
+			 new FlowCommand.Check(),
+			 new FlowCommand.Reset()};
+	
+	protected IFlowCommand[] cmds;
+	
 	protected int counter = 0;
 
 	protected DeliveryEngine de;
@@ -29,7 +48,8 @@ public class FlowRequestProcessorExtensionTest extends ExtensionTestBase {
 	protected FlowCommandsExecutor fce;
 	protected FlowDataSupplier fds;
 	
-	public void testFlow(){
+	public void testNavigation(){
+		cmds = cmdsNavigation;
 		de = initDeliveryEngine(new MockFlowRequestProcessorExtension());
 
 		assertEquals(PageType.TOC, fds.getCurrentPageType());
@@ -38,10 +58,75 @@ public class FlowRequestProcessorExtensionTest extends ExtensionTestBase {
 		assertEquals(PageType.TEST, fds.getCurrentPageType());
 		assertEquals(0, fds.getCurrentPageIndex());
 		
+		fri.invokeRequest(new FlowRequest.NavigateNextItem());
+		assertEquals(PageType.TEST, fds.getCurrentPageType());
+		assertEquals(1, fds.getCurrentPageIndex());
+		
+		fri.invokeRequest(new FlowRequest.NavigateSummary());
+		assertEquals(PageType.SUMMARY, fds.getCurrentPageType());
+		
+		fri.invokeRequest(new FlowRequest.NavigateToc());
+		assertEquals(PageType.TOC, fds.getCurrentPageType());
+		
+		assertEquals(cmdsNavigation.length, counter);
+		
 	}
 	
-	protected void processRequestCheck(IFlowRequest request){
+	public void testActivity(){
+		cmds = cmdsActivity;
+		de = initDeliveryEngine(new MockFlowRequestProcessorExtension());
+
+		assertEquals(PageType.TOC, fds.getCurrentPageType());
+		
+		fri.invokeRequest(new FlowRequest.NavigateFirstItem());
+		assertEquals(PageType.TEST, fds.getCurrentPageType());
+		assertEquals(0, fds.getCurrentPageIndex());
+		
+		fri.invokeRequest(new FlowRequest.Check());
+		assertTrue(fds.getFlowFlagCheck());
+		
+		fri.invokeRequest(new FlowRequest.Continue());
+		assertFalse(fds.getFlowFlagCheck());
+		
+		fri.invokeRequest(new FlowRequest.ShowAnswers());
+		assertTrue(fds.getFlowFlagShowAnswers());
+		
+		fri.invokeRequest(new FlowRequest.Continue());
+		assertFalse(fds.getFlowFlagShowAnswers());
+		
+		fri.invokeRequest(new FlowRequest.Check());
+		assertTrue(fds.getFlowFlagCheck());
+		
+		fri.invokeRequest(new FlowRequest.ShowAnswers());
+		assertFalse(fds.getFlowFlagCheck());
+		assertTrue(fds.getFlowFlagShowAnswers());
+		
+		fri.invokeRequest(new FlowRequest.Check());
+		assertFalse(fds.getFlowFlagShowAnswers());
+		assertTrue(fds.getFlowFlagCheck());
+		
+		fri.invokeRequest(new FlowRequest.ShowAnswers());
+		assertFalse(fds.getFlowFlagCheck());
+		assertTrue(fds.getFlowFlagShowAnswers());
+		
+		fri.invokeRequest(new FlowRequest.Reset());
+		assertFalse(fds.getFlowFlagCheck());
+		assertFalse(fds.getFlowFlagShowAnswers());
+		
+		fri.invokeRequest(new FlowRequest.Check());
+		assertFalse(fds.getFlowFlagShowAnswers());
+		assertTrue(fds.getFlowFlagCheck());
+		
+		fri.invokeRequest(new FlowRequest.Reset());
+		assertFalse(fds.getFlowFlagCheck());
+		assertFalse(fds.getFlowFlagShowAnswers());
+		
+		assertEquals(cmdsActivity.length, counter);
+	}
+	
+	protected void processRequestCheck(IFlowRequest request){		
 		if (counter < cmds.length){
+			assertEquals(cmds[counter].getName(), request.getName());
 			fce.executeCommand(cmds[counter]);
 			counter++;
 		}
---
+++ empiria.player/test/eu/ydp/empiria/player/client/controller/extensions/InteractionEventSocketUserExtensionTest.java
@@ -0,0 +1,119 @@
+package eu.ydp.empiria.player.client.controller.extensions;
+
+import eu.ydp.empiria.player.client.controller.delivery.DeliveryEngine;
+import eu.ydp.empiria.player.client.controller.events.delivery.DeliveryEvent;
+import eu.ydp.empiria.player.client.controller.events.delivery.DeliveryEventType;
+import eu.ydp.empiria.player.client.controller.events.interaction.FeedbackInteractionSoundEvent;
+import eu.ydp.empiria.player.client.controller.events.interaction.InteractionEventsListener;
+import eu.ydp.empiria.player.client.controller.events.interaction.MediaInteractionSoundEvent;
+import eu.ydp.empiria.player.client.controller.events.interaction.MediaInteractionSoundEventCallback;
+import eu.ydp.empiria.player.client.controller.events.interaction.MediaInteractionSoundEventCallforward;
+import eu.ydp.empiria.player.client.controller.events.interaction.StateChangedInteractionEvent;
+import eu.ydp.empiria.player.client.controller.extensions.FlowRequestProcessorExtensionTest.MockFlowRequestProcessorExtension;
+import eu.ydp.empiria.player.client.controller.extensions.internal.InternalExtension;
+import eu.ydp.empiria.player.client.controller.extensions.types.DeliveryEventsListenerExtension;
+import eu.ydp.empiria.player.client.controller.extensions.types.InteractionEventSocketUserExtension;
+import eu.ydp.empiria.player.client.module.IUniqueModule;
+
+public class InteractionEventSocketUserExtensionTest extends ExtensionTestBase {
+
+	protected String test = "";
+	protected int counter = 0;
+	
+	protected DeliveryEngine de;
+	protected InteractionEventsListener iel;
+	
+	public void testStateChangedEvent(){
+		de = initDeliveryEngine(new MockInteractionEventSocketUserExtension(), false);
+		test = "testStateChangedEvent";
+		iel.onStateChanged(new StateChangedInteractionEvent(true, new IUniqueModule() {
+			
+			@Override
+			public String getIdentifier() {
+				return "RESPONSE1";
+			}
+		}));
+		assertEquals(1, counter);		
+	}
+	
+	public void testFeedbackInteractionSoundEvent(){
+		de = initDeliveryEngine(new MockInteractionEventSocketUserExtension(), false);
+		test = "testFeedbackInteractionSoundEvent";
+		iel.onFeedbackSound(new FeedbackInteractionSoundEvent("http://www.ydp.eu./xxx.mp3"));
+		assertEquals(1, counter);		
+	}
+	
+	public void testMediaInteractionSoundEvent(){
+		de = initDeliveryEngine(new MockInteractionEventSocketUserExtension(), false);
+		test = "testMediaInteractionSoundEvent";
+		iel.onMediaSound(new MediaInteractionSoundEvent("http://www.ydp.eu./xxx.mp3", new MediaInteractionSoundEventCallback() {
+			
+			@Override
+			public void setCallforward(MediaInteractionSoundEventCallforward callforward) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStop() {
+				counter++;
+			}
+			
+			@Override
+			public void onPlay() {
+				counter++;
+			}
+		}));
+		assertEquals(3, counter);		
+	}
+	
+	protected void onDeliveryEventMain(DeliveryEvent deliveryEvent){
+		if (test.equals("testStateChangedEvent")){
+			assertEquals(DeliveryEventType.STATE_CHANGED, deliveryEvent.getType());
+			assertEquals(2, deliveryEvent.getParams().size());
+			assertTrue(deliveryEvent.getParams().containsKey("userInteract"));
+			assertEquals(deliveryEvent.getParams().get("userInteract"), Boolean.TRUE);
+			assertTrue(deliveryEvent.getParams().containsKey("sender"));
+			assertTrue(deliveryEvent.getParams().get("sender") instanceof IUniqueModule);
+			assertEquals(((IUniqueModule)deliveryEvent.getParams().get("sender")).getIdentifier(), "RESPONSE1");
+			counter = 1;
+		} else if (test.equals("testFeedbackInteractionSoundEvent")){
+			assertEquals(DeliveryEventType.FEEDBACK_SOUND, deliveryEvent.getType());
+			assertEquals(1, deliveryEvent.getParams().size());
+			assertTrue(deliveryEvent.getParams().containsKey("url"));
+			assertEquals(deliveryEvent.getParams().get("url"), "http://www.ydp.eu./xxx.mp3");
+			counter = 1;
+		} else if (test.equals("testMediaInteractionSoundEvent")){
+			assertEquals(DeliveryEventType.MEDIA_SOUND_PLAY, deliveryEvent.getType());
+			assertEquals(2, deliveryEvent.getParams().size());
+			assertTrue(deliveryEvent.getParams().containsKey("url"));
+			assertEquals(deliveryEvent.getParams().get("url"), "http://www.ydp.eu./xxx.mp3");
+			assertTrue(deliveryEvent.getParams().containsKey("callback"));
+			assertTrue(deliveryEvent.getParams().get("callback") instanceof MediaInteractionSoundEventCallback);
+			((MediaInteractionSoundEventCallback)deliveryEvent.getParams().get("callback")).onPlay();
+			((MediaInteractionSoundEventCallback)deliveryEvent.getParams().get("callback")).onStop();
+			counter++;
+		}
+	}
+	
+	
+	protected class MockInteractionEventSocketUserExtension extends InternalExtension implements DeliveryEventsListenerExtension, InteractionEventSocketUserExtension{
+
+		@Override
+		public void init() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void onDeliveryEvent(DeliveryEvent deliveryEvent) {
+			onDeliveryEventMain(deliveryEvent);
+		}
+
+		@Override
+		public void setInteractionEventsListener(InteractionEventsListener listener) {
+			iel = listener;
+		}
+		
+	}
+}
