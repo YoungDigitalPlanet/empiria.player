cf522039a42cbf6b2573a77d0557df98d2c1cbe1
mkaldonek <mkaldonek@ydp.com.pl>
2012-03-05 07:50:47 +0000

    added next/prev button bug 57982
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@98019 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/controller/extensions/internal/modules/PrevPageButtonExtension.java
@@ -0,0 +1,10 @@
+package eu.ydp.empiria.player.client.controller.extensions.internal.modules;
+
+public class PrevPageButtonExtension extends NavigationButtonExtension {
+
+	@Override
+	public String getModuleNodeName() {
+		return "prevItemNavigation";
+	}
+
+}
