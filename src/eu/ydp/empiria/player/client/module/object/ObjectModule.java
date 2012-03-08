34608cc89877a15ad249a4007f0bd9fdb62d4cb1
rrybacki <rrybacki@ydp.com.pl>
2012-03-08 14:01:47 +0000

    title and description in object module task#59446
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@98482 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/module/object/ObjectModule.java
+++ src/eu/ydp/empiria/player/client/module/object/ObjectModule.java
@@ -1,9 +1,12 @@
 package eu.ydp.empiria.player.client.module.object;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.xml.client.Element;
+import com.google.gwt.xml.client.NodeList;
 
 import eu.ydp.empiria.player.client.module.ISimpleModule;
 import eu.ydp.empiria.player.client.module.ModuleSocket;
@@ -16,31 +19,66 @@ public class ObjectModule implements ISimpleModule {
 
 	protected Widget widget;
 	
+	protected Panel containerPanel;
+	protected Panel titlePanel;
+	protected Panel descriptionPanel;
+	protected Panel contentPanel;
+	
 	@Override
 	public Widget getView() {
-		return widget;
+		return containerPanel;
 	}
 
 	@Override
 	public void initModule(Element element, ModuleSocket ms, ModuleInteractionListener mil) {
-		
-		  String html;
-		  String src = XMLUtils.getAttributeAsString(element, "data");
-		  String type = XMLUtils.getAttributeAsString(element, "type");
-		  
-		  if(type.startsWith("video")){
-
-			  VideoImpl video = GWT.create(VideoImpl.class);
-			  video.setSrc(src);
-			  widget = video.asWidget();
-		  }
-		  else{
-		    
-		      AudioImpl audio = GWT.create(AudioImpl.class);
-		      html = audio.getHTML(src);
-			  widget = new HTML(html);
-		  }
 			
+		String html;
+		String src = XMLUtils.getAttributeAsString(element, "data");
+		String type = XMLUtils.getAttributeAsString(element, "type");
+		  
+		if(type.startsWith("video")){
+			VideoImpl video = GWT.create(VideoImpl.class);
+			video.setSrc(src);
+			widget = video.asWidget();
+		}
+		else{
+			AudioImpl audio = GWT.create(AudioImpl.class);
+			html = audio.getHTML(src);
+			widget = new HTML(html);
+		}
+			
+		containerPanel = new FlowPanel();
+		containerPanel.setStyleName("qp-object-container");
+		
+		titlePanel = new FlowPanel();
+		titlePanel.setStyleName("qp-object-title");
+		
+		contentPanel = new FlowPanel();
+		contentPanel.setStyleName("qp-object-content");
+		if (widget != null)
+			contentPanel.add(widget);
+		
+		descriptionPanel = new FlowPanel();
+		descriptionPanel.setStyleName("qp-object-description");
+		
+		containerPanel.add(titlePanel);
+		containerPanel.add(contentPanel);
+		containerPanel.add(descriptionPanel);
+
+		NodeList titleNodes = element.getElementsByTagName("title");
+		if (titleNodes.getLength() > 0){
+			Widget titleWidget = ms.getInlineBodyGeneratorSocket().generateInlineBody(titleNodes.item(0));
+			if (titleWidget != null){
+				titlePanel.add(titleWidget);
+			}
+		}
+		NodeList descriptionNodes = element.getElementsByTagName("description");
+		if (descriptionNodes.getLength() > 0){
+			Widget descriptionWidget = ms.getInlineBodyGeneratorSocket().generateInlineBody(descriptionNodes.item(0));
+			if (descriptionWidget != null){
+				descriptionPanel.add(descriptionWidget);
+			}
+		}
 		
 	}
 
