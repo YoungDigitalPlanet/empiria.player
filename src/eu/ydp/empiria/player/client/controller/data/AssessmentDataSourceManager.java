11cef7ccd6cc7d38dda81cec615f5db1d418c865
plelakowski <plelakowski@ydp.com.pl>
2012-05-11 09:33:46 +0000

    page template as class name bug id : 63063
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@103898 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/controller/data/AssessmentDataSourceManager.java
+++ src/eu/ydp/empiria/player/client/controller/data/AssessmentDataSourceManager.java
@@ -1,5 +1,7 @@
 package eu.ydp.empiria.player.client.controller.data;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 import com.google.gwt.xml.client.Document;
@@ -17,15 +19,15 @@ import eu.ydp.empiria.player.client.util.localisation.LocalePublisher;
 import eu.ydp.empiria.player.client.util.localisation.LocaleVariable;
 import eu.ydp.empiria.player.client.util.xml.document.XMLData;
 
-public class AssessmentDataSourceManager implements SkinDataLoaderListener{
+public class AssessmentDataSourceManager implements SkinDataLoaderListener {
 
-	public AssessmentDataSourceManager(AssessmentDataLoaderEventListener l){
+	public AssessmentDataSourceManager(AssessmentDataLoaderEventListener l) {
 		listener = l;
 		itemsCount = -1;
 		errorMessage = "";
 		skinData = new SkinDataSourceManager(this);
 	}
-	
+
 	private XMLData data;
 	private AssessmentData assessmentData;
 	private AssessmentDataLoaderEventListener listener;
@@ -35,131 +37,161 @@ public class AssessmentDataSourceManager implements SkinDataLoaderListener{
 	private LibraryLink libraryLink;
 	private SkinDataSourceManager skinData;
 	private boolean isDefaultData;
-	
-	public void setAssessmentData(XMLData d){
-		if(!isItemDocument(d.getDocument())){
+	private List<Element> items = null;
+
+	public void setAssessmentData(XMLData d) {
+		if (!isItemDocument(d.getDocument())) {
 			isDefaultData = false;
 			initializeData(d);
-		}else{
+		} else {
 			isDefaultData = true;
 			initializeDefaultData();
 		}
 	}
-	
-	public void setAssessmentLoadingError(String err){
+
+	public void setAssessmentLoadingError(String err) {
 		String detail = "";
-		if (err != null  &&  err.indexOf(":") != -1)
+		if (err != null && err.indexOf(":") != -1)
 			detail = err.substring(0, err.indexOf(":"));
 		errorMessage = LocalePublisher.getText(LocaleVariable.ERROR_ASSESSMENT_FAILED_TO_LOAD) + detail;
 	}
-	
-	private void initializeDefaultData(){
+
+	private void initializeDefaultData() {
 		data = new XMLData(XMLParser.parse("<assessmentTest title=\"\"/>"), "");
 		itemsCount = 1;
 		styleDeclaration = new StyleLinkDeclaration(data.getDocument().getElementsByTagName("styleDeclaration"), data.getBaseURL());
 		listener.onAssessmentDataLoaded();
 	}
-	
-	private void initializeData(XMLData d){
+
+	private void initializeData(XMLData d) {
 		String skinUrl = getSkinUrl(d.getDocument());
-		
+
 		data = d;
 		itemsCount = -1;
 		styleDeclaration = new StyleLinkDeclaration(data.getDocument().getElementsByTagName("styleDeclaration"), data.getBaseURL());
 		libraryLink = new LibraryLink(data.getDocument().getElementsByTagName("extensionsLibrary"), data.getBaseURL());
-		
-		if(skinUrl != null){
+
+		if (skinUrl != null) {
 			skinUrl = data.getBaseURL().concat(skinUrl);
 			skinData.load(skinUrl);
-		}else{
+		} else {
 			assessmentData = new AssessmentData(data, null);
 			listener.onAssessmentDataLoaded();
 		}
 	}
-	
-	public XMLData getAssessmentXMLData(){
+
+	public XMLData getAssessmentXMLData() {
 		return data;
 	}
-	
-	public AssessmentData getAssessmentData(){
+
+	public AssessmentData getAssessmentData() {
 		return assessmentData;
 	}
-	
-	public boolean isError(){
+
+	public boolean isError() {
 		return errorMessage.length() > 0;
 	}
-	
-	public String getErrorMessage(){
+
+	public String getErrorMessage() {
 		return errorMessage;
 	}
-	
-	public boolean hasLibrary(){
+
+	public boolean hasLibrary() {
 		return libraryLink.hasLink();
 	}
-	
-	public String getLibraryLink(){
+
+	public String getLibraryLink() {
 		return libraryLink.getLink();
 	}
-	
-	public boolean isDefaultData(){
+
+	public boolean isDefaultData() {
 		return isDefaultData;
 	}
-	
-	public String getAssessmentTitle(){
+
+	public String getAssessmentTitle() {
 		String title = "";
-		if (data != null){
-			try{
+		if (data != null) {
+			try {
 				Node rootNode = data.getDocument().getElementsByTagName("assessmentTest").item(0);
-				title = ((Element)rootNode).getAttribute("title");
+				title = ((Element) rootNode).getAttribute("title");
 			} catch (Exception e) {
 			}
-			
+
 		}
 		return title;
 	}
-	
-	public String[] getItemUrls(){
-		
+
+	public String[] getItemUrls() {
+
 		String[] itemUrls = new String[0];
-		
-		if (data != null){
+
+		if (data != null) {
 			try {
 				NodeList nodes = data.getDocument().getElementsByTagName("assessmentItemRef");
 				String[] tmpItemUrls = new String[nodes.getLength()];
-				for(int i = 0; i < nodes.getLength(); i++){
+				for (int i = 0; i < nodes.getLength(); i++) {
 					Node itemRefNode = nodes.item(i);
-					
-					if (((Element)itemRefNode).getAttribute("href").startsWith("http"))
-						tmpItemUrls[i] = ((Element)itemRefNode).getAttribute("href");
+
+					if (((Element) itemRefNode).getAttribute("href").startsWith("http"))
+						tmpItemUrls[i] = ((Element) itemRefNode).getAttribute("href");
 					else
-						tmpItemUrls[i] = data.getBaseURL() + ((Element)itemRefNode).getAttribute("href");
-			    }
+						tmpItemUrls[i] = data.getBaseURL() + ((Element) itemRefNode).getAttribute("href");
+				}
 				itemUrls = tmpItemUrls;
-			} catch (Exception e) {	}
+			} catch (Exception e) {
+			}
 		}
-		
-		//TODO
+
+		// TODO
 		return itemUrls;
 	}
-	
-	public int getItemsCount(){
+
+	/**
+	 * Zwraca wezel assessmentItemRef o wskazanym id
+	 *
+	 * @param index
+	 *            index wezla
+	 * @return Element lub null gdy element o podanym indeksie nie istnieje
+	 */
+	public Element getItem(int index) {
+		if (data != null && items == null) {
+			try {
+				items = new ArrayList<Element>();
+				NodeList nodes = data.getDocument().getElementsByTagName("assessmentItemRef");
+				for (int x = 0; x < nodes.getLength(); ++x) {
+					Node n = nodes.item(x);
+					if (n.getNodeType() == Node.ELEMENT_NODE) {
+						items.add((Element) n);
+					}
+				}
+			} catch (Exception e) {
+
+			}
+		}
+		if (items != null && index < items.size() - 1 && index > -1) {
+			return items.get(index);
+		}
+		return null;
+	}
+
+	public int getItemsCount() {
 		if (itemsCount == -1)
 			itemsCount = getItemUrls().length;
-		
+
 		return itemsCount;
 	}
-	
-	public Vector<String> getStyleLinksForUserAgent(String userAgent){
+
+	public Vector<String> getStyleLinksForUserAgent(String userAgent) {
 		Vector<String> declarations = new Vector<String>();
-		
+
 		if (styleDeclaration != null)
 			declarations.addAll(styleDeclaration.getStyleLinksForUserAgent(userAgent));
-		
+
 		declarations.addAll(skinData.getStyleLinksForUserAgent(userAgent));
-		
+
 		return declarations;
 	}
-	
+
 	public void onSkinLoad() {
 		assessmentData = new AssessmentData(data, skinData.getSkinData());
 		listener.onAssessmentDataLoaded();
@@ -167,21 +199,22 @@ public class AssessmentDataSourceManager implements SkinDataLoaderListener{
 
 	public void onSkinLoadError() {
 		assessmentData = new AssessmentData(data, null);
-		listener.onAssessmentDataLoaded();		
+		listener.onAssessmentDataLoaded();
 	}
-	
-	private String getSkinUrl(Document document){
+
+	private String getSkinUrl(Document document) {
 		String url = null;
-		
-		try{
+
+		try {
 			Node testViewNode = document.getElementsByTagName("testView").item(0);
 			url = testViewNode.getAttributes().getNamedItem("href").getNodeValue();
-		}catch (Exception e) {}
-		
+		} catch (Exception e) {
+		}
+
 		return url;
 	}
-	
-	private boolean isItemDocument(Document doc){
+
+	private boolean isItemDocument(Document doc) {
 		try {
 			return doc.getDocumentElement().getNodeName().equals("assessmentItem");
 		} catch (Exception e) {
--- src/eu/ydp/empiria/player/client/module/report/ReportModule.java
+++ src/eu/ydp/empiria/player/client/module/report/ReportModule.java
@@ -1,11 +1,9 @@
 package eu.ydp.empiria.player.client.module.report;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasWidgets;
@@ -20,20 +18,19 @@ import eu.ydp.empiria.player.client.controller.data.DataSourceDataSupplier;
 import eu.ydp.empiria.player.client.controller.flow.request.FlowRequestInvoker;
 import eu.ydp.empiria.player.client.controller.session.datasupplier.SessionDataSupplier;
 import eu.ydp.empiria.player.client.module.IContainerModule;
-import eu.ydp.empiria.player.client.module.ISimpleModule;
 import eu.ydp.empiria.player.client.module.ModuleSocket;
 import eu.ydp.empiria.player.client.module.listener.ModuleInteractionListener;
 import eu.ydp.empiria.player.client.util.IntegerUtils;
 import eu.ydp.empiria.player.client.util.xml.XMLUtils;
 
 public class ReportModule implements IContainerModule {
-	
+
 	protected SessionDataSupplier sessionDataSupplier;
 	protected DataSourceDataSupplier dataSourceDataSupplier;
 	protected FlowRequestInvoker flowRequestInvoker;
 	protected String content;
 	private String userClass;
-	
+
 	protected Panel mainPanel;
 	protected FlexTable table;
 
@@ -47,33 +44,33 @@ public class ReportModule implements IContainerModule {
 	public void initModule(Element element, ModuleSocket ms, ModuleInteractionListener mil, BodyGeneratorSocket bgs) {
 
 		userClass = XMLUtils.getAttributeAsString(element, "class");
-		
+
 		String range = "1:-1";
 		Map<String, String> styles = ms.getStyles(element);
-		if (styles.containsKey("-empiria-report-items-include")){
+		if (styles.containsKey("-empiria-report-items-include")) {
 			range = styles.get("-empiria-report-items-include");
 		}
 		List<Integer> itemIndexes = parseRange(range);
 		NodeList rowNodes = element.getChildNodes();
-		
+
 		table = new FlexTable();
 		table.setStyleName("qp-report-table");
 		String cls = element.getAttribute("class");
 		if (cls != null)
 			table.addStyleName(cls);
-		
+
 		int currCol = 0;
 		int currRow = 0;
-		
-		for (int r = 0 ; r < rowNodes.getLength() ; r ++ ){
-			if (rowNodes.item(r).getNodeType() == Node.ELEMENT_NODE  &&  "rr".equals(rowNodes.item(r).getNodeName())){
+
+		for (int r = 0; r < rowNodes.getLength(); r++) {
+			if (rowNodes.item(r).getNodeType() == Node.ELEMENT_NODE && "rr".equals(rowNodes.item(r).getNodeName())) {
 				currCol = 0;
 				NodeList cellNodes = rowNodes.item(r).getChildNodes();
-				for (int d = 0 ; d < cellNodes.getLength() ; d++ ){
-					if (cellNodes.item(d).getNodeType() == Node.ELEMENT_NODE  &&  "rd".equals(cellNodes.item(d).getNodeName())){
+				for (int d = 0; d < cellNodes.getLength(); d++) {
+					if (cellNodes.item(d).getNodeType() == Node.ELEMENT_NODE && "rd".equals(cellNodes.item(d).getNodeName())) {
 						int colspan = 1;
-						if (((Element)cellNodes.item(d)).hasAttribute("colspan")){
-							colspan = IntegerUtils.tryParseInt(((Element)cellNodes.item(d)).getAttribute("colspan"), 1);
+						if (((Element) cellNodes.item(d)).hasAttribute("colspan")) {
+							colspan = IntegerUtils.tryParseInt(((Element) cellNodes.item(d)).getAttribute("colspan"), 1);
 						}
 						Panel cellPanel = new FlowPanel();
 						cellPanel.setStyleName("qp-report-cell");
@@ -83,32 +80,39 @@ public class ReportModule implements IContainerModule {
 						table.getFlexCellFormatter().addStyleName(currRow, currCol, "qp-report-table-cell");
 						table.getFlexCellFormatter().addStyleName(currRow, currCol, "qp-report-table-col-" + String.valueOf(currCol));
 						table.getRowFormatter().addStyleName(currRow, "qp-report-table-row");
-						table.getRowFormatter().addStyleName(currRow, "qp-report-table-row-"+String.valueOf(currRow));
+						table.getRowFormatter().addStyleName(currRow, "qp-report-table-row-" + String.valueOf(currRow));
+						Element el = dataSourceDataSupplier.getItem(currRow-1);
+						if (el != null) {
+							String className = XMLUtils.getAttributeAsString(el, "class");
+							if (className != null) {
+								table.getRowFormatter().addStyleName(currRow,className);
+							}
+						}
 						currCol++;
 					}
-					
+
 				}
 				currRow++;
-			} else if (rowNodes.item(r).getNodeType() == Node.ELEMENT_NODE  &&  "prr".equals(rowNodes.item(r).getNodeName())){
+			} else if (rowNodes.item(r).getNodeType() == Node.ELEMENT_NODE && "prr".equals(rowNodes.item(r).getNodeName())) {
 				NodeList cellNodes = rowNodes.item(r).getChildNodes();
-				for (int ir = 0 ; ir < itemIndexes.size() ; ir ++){
+				for (int ir = 0; ir < itemIndexes.size(); ir++) {
 					currCol = 0;
-					for (int d = 0 ; d < cellNodes.getLength() ; d++ ){
-						if (cellNodes.item(d).getNodeType() == Node.ELEMENT_NODE  &&  "rd".equals(cellNodes.item(d).getNodeName())){
+					for (int d = 0; d < cellNodes.getLength(); d++) {
+						if (cellNodes.item(d).getNodeType() == Node.ELEMENT_NODE && "rd".equals(cellNodes.item(d).getNodeName())) {
 							int colspan = 1;
-							if (((Element)cellNodes.item(d)).hasAttribute("colspan")){
-								colspan = IntegerUtils.tryParseInt(((Element)cellNodes.item(d)).getAttribute("colspan"), 1);
+							if (((Element) cellNodes.item(d)).hasAttribute("colspan")) {
+								colspan = IntegerUtils.tryParseInt(((Element) cellNodes.item(d)).getAttribute("colspan"), 1);
 							}
 							Node dNode = cellNodes.item(d).cloneNode(true);
-							NodeList linkNodes = ((Element)dNode).getElementsByTagName("link");
-							for (int in = 0 ; in < linkNodes.getLength() ; in ++){
-								if (!((Element)linkNodes.item(in)).hasAttribute("itemIndex")  &&  !((Element)linkNodes.item(in)).hasAttribute("url"))
-									((Element)linkNodes.item(in)).setAttribute("itemIndex", itemIndexes.get(ir).toString());
+							NodeList linkNodes = ((Element) dNode).getElementsByTagName("link");
+							for (int in = 0; in < linkNodes.getLength(); in++) {
+								if (!((Element) linkNodes.item(in)).hasAttribute("itemIndex") && !((Element) linkNodes.item(in)).hasAttribute("url"))
+									((Element) linkNodes.item(in)).setAttribute("itemIndex", itemIndexes.get(ir).toString());
 							}
-							NodeList infoNodes = ((Element)dNode).getElementsByTagName("info");
-							for (int in = 0 ; in < infoNodes.getLength() ; in ++){
-								if (!((Element)infoNodes.item(in)).hasAttribute("itemIndex"))
-									((Element)infoNodes.item(in)).setAttribute("itemIndex", itemIndexes.get(ir).toString());
+							NodeList infoNodes = ((Element) dNode).getElementsByTagName("info");
+							for (int in = 0; in < infoNodes.getLength(); in++) {
+								if (!((Element) infoNodes.item(in)).hasAttribute("itemIndex"))
+									((Element) infoNodes.item(in)).setAttribute("itemIndex", itemIndexes.get(ir).toString());
 							}
 							Panel cellPanel = new FlowPanel();
 							cellPanel.setStyleName("qp-report-cell");
@@ -118,43 +122,50 @@ public class ReportModule implements IContainerModule {
 							table.getFlexCellFormatter().addStyleName(currRow, currCol, "qp-report-table-cell");
 							table.getFlexCellFormatter().addStyleName(currRow, currCol, "qp-report-table-col-" + String.valueOf(currCol));
 							table.getRowFormatter().addStyleName(currRow, "qp-report-table-row");
-							table.getRowFormatter().addStyleName(currRow, "qp-report-table-row-"+String.valueOf(currRow));
+							table.getRowFormatter().addStyleName(currRow, "qp-report-table-row-" + String.valueOf(currRow));
+							Element el = dataSourceDataSupplier.getItem(currRow-1);
+							if (el != null) {
+								String className = XMLUtils.getAttributeAsString(el, "class");
+								if (className != null) {
+									table.getRowFormatter().addStyleName(currRow,className);
+								}
+							}
 							currCol++;
-							
+
 						}
 					}
 					currRow++;
 				}
 			}
 		}
-		
+
 		mainPanel = new FlowPanel();
 		mainPanel.setStyleName("qp-report");
-		if (userClass != null  &&  !"".equals(userClass))
+		if (userClass != null && !"".equals(userClass))
 			mainPanel.addStyleName(userClass);
-		
+
 		mainPanel.add(table);
 	}
 
 	protected List<Integer> parseRange(String range) {
 		List<Integer> items = new ArrayList<Integer>();
 		String[] level1 = range.split(",");
-		for (int i = 0 ; i < level1.length ; i ++){
-			if (level1[i].contains(":")){
-				if (level1[i].split(":").length == 2){
+		for (int i = 0; i < level1.length; i++) {
+			if (level1[i].contains(":")) {
+				if (level1[i].split(":").length == 2) {
 					String from = level1[i].split(":")[0];
 					String to = level1[i].split(":")[1];
 					int fromInt = IntegerUtils.tryParseInt(from, 0);
 					int toInt = IntegerUtils.tryParseInt(to, 0);
-					if (fromInt != 0  &&  toInt != 0){
-						if (toInt > 0){
-							for (int ii = fromInt ; ii <= toInt ; ii ++){
-								items.add(ii-1);
+					if (fromInt != 0 && toInt != 0) {
+						if (toInt > 0) {
+							for (int ii = fromInt; ii <= toInt; ii++) {
+								items.add(ii - 1);
 							}
 						} else {
 							int itemsCount = dataSourceDataSupplier.getItemsCount();
-							for (int ii = fromInt ; ii <= itemsCount+toInt+1 ; ii ++){
-								items.add(ii-1);
+							for (int ii = fromInt; ii <= itemsCount + toInt + 1; ii++) {
+								items.add(ii - 1);
 							}
 						}
 					}
@@ -162,7 +173,7 @@ public class ReportModule implements IContainerModule {
 			} else {
 				Integer intValue = IntegerUtils.tryParseInt(level1[i].trim(), 0);
 				if (intValue != 0)
-					items.add(intValue-1);
+					items.add(intValue - 1);
 			}
 		}
 		return items;
@@ -171,7 +182,7 @@ public class ReportModule implements IContainerModule {
 	@Override
 	public Widget getView() {
 		return mainPanel;
-	}	
+	}
 
 	@Override
 	public HasWidgets getContainer() {
