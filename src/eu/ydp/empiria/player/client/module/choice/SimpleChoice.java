fdc414c8db1c69194e6303144530bd09a95574f3
plelakowski <plelakowski@ydp.com.pl>
2012-05-23 12:20:52 +0000

    not all  mchoice elements was reset bug id: 63797
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@104922 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/module/choice/SimpleChoice.java
+++ src/eu/ydp/empiria/player/client/module/choice/SimpleChoice.java
@@ -1,39 +1,25 @@
 package eu.ydp.empiria.player.client.module.choice;
 
-import java.util.Vector;
-
-import com.google.gwt.dom.client.Document;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.MouseOutEvent;
 import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.event.dom.client.MouseOverEvent;
 import com.google.gwt.event.dom.client.MouseOverHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AbsolutePanel;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.InlineHTML;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.xml.client.Element;
 import com.google.gwt.xml.client.NodeList;
 
-import eu.ydp.empiria.player.client.components.AccessibleCheckBox;
-import eu.ydp.empiria.player.client.components.AccessibleRadioButton;
-import eu.ydp.empiria.player.client.components.ElementWrapperWidget;
 import eu.ydp.empiria.player.client.controller.feedback.InlineFeedback;
-import eu.ydp.empiria.player.client.controller.feedback.InlineFeedbackSocket;
 import eu.ydp.empiria.player.client.module.ModuleSocket;
 import eu.ydp.empiria.player.client.module.components.choicebutton.ChoiceButtonBase;
 import eu.ydp.empiria.player.client.module.components.choicebutton.ChoiceGroupController;
 import eu.ydp.empiria.player.client.module.components.choicebutton.MultiChoiceButton;
 import eu.ydp.empiria.player.client.module.components.choicebutton.SingleChoiceButton;
-import eu.ydp.empiria.player.client.module.listener.FeedbackModuleInteractionListener;
 import eu.ydp.empiria.player.client.module.listener.ModuleInteractionListener;
-import eu.ydp.empiria.player.client.util.BrowserCompatibility;
 import eu.ydp.empiria.player.client.util.xml.XMLUtils;
 
 public class SimpleChoice extends FlowPanel {
@@ -45,19 +31,19 @@ public class SimpleChoice extends FlowPanel {
 	private AbsolutePanel container;
 	private Panel optionPanel;
 	private Panel labelPanel;
-	private Panel markAnswersPanel; 
+	private Panel markAnswersPanel;
 	protected boolean multi;
-	
-	
+
+
 	public SimpleChoice(Element element,  boolean multi, final SimpleChoiceListener listener, ModuleSocket ms, ModuleInteractionListener mil,
 			ChoiceGroupController ctrl) {
-		
+
 		this.multi = multi;
 
 		identifier = XMLUtils.getAttributeAsString(element, "identifier");
-		
+
 		setStyleName("qp-choice-option-box");
-		
+
 		// button
 		if (multi)
 			button = new MultiChoiceButton("choice-multi");
@@ -82,7 +68,7 @@ public class SimpleChoice extends FlowPanel {
 		});
 
 		Widget contentWidget = ms.getInlineBodyGeneratorSocket().generateInlineBody(element);
-		
+
 		cover = new AbsolutePanel();
 		cover.setStyleName("qp-choice-option-cover");
 		cover.addDomHandler(new MouseOverHandler() {
@@ -100,16 +86,16 @@ public class SimpleChoice extends FlowPanel {
 				listener.onSimpleChoiceClick(instance);
 			}
 		}, ClickEvent.getType());
-		
+
 		container = new AbsolutePanel();
 		container.setStyleName("qp-choice-option-container");
 		container.add(contentWidget, 0, 0);
 		container.add(cover, 0, 0);
-		
+
 		labelPanel = new FlowPanel();
 		labelPanel.setStyleName("qp-choice-label");
 		labelPanel.add(container);
-		
+
 		markAnswersPanel = new FlowPanel();
 		String buttonTypeName;
 		if (multi)
@@ -125,28 +111,30 @@ public class SimpleChoice extends FlowPanel {
 		optionPanel.add(labelPanel); // tmp
 
 		add(optionPanel);
-		
+
 		// feedback
-		
+
 		NodeList inlineFeedbackNodes = element.getElementsByTagName("feedbackInline");
 		for (int f = 0 ; f < inlineFeedbackNodes.getLength() ; f ++){
 			ms.addInlineFeedback(new InlineFeedback(labelPanel, inlineFeedbackNodes.item(f), ms, mil));
 		}
-		
-	    
+
+
 	}
-	
+
 	public void onOwnerAttached(){
 	}
 
-	public void markAnswers(boolean mark, boolean correct) {
-		
-		String buttonTypeName;
-		if (multi)
-			buttonTypeName = "multi";
-		else
-			buttonTypeName = "single";
+	/**
+	 * Zwraca typ przycisku multi lub single
+	 * @return
+	 */
+	private String getButtonType(){
+		return multi ? "multi" : "single";
+	}
 
+	public void markAnswers(boolean mark, boolean correct) {
+		String buttonTypeName = getButtonType();
 		if (!mark){
 			markAnswersPanel.setStyleName("qp-choice-button-"+buttonTypeName+"-markanswers");
 		} else {
@@ -159,12 +147,10 @@ public class SimpleChoice extends FlowPanel {
 				markAnswersPanel.setStyleName("qp-choice-button-"+buttonTypeName+"-markanswers-none");
 			}
 		}
-			
-			
 		setEnabled(!mark);
 
 	}
-	
+
 	public void setSelected(boolean sel){
 		button.setSelected(sel);
 	}
@@ -187,17 +173,18 @@ public class SimpleChoice extends FlowPanel {
 	public String getIdentifier() {
 		return identifier;
 	}
-	
+
 	public void reset(){
 		button.setSelected(false);
 		setEnabled(true);
+		markAnswersPanel.setStyleName("qp-choice-button-"+getButtonType()+"-markanswers-none");
 	}
 
 
 	public void setMouseOver(){
 		button.setMouseOver(true);
 	}
-	
+
 	public void setMouseOut(){
 		button.setMouseOver(false);
 	}
