7d9fc233036b36e7f4e9f818b5c0a91494c3cf6e
plelakowski <plelakowski@ydp.com.pl>
2012-06-14 07:26:26 +0000

    add skins to html5 videoplayer bug id: 63890
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@107263 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/module/object/ObjectModule.java
+++ src/eu/ydp/empiria/player/client/module/object/ObjectModule.java
@@ -1,104 +1,164 @@
 package eu.ydp.empiria.player.client.module.object;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.media.client.MediaBase;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.xml.client.Element;
 import com.google.gwt.xml.client.NodeList;
 
+import eu.ydp.empiria.player.client.controller.body.BodyGeneratorSocket;
 import eu.ydp.empiria.player.client.module.Factory;
 import eu.ydp.empiria.player.client.module.SimpleModuleBase;
 import eu.ydp.empiria.player.client.module.object.impl.AudioImpl;
+import eu.ydp.empiria.player.client.module.object.impl.OggVideoImpl;
 import eu.ydp.empiria.player.client.module.object.impl.VideoImpl;
+import eu.ydp.empiria.player.client.util.KeyValue;
+import eu.ydp.empiria.player.client.util.UserAgentChecker;
+import eu.ydp.empiria.player.client.util.UserAgentChecker.MobileUserAgent;
+import eu.ydp.empiria.player.client.util.UserAgentChecker.UserAgent;
 import eu.ydp.empiria.player.client.util.XMLUtils;
 
 public class ObjectModule extends SimpleModuleBase implements Factory<ObjectModule> {
 
 	protected Widget widget;
+	protected MediaBase media;
+	ObjectModuleView moduleView = new ObjectModuleView();
 
-	protected Panel containerPanel;
-	protected Panel titlePanel;
-	protected Panel descriptionPanel;
-	protected Panel contentPanel;
+	BodyGeneratorSocket bodyGeneratorSocket = null;
 
 	@Override
 	public Widget getView() {
-		return containerPanel;
+		return moduleView;
+	}
+
+	/**
+	 * Zwraca kolekcje zrodel powiazanych z tym elementem key=url,value=type
+	 *
+	 * @param element
+	 * @return
+	 */
+	private List<KeyValue<String, String>> getSource(Element element, String mediaType) {
+		NodeList sources = element.getElementsByTagName("source");
+		String src = XMLUtils.getAttributeAsString(element, "data");
+		List<KeyValue<String, String>> retValue = new ArrayList<KeyValue<String, String>>();
+		if (sources.getLength() > 0) {
+			for (int x = 0; x < sources.getLength(); ++x) {
+				src = XMLUtils.getAttributeAsString((Element) sources.item(x), "src");
+				String type = XMLUtils.getAttributeAsString((Element) sources.item(x), "type");
+				retValue.add(new KeyValue<String, String>(src, type));
+			}
+		} else {
+			String[] type = src.split("[.]");
+			retValue.add(new KeyValue<String, String>(src, mediaType + "/" + type[type.length - 1]));
+		}
+		return retValue;
+	}
+
+	/**
+	 * tworzy element video
+	 *
+	 * @param element
+	 * @param template
+	 *            czy bedzie urzywana skorka
+	 */
+	private void createVideo(Element element, boolean template) {
+		List<KeyValue<String, String>> sources = getSource(element, "video");
+		boolean containsOgg = false;
+		VideoImpl video = null;
+		if (UserAgentChecker.isUserAgent(UserAgent.GECKO1_8) || UserAgentChecker.isMobileUserAgent(MobileUserAgent.FIREFOX)) {
+			for (KeyValue<String, String> src : sources) {
+				if (src.getValue().matches(".*ogv|.*ogg")) {
+					containsOgg = true;
+					break;
+				}
+			}
+			if (containsOgg) {
+				video = GWT.create(OggVideoImpl.class);
+			}
+		}
+		if (video == null) {
+			video = GWT.create(VideoImpl.class);
+		}
+
+		video.setShowNativeControls(!template);
+		for (KeyValue<String, String> src : sources) {
+			video.addSrc(src.getKey(), src.getValue());
+		}
+		int width = XMLUtils.getAttributeAsInt(element, "width");
+		int height = XMLUtils.getAttributeAsInt(element, "height");
+		String poster = XMLUtils.getAttributeAsString(element, "poster");
+		if (width > 0) {
+			video.setWidth(width);
+		}
+		if (height > 0) {
+			video.setHeight(height);
+		}
+		if (poster.length() > 0) {
+			video.setPoster(poster);
+		}
+		media = video.getMedia();
+		widget = video.asWidget();
+	}
+
+	/**
+	 * tworzy element audio
+	 *
+	 * @param element
+	 * @param template
+	 *            czy bedzie urzywana skorka
+	 */
+	private void createAudio(Element element, boolean template) {
+		AudioImpl audio = GWT.create(AudioImpl.class);
+		audio.setSource(getSource(element, "audio").get(0).getKey());
+		media = audio.getMedia();
+		widget = audio.asWidget();
+	}
+
+	private void parseTemplate(Element template, FlowPanel parent) {
+		TemplateParser parser = new TemplateParser(media);
+		parser.parse(template, parent);
 	}
 
 	@Override
 	public void initModule(Element element) {
-		String html;
-		String src = XMLUtils.getAttributeAsString(element, "data");
 		String type = XMLUtils.getAttributeAsString(element, "type");
-		if(src.length()==0){
-			Element source = XMLUtils.getFirstElementWithTagName(element, "source");
-			if(source!=null){
-				src = XMLUtils.getAttributeAsString(source, "src");
-			}
-		}
-		if(type.startsWith("video")){
-			VideoImpl video = GWT.create(VideoImpl.class);
-			video.setSrc(src);
-			int width = XMLUtils.getAttributeAsInt(element, "width");
-			int height = XMLUtils.getAttributeAsInt(element, "height");
-			String poster = XMLUtils.getAttributeAsString(element, "poster");
-			if(width>0){
-				video.setWidth(width);
-			}
-			if(height>0){
-				video.setHeight(height);
-			}
-			if(poster.length()>0){
-				video.setPoster(poster);
-			}
-			widget = video.asWidget();
-		}
-		else{
-			AudioImpl audio = GWT.create(AudioImpl.class);
-			html = audio.getHTML(src);
-			widget = new HTML(html);
+		Element template = XMLUtils.getFirstElementWithTagName(element, "template");
+		if (type.startsWith("video")) {
+			createVideo(element, template != null);
+		} else {
+			createAudio(element, template != null);
 		}
 
-		containerPanel = new FlowPanel();
-		containerPanel.setStyleName("qp-object-container");
 		String cls = element.getAttribute("class");
-		if (cls != null  &&  !"".equals(cls))
-			containerPanel.addStyleName(cls);
+		if (cls != null && !"".equals(cls))
+			moduleView.getContainerPanel().addStyleName(cls);
 
-		titlePanel = new FlowPanel();
-		titlePanel.setStyleName("qp-object-title");
-
-		contentPanel = new FlowPanel();
-		contentPanel.setStyleName("qp-object-content");
 		if (widget != null)
-			contentPanel.add(widget);
+			moduleView.getContainerPanel().add(widget);
 
-		descriptionPanel = new FlowPanel();
-		descriptionPanel.setStyleName("qp-object-description");
-
-		containerPanel.add(contentPanel);
-		containerPanel.add(titlePanel);
-		containerPanel.add(descriptionPanel);
-
+		if (template != null && media!=null) {
+			parseTemplate(template, moduleView.getContainerPanel());
+		}
 		NodeList titleNodes = element.getElementsByTagName("title");
-		if (titleNodes.getLength() > 0){
+		if (titleNodes.getLength() > 0) {
 			Widget titleWidget = getModuleSocket().getInlineBodyGeneratorSocket().generateInlineBody(titleNodes.item(0));
-			if (titleWidget != null){
-				titlePanel.add(titleWidget);
+			if (titleWidget != null) {
+				moduleView.getTitlePanel().add(titleWidget);
 			}
 		}
 		NodeList descriptionNodes = element.getElementsByTagName("description");
-		if (descriptionNodes.getLength() > 0){
+		if (descriptionNodes.getLength() > 0) {
 			Widget descriptionWidget = getModuleSocket().getInlineBodyGeneratorSocket().generateInlineBody(descriptionNodes.item(0));
-			if (descriptionWidget != null){
-				descriptionPanel.add(descriptionWidget);
+			if (descriptionWidget != null) {
+				moduleView.getDescriptionPanel().add(descriptionWidget);
 			}
 		}
-
 	}
+
 	@Override
 	public ObjectModule getNewInstance() {
 		return new ObjectModule();
---
+++ src/eu/ydp/empiria/player/client/module/object/ObjectModuleView.java
@@ -0,0 +1,54 @@
+package eu.ydp.empiria.player.client.module.object;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * Widok dla {@link ObjectModule}
+ * @author plelakowski
+ *
+ */
+public class ObjectModuleView extends Composite {
+
+	private static ObjectModuleViewUiBinder uiBinder = GWT.create(ObjectModuleViewUiBinder.class);
+
+	interface ObjectModuleViewUiBinder extends UiBinder<Widget, ObjectModuleView> {
+	}
+
+	@UiField
+	protected FlowPanel containerPanel;
+
+	@UiField
+	protected FlowPanel contentPanel;
+
+	@UiField
+	protected FlowPanel titlePanel;
+
+	@UiField
+	protected FlowPanel descriptionPanel;
+
+	public ObjectModuleView() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	public FlowPanel getContainerPanel() {
+		return containerPanel;
+	}
+
+	public FlowPanel getContentPanel() {
+		return contentPanel;
+	}
+
+	public FlowPanel getTitlePanel() {
+		return titlePanel;
+	}
+
+	public FlowPanel getDescriptionPanel() {
+		return descriptionPanel;
+	}
+
+}
