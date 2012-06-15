06d889a7d79aeef48395a0935cd9b61aa15ff477
pmurawski <pmurawski@ydp.com.pl>
2012-06-15 10:34:56 +0000

    Added debug console logger.
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@107510 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ empiria.player/src/eu/ydp/empiria/player/client/debug/Logger.java
@@ -0,0 +1,8 @@
+package eu.ydp.empiria.player.client.debug;
+
+
+public interface Logger {
+
+	public abstract void log(String text);
+
+}
---
+++ empiria.player/src/eu/ydp/empiria/player/client/debug/LoggerEmpty.java
@@ -0,0 +1,7 @@
+package eu.ydp.empiria.player.client.debug;
+
+public class LoggerEmpty implements Logger {
+		
+	public void log(String text){}
+
+}
