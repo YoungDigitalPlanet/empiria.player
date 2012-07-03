74ea6bb08a57923334cdabc16db5eb09fc4e8321
plelakowski <plelakowski@ydp.com.pl>
2012-07-03 11:49:30 +0000

    sound processor + eventbus and some media player bug fixes bug id: 65658
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@109983 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/module/media/html5/HTML5MediaAvailableOptions.java
@@ -0,0 +1,58 @@
+package eu.ydp.empiria.player.client.module.media.html5;
+
+import eu.ydp.empiria.player.client.module.media.MediaAvailableOptions;
+import eu.ydp.gwtutil.client.util.UserAgentChecker;
+import eu.ydp.gwtutil.client.util.UserAgentChecker.MobileUserAgent;
+
+public class HTML5MediaAvailableOptions implements MediaAvailableOptions {
+
+	@Override
+	public boolean isPlaySupported() {
+		return true;
+	}
+
+	@Override
+	public boolean isPauseSupported() {
+		return true;
+	}
+
+	@Override
+	public boolean isMuteSupported() {
+		return isVolumeChangeSupported();
+	}
+
+	@Override
+	public boolean isVolumeChangeSupported() {
+		//mobilna i inna niz ff i chrome bo tam dziala
+		if (UserAgentChecker.getMobileUserAgent() != MobileUserAgent.UNKNOWN && !UserAgentChecker.isMobileUserAgent(MobileUserAgent.FIREFOX, MobileUserAgent.CHROME)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean isStopSupported() {
+		return true;
+	}
+
+	@Override
+	public boolean isSeekSupported() {
+		return true;
+	}
+
+	@Override
+	public boolean isFullScreenSupported() {
+		return false;
+	}
+
+	@Override
+	public boolean isMediaMetaAvailable() {
+		return true;
+	}
+
+	@Override
+	public boolean isTemplateSupported() {
+		return true;
+	}
+
+}
