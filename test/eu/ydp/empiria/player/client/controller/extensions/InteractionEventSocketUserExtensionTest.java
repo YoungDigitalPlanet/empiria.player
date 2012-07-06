1073ce49e194a2c7fb513f3d4af02957779dc0de
plelakowski <plelakowski@ydp.com.pl>
2012-07-06 08:51:01 +0000

    prepare to extend eventbus functionality bug 66688
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@110552 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/client/controller/extensions/InteractionEventSocketUserExtensionTest.java
+++ test/eu/ydp/empiria/player/client/controller/extensions/InteractionEventSocketUserExtensionTest.java
@@ -16,60 +16,65 @@ import eu.ydp.empiria.player.client.controller.extensions.internal.InternalExten
 import eu.ydp.empiria.player.client.controller.extensions.types.DeliveryEventsListenerExtension;
 import eu.ydp.empiria.player.client.controller.extensions.types.InteractionEventSocketUserExtension;
 import eu.ydp.empiria.player.client.controller.extensions.types.SoundProcessorExtension;
+import eu.ydp.empiria.player.client.module.HasParent;
 import eu.ydp.empiria.player.client.module.IUniqueModule;
 
 public class InteractionEventSocketUserExtensionTest extends ExtensionTestBase {
 
 	protected String test = "";
 	protected int counter = 0;
-	
+
 	protected DeliveryEngine de;
 	protected InteractionEventsListener iel;
-	
+
 	public void testStateChangedEvent(){
 		de = initDeliveryEngine(new MockInteractionStateChangedEventSocketUserExtension(), false);
 		test = "testStateChangedEvent";
 		iel.onStateChanged(new StateChangedInteractionEvent(true, new IUniqueModule() {
-			
+
 			@Override
 			public String getIdentifier() {
 				return "RESPONSE1";
 			}
+			@Override
+			public HasParent getParentModule() {
+				return null;
+			}
 		}));
-		assertEquals(1, counter);		
+		assertEquals(1, counter);
 	}
-	
+
 	public void testFeedbackInteractionSoundEvent(){
 		de = initDeliveryEngine(new MockInteractionSoundEventSocketUserExtension(), false);
 		test = "testFeedbackInteractionSoundEvent";
 		iel.onFeedbackSound(new FeedbackInteractionSoundEvent("http://www.ydp.eu./xxx.mp3"));
-		assertEquals(1, counter);		
+		assertEquals(1, counter);
 	}
-	
+
 	public void testMediaInteractionSoundEvent(){
 		de = initDeliveryEngine(new MockInteractionSoundEventSocketUserExtension(), false);
 		test = "testMediaInteractionSoundEvent";
 		iel.onMediaSound(new MediaInteractionSoundEvent("http://www.ydp.eu./xxx.mp3", new MediaInteractionSoundEventCallback() {
-			
+
 			@Override
 			public void setCallforward(MediaInteractionSoundEventCallforward callforward) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void onStop() {
 				counter++;
 			}
-			
+
 			@Override
 			public void onPlay() {
 				counter++;
 			}
 		}));
-		assertEquals(3, counter);		
+		assertEquals(3, counter);
 	}
-	
+
 	protected void onDeliveryEventMain(DeliveryEvent deliveryEvent){
 		if (test.equals("testStateChangedEvent")){
 			assertEquals(DeliveryEventType.STATE_CHANGED, deliveryEvent.getType());
@@ -99,15 +104,15 @@ public class InteractionEventSocketUserExtensionTest extends ExtensionTestBase {
 		}
 	}
 
-	
+
 	protected class MockInteractionEventSocketUserExtension extends InternalExtension implements DeliveryEventsListener, InteractionEventSocketUserExtension{
 
 		@Override
 		public void init() {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
+
 		@Override
 		public void onDeliveryEvent(DeliveryEvent deliveryEvent) {
 			onDeliveryEventMain(deliveryEvent);
@@ -117,13 +122,13 @@ public class InteractionEventSocketUserExtensionTest extends ExtensionTestBase {
 		public void setInteractionEventsListener(InteractionEventsListener listener) {
 			iel = listener;
 		}
-		
+
 	}
-	
+
 	protected class MockInteractionStateChangedEventSocketUserExtension extends MockInteractionEventSocketUserExtension implements DeliveryEventsListenerExtension{
-	
+
 	}
-	
+
 	protected class MockInteractionSoundEventSocketUserExtension extends MockInteractionEventSocketUserExtension implements SoundProcessorExtension{
 
 	}
