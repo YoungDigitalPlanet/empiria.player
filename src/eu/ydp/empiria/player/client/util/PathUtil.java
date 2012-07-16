2584a88726efc49034dff4f89a420e1de7aa0e1b
rrybacki <rrybacki@ydp.com.pl>
2012-07-16 11:52:53 +0000

    fix path resolving bug 67180
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@111476 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/util/PathUtil.java
+++ src/eu/ydp/empiria/player/client/util/PathUtil.java
@@ -17,29 +17,10 @@ public class PathUtil {
 		
 	}
 	
-	private static String playerPathDir;
-	
 	public static String getPlayerPathDir(){
-		if (playerPathDir != null)
-			return playerPathDir;
-		playerPathDir = findPlayerPathDir();
-		return playerPathDir;
+		return GWT.getModuleBaseURL();
 	}
-	
-	private static String findPlayerPathDir(){
-		NodeList<Element> scriptNodes = Document.get().getElementsByTagName("script");
-		String empiriaPlayerFileName = "/empiria.player.nocache.js";
-		for (int s = 0 ; s < scriptNodes.getLength() ; s ++){
-			if (((Element)scriptNodes.getItem(s)).hasAttribute("src")){
-				String src = ((Element)scriptNodes.getItem(s)).getAttribute("src");				
-				if (src.endsWith(empiriaPlayerFileName)){
-					return src.substring(0, src.indexOf(empiriaPlayerFileName) +1);
-				}
-			}
-		}
-		return "";		
-	}
-	
+		
 	public static String normalizePath(String path){
 		while (path.matches(".*\\\\[^\\\\]*\\\\[.]{2}.*")){
 			path = path.replaceAll("\\\\[^\\\\]*\\\\[.]{2}", "");
