24a2e3762c867736b9f61ccfc6824293ff259ad1
plelakowski <plelakowski@ydp.com.pl>
2012-07-18 13:08:25 +0000

    style name constants bug 67459
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@111828 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ empiria.player/src/eu/ydp/empiria/player/client/resources/StyleNameConstants.java
@@ -0,0 +1,62 @@
+package eu.ydp.empiria.player.client.resources;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface StyleNameConstants extends Constants {
+	@DefaultStringValue("qp-page-title")
+	public String QP_PAGE_TITLE();
+
+	@DefaultStringValue("qp-page-content")
+	public String QP_PAGE_CONTENT();
+
+	@DefaultStringValue("qp-header")
+	public String QP_HEADER();
+
+	@DefaultStringValue("qp-body")
+	public String QP_BODY();
+
+	@DefaultStringValue("qp-footer")
+	public String QP_FOOTER();
+
+	@DefaultStringValue("qp-page-item")
+	public String QP_PAGE_ITEM();
+
+	@DefaultStringValue("qp-toc-item-title")
+	public String QP_TOC_ITEM_TITLE();
+
+	@DefaultStringValue("qp-toc-item-title-hover")
+	public String QP_TOC_ITEM_TITLE_HOVER();
+
+	@DefaultStringValue("qp-resultpage-items")
+	public String QP_RESULTPAGE_ITEMS();
+
+	@DefaultStringValue("qp-toc-items")
+	public String QP_TOC_ITEMS();
+
+	@DefaultStringValue("qp-summary")
+	public String QP_SUMMARY();
+
+	@DefaultStringValue("qp-resultpage-points")
+	public String QP_RESULTPAGE_POINTS();
+
+	@DefaultStringValue("qp-resultpage-percents")
+	public String QP_RESULTPAGE_PERCENTS();
+
+	@DefaultStringValue("qp-toc-title")
+	public String QP_TOC_TITLE();
+
+	@DefaultStringValue("qp-toc")
+	public String QP_TOC();
+
+	@DefaultStringValue("qp-page-error-text")
+	public String QP_PAGE_ERROR_TEXT();
+
+	@DefaultStringValue("qp-resultpage-time")
+	public String QP_RESULTPAGE_TIME();
+
+	@DefaultStringValue("qp-resultpage-score")
+	public String QP_RESULTPAGE_SCORE();
+
+	@DefaultStringValue("qp-page-error")
+	public String QP_PAGE_ERROR();
+}
