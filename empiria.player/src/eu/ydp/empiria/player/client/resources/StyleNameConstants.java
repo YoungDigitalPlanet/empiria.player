e16f40a1a21e97f91fc50aa45b84074116b4ebd4
plelakowski <plelakowski@ydp.com.pl>
2012-07-18 16:31:23 +0000

    style name constants update bug 67459
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@111860 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- empiria.player/src/eu/ydp/empiria/player/client/resources/StyleNameConstants.java
+++ empiria.player/src/eu/ydp/empiria/player/client/resources/StyleNameConstants.java
@@ -3,23 +3,125 @@ package eu.ydp.empiria.player.client.resources;
 import com.google.gwt.i18n.client.Constants;
 
 public interface StyleNameConstants extends Constants {
-	@DefaultStringValue("qp-page-title")
-	public String QP_PAGE_TITLE();
-
-	@DefaultStringValue("qp-page-content")
-	public String QP_PAGE_CONTENT();
-
-	@DefaultStringValue("qp-header")
-	public String QP_HEADER();
-
 	@DefaultStringValue("qp-body")
 	public String QP_BODY();
 
 	@DefaultStringValue("qp-footer")
 	public String QP_FOOTER();
 
+	@DefaultStringValue("qp-header")
+	public String QP_HEADER();
+
+	@DefaultStringValue("qp-img")
+	public String QP_IMG();
+
+	@DefaultStringValue("qp-img-container")
+	public String QP_IMG_CONTAINER();
+
+	@DefaultStringValue("qp-img-content")
+	public String QP_IMG_CONTENT();
+
+	@DefaultStringValue("qp-img-default")
+	public String QP_IMG_DEFAULT();
+
+	@DefaultStringValue("qp-img-description")
+	public String QP_IMG_DESCRIPTION();
+
+	@DefaultStringValue("qp-img-explorable")
+	public String QP_IMG_EXPLORABLE();
+
+	@DefaultStringValue("qp-img-explorable-image-canvas")
+	public String QP_IMG_EXPLORABLE_IMAGE_CANVAS();
+
+	@DefaultStringValue("qp-img-explorable-image-canvas-panel")
+	public String QP_IMG_EXPLORABLE_IMAGE_CANVAS_PANEL();
+
+	@DefaultStringValue("qp-img-explorable-image-img-img")
+	public String QP_IMG_EXPLORABLE_IMAGE_IMG_IMG();
+
+	@DefaultStringValue("qp-img-explorable-image-img-panel")
+	public String QP_IMG_EXPLORABLE_IMAGE_IMG_PANEL();
+
+	@DefaultStringValue("qp-img-explorable-image-img-window")
+	public String QP_IMG_EXPLORABLE_IMAGE_IMG_WINDOW();
+
+	@DefaultStringValue("qp-img-explorable-image-panel")
+	public String QP_IMG_EXPLORABLE_IMAGE_PANEL();
+
+	@DefaultStringValue("qp-img-explorable-toolbox")
+	public String QP_IMG_EXPLORABLE_TOOLBOX();
+
+	@DefaultStringValue("qp-img-explorable-window")
+	public String QP_IMG_EXPLORABLE_WINDOW();
+
+	@DefaultStringValue("qp-img-explorable-zoomin")
+	public String QP_IMG_EXPLORABLE_ZOOMIN();
+
+	@DefaultStringValue("qp-img-explorable-zoomout")
+	public String QP_IMG_EXPLORABLE_ZOOMOUT();
+
+	@DefaultStringValue("qp-img-labelled-canvas")
+	public String QP_IMG_LABELLED_CANVAS();
+
+	@DefaultStringValue("qp-img-labelled-image")
+	public String QP_IMG_LABELLED_IMAGE();
+
+	@DefaultStringValue("qp-img-labelled-panel")
+	public String QP_IMG_LABELLED_PANEL();
+
+	@DefaultStringValue("qp-img-labelled-text")
+	public String QP_IMG_LABELLED_TEXT();
+
+	@DefaultStringValue("qp-img-labelled-text-panel")
+	public String QP_IMG_LABELLED_TEXT_PANEL();
+
+	@DefaultStringValue("qp-img-title")
+	public String QP_IMG_TITLE();
+
+	@DefaultStringValue("qp-media-description")
+	public String QP_MEDIA_DESCRIPTION();
+
+	@DefaultStringValue("qp-media-fullscreen-button")
+	public String QP_MEDIA_FULLSCREEN_BUTTON();
+
+	@DefaultStringValue("qp-media-title")
+	public String QP_MEDIA_TITLE();
+
+	@DefaultStringValue("qp-page-content")
+	public String QP_PAGE_CONTENT();
+
+	@DefaultStringValue("qp-page-error")
+	public String QP_PAGE_ERROR();
+
+	@DefaultStringValue("qp-page-error-text")
+	public String QP_PAGE_ERROR_TEXT();
+
 	@DefaultStringValue("qp-page-item")
 	public String QP_PAGE_ITEM();
+
+	@DefaultStringValue("qp-page-title")
+	public String QP_PAGE_TITLE();
+
+	@DefaultStringValue("qp-resultpage-items")
+	public String QP_RESULTPAGE_ITEMS();
+
+	@DefaultStringValue("qp-resultpage-percents")
+	public String QP_RESULTPAGE_PERCENTS();
+
+	@DefaultStringValue("qp-resultpage-points")
+	public String QP_RESULTPAGE_POINTS();
+
+	@DefaultStringValue("qp-resultpage-score")
+	public String QP_RESULTPAGE_SCORE();
+
+	@DefaultStringValue("qp-resultpage-time")
+	public String QP_RESULTPAGE_TIME();
+
+	@DefaultStringValue("qp-summary")
+	public String QP_SUMMARY();
+
+	@DefaultStringValue("qp-toc")
+	public String QP_TOC();
 
 	@DefaultStringValue("qp-toc-item-title")
 	public String QP_TOC_ITEM_TITLE();
@@ -27,36 +129,10 @@ public interface StyleNameConstants extends Constants {
 	@DefaultStringValue("qp-toc-item-title-hover")
 	public String QP_TOC_ITEM_TITLE_HOVER();
 
-	@DefaultStringValue("qp-resultpage-items")
-	public String QP_RESULTPAGE_ITEMS();
-
 	@DefaultStringValue("qp-toc-items")
 	public String QP_TOC_ITEMS();
-
-	@DefaultStringValue("qp-summary")
-	public String QP_SUMMARY();
-
-	@DefaultStringValue("qp-resultpage-points")
-	public String QP_RESULTPAGE_POINTS();
-
-	@DefaultStringValue("qp-resultpage-percents")
-	public String QP_RESULTPAGE_PERCENTS();
 
 	@DefaultStringValue("qp-toc-title")
 	public String QP_TOC_TITLE();
 
-	@DefaultStringValue("qp-toc")
-	public String QP_TOC();
-
-	@DefaultStringValue("qp-page-error-text")
-	public String QP_PAGE_ERROR_TEXT();
-
-	@DefaultStringValue("qp-resultpage-time")
-	public String QP_RESULTPAGE_TIME();
-
-	@DefaultStringValue("qp-resultpage-score")
-	public String QP_RESULTPAGE_SCORE();
-
-	@DefaultStringValue("qp-page-error")
-	public String QP_PAGE_ERROR();
 }
