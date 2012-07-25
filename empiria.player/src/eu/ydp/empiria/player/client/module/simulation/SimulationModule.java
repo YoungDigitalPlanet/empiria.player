f4490978ed4571c347978c3b2cd365f44d774223
mkaldonek <mkaldonek@ydp.com.pl>
2012-07-25 12:01:31 +0000

    unloading simulation onClose
    bug 65448
    
    
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@112588 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- empiria.player/src/eu/ydp/empiria/player/client/module/simulation/SimulationModule.java
+++ empiria.player/src/eu/ydp/empiria/player/client/module/simulation/SimulationModule.java
@@ -5,13 +5,16 @@ import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.xml.client.Element;
 
 import eu.ydp.empiria.player.client.module.Factory;
+import eu.ydp.empiria.player.client.module.ILifecycleModule;
 import eu.ydp.empiria.player.client.module.SimpleModuleBase;
 import eu.ydp.gwtcreatejs.client.handler.CompleteHandler;
 import eu.ydp.gwtcreatejs.client.loader.CreateJsLoader;
 
-public class SimulationModule extends SimpleModuleBase implements Factory<SimulationModule> {
+public class SimulationModule extends SimpleModuleBase implements Factory<SimulationModule>, ILifecycleModule {
 
 	protected SimulationModuleView view;
+	
+	protected CreateJsLoader loader;
 
 	@Override
 	public SimulationModule getNewInstance() {
@@ -25,21 +28,50 @@ public class SimulationModule extends SimpleModuleBase implements Factory<Simula
 		}
 		return view;
 	}
+	
+	@Override
+	public void onBodyLoad() {
+				
+	}
+
+	@Override
+	public void onBodyUnload() {
+				
+	}
+
+	@Override
+	public void onSetUp() {
+				
+	}
+
+	@Override
+	public void onStart() {
+				
+	}
+
+	@Override
+	public void onClose() {
+		if(loader != null)
+			loader.unload();
+	}
 
 	@Override
 	protected void initModule(Element element) {
 		String src = element.getAttribute("src");
-		final CreateJsLoader loader = new CreateJsLoader();
-
+		
+		initializeLoader();
+		loader.load(src);
+	}
+	
+	protected void initializeLoader(){
+		loader = new CreateJsLoader();
 		loader.addCompleteHandler(new CompleteHandler() {
-
+			
 			@Override
 			public void onComplete() {
 				addChildView(loader.getContent().getCanvas());
 			}
 		});
-
-		loader.load(src);
 	}
 
 	protected void addChildView(IsWidget child) {
