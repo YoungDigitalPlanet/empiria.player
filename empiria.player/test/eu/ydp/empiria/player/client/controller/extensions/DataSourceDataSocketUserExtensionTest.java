0ba4e15249959662d3954abdda517c5b1a5edd6b
rrybacki <rrybacki@ydp.com.pl>
2012-07-26 09:40:13 +0000

    before flow event notification for fixing focus out on mobiles bug 68044
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@112717 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- empiria.player/test/eu/ydp/empiria/player/client/controller/extensions/DataSourceDataSocketUserExtensionTest.java
+++ empiria.player/test/eu/ydp/empiria/player/client/controller/extensions/DataSourceDataSocketUserExtensionTest.java
@@ -1,20 +1,9 @@
 package eu.ydp.empiria.player.client.controller.extensions;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.junit.client.GWTTestCase;
-import com.google.gwt.xml.client.Document;
-import com.google.gwt.xml.client.XMLParser;
-
-import eu.ydp.empiria.player.client.PlayerGinjector;
-import eu.ydp.empiria.player.client.controller.communication.ActivityMode;
-import eu.ydp.empiria.player.client.controller.communication.FlowOptions;
-import eu.ydp.empiria.player.client.controller.communication.PageItemsDisplayMode;
 import eu.ydp.empiria.player.client.controller.data.DataSourceDataSupplier;
 import eu.ydp.empiria.player.client.controller.delivery.DeliveryEngine;
 import eu.ydp.empiria.player.client.controller.extensions.internal.InternalExtension;
 import eu.ydp.empiria.player.client.controller.extensions.types.DataSourceDataSocketUserExtension;
-import eu.ydp.empiria.player.client.util.file.xml.XmlData;
 
 public class DataSourceDataSocketUserExtensionTest extends ExtensionTestBase {
 
