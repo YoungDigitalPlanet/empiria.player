8362d65d2951b9aa3bb4f4c80e6d1eb4ac3c9ba8
plelakowski <plelakowski@ydp.com.pl>
2012-08-03 08:43:55 +0000

    code refactoring for swipe page changing bug 66688
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@113662 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/resources/EmpiriaStyleNameConstants.java
+++ src/eu/ydp/empiria/player/client/resources/EmpiriaStyleNameConstants.java
@@ -8,7 +8,9 @@ public final class EmpiriaStyleNameConstants {
 	public static final String EMPIRIA_IMG_EXPLORABLE_WINDOW_HEIGHT = "-empiria-img-explorable-window-height";
 	public static final String EMPIRIA_IMG_EXPLORABLE_WINDOW_WIDTH = "-empiria-img-explorable-window-width";
 	public static final String EMPIRIA_IMG_EXPLORABLE_SCALE_INITIAL = "-empiria-img-explorable-scale-initial";
-
+	public static final String EMPIRIA_INFO_CONTENT = "-empiria-info-content";
+	public static final String EMPIRIA_REPORT_SHOW_NON_ACTIVITES = "-empiria-report-show-non-activites";
+	public static final String EMPIRIA_REPORT_ITEMS_INCLUDE = "-empiria-report-items-include";
 	private EmpiriaStyleNameConstants() {
 	}
 }
