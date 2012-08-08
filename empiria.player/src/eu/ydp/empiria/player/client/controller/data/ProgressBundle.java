21416118e4dc3687ee8fd95d987447e065e39bad
pmurawski <pmurawski@ydp.com.pl>
2012-08-08 08:56:00 +0000

    Added preloader bundle.
    bug 67931
    
    
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@114171 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ empiria.player/src/eu/ydp/empiria/player/client/controller/data/ProgressBundle.java
@@ -0,0 +1,10 @@
+package eu.ydp.empiria.player.client.controller.data;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface ProgressBundle extends ClientBundle {
+
+	@Source("preloader.gif")
+	public ImageResource getProgressImage();
+}
