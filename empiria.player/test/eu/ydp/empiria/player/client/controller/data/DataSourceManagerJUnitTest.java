b2452c9eb60bb57546815cc626d5ef9803b3f952
pmurawski <pmurawski@ydp.com.pl>
2012-08-08 07:16:02 +0000

    Added preloader at the start of Empiria and Mockito framework for mocking objects in tests. bug 67931
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@114140 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ empiria.player/test/eu/ydp/empiria/player/client/controller/data/DataSourceManagerJUnitTest.java
@@ -0,0 +1,29 @@
+package eu.ydp.empiria.player.client.controller.data;
+
+import static org.mockito.Mockito.*;
+
+import com.google.gwt.user.client.ui.InsertPanel.ForIsWidget;
+import com.google.gwt.user.client.ui.IsWidget;
+
+import eu.ydp.empiria.player.client.controller.data.events.DataLoaderEventListener;
+
+import junit.framework.TestCase;
+
+public class DataSourceManagerJUnitTest extends TestCase{
+	
+	public void testMainPreloaderAddedAndRemoved() {
+		DataSourceManager dsm = new DataSourceManager();
+		
+		ForIsWidget mockRootPanel = mock(ForIsWidget.class);
+		IsWidget mockImage = mock(IsWidget.class);
+		
+		dsm.loadMainDocument("", mockRootPanel, false, mockImage);
+		verify(mockRootPanel).add(any(IsWidget.class));
+		
+		DataLoaderEventListener mockListener = mock(DataLoaderEventListener.class);
+		dsm.setDataLoaderEventListener(mockListener);
+		
+		dsm.onLoadFinished(mockImage, mockRootPanel);
+		verify(mockRootPanel).remove(any(int.class));
+	}
+}
