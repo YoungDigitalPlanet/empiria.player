ddf69c6da0e9814e21a83a29e4d20d5c94fcd2a0
pmurawski <pmurawski@ydp.com.pl>
2012-08-27 09:38:13 +0000

    Feedback sounds in gaps on iPad. bug 67443
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@116052 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/controller/extensions/internal/sound/SoundExecutorHtml5.java
+++ src/eu/ydp/empiria/player/client/controller/extensions/internal/sound/SoundExecutorHtml5.java
@@ -3,24 +3,31 @@ package eu.ydp.empiria.player.client.controller.extensions.internal.sound;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.EndedEvent;
 import com.google.gwt.event.dom.client.EndedHandler;
+import com.google.gwt.event.dom.client.TouchStartEvent;
+import com.google.gwt.event.dom.client.TouchStartHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.media.client.Audio;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import eu.ydp.empiria.player.client.module.media.BaseMediaConfiguration;
 import eu.ydp.empiria.player.client.module.media.MediaWrapper;
+
 @Deprecated
-public class SoundExecutorHtml5 implements SoundExecutor<Widget> {
+public class SoundExecutorHtml5 implements SoundExecutor<Widget>, TouchStartHandler {
 
 	protected Audio audioCtrl;
 	protected boolean playing = false;
 	protected SoundExecutorListener listener;
 	private BaseMediaConfiguration baseMediaConfiguration;
+	private HandlerRegistration touchHandlerRegistration;
 
 	public SoundExecutorHtml5(){
 		audioCtrl = Audio.createIfSupported();
 		RootPanel.get().add( audioCtrl );
 		audioCtrl.setVisible(false);
+		simulateUserInteraction();
+		
 		if (audioCtrl != null){
 			addEndedListener(audioCtrl.getElement());
 			audioCtrl.addEndedHandler(new EndedHandler(){
@@ -50,7 +57,8 @@ public class SoundExecutorHtml5 implements SoundExecutor<Widget> {
 
 	@Override
 	public void play(String src) {
-		if (audioCtrl != null){
+		
+		if (audioCtrl != null) {
 			if (playing)
 				stop();
 			listener.onPlay();
@@ -64,6 +72,20 @@ public class SoundExecutorHtml5 implements SoundExecutor<Widget> {
 	public void stop() {
 		audioCtrl.pause();
 		onEnded2();
+	}
+	
+	/** Ta metoda jest potrzebna aby obejsc problemy z odtwarzaniem dzwieku na iOS. Apple w swej madrosci nie pozwolilo na odtwarzanie dzwiekow
+	 * dopoki nie zostanie odtworzony jakikolwiek dzwiek w reakcji na dzialanie uzytkownika. */
+	protected void simulateUserInteraction() {
+		RootPanel root = RootPanel.get();
+		touchHandlerRegistration = root.addDomHandler(this, TouchStartEvent.getType());
+	}
+	
+	@Override
+	public void onTouchStart(TouchStartEvent event) {
+		touchHandlerRegistration.removeHandler();
+		audioCtrl.play();
+		audioCtrl.pause();
 	}
 
 	@Override
