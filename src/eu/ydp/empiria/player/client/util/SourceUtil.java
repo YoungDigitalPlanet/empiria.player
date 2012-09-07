4612346327edd6aa2155efb9ff4bce873816dc01
plelakowski <plelakowski@ydp.com.pl>
2012-09-07 06:28:14 +0000

    Sourceutil pmd refactoring + tests
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@117215 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/util/SourceUtil.java
+++ src/eu/ydp/empiria/player/client/util/SourceUtil.java
@@ -2,11 +2,14 @@ package eu.ydp.empiria.player.client.util;
 
 import java.util.Map;
 
-public class SourceUtil {
+public final class SourceUtil {
+	private SourceUtil() {
+	}
+
 	public static String getMpegSource(Map<String, String> sources) {
 		for (Map.Entry<String, String> src : sources.entrySet()) {
 			if (src.getValue().matches(".*mpeg|.*mp4|.*mp3")) {
-				return src.getKey();
+				return src.getKey(); // NOPMD
 			}
 		}
 		return null;
