6b043bbbba1197b63b7476e4d70131a23e303cb3
plelakowski <plelakowski@ydp.com.pl>
2012-09-19 10:31:29 +0000

    template parser test bug 69873
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@118372 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/client/TestGuiceModule.java
+++ test/eu/ydp/empiria/player/client/TestGuiceModule.java
@@ -1,16 +1,29 @@
 package eu.ydp.empiria.player.client;
 
+import static org.mockito.Mockito.mock;
+
+import com.google.inject.Provides;
 import com.google.inject.Singleton;
 
+import eu.ydp.empiria.player.client.controller.multiview.PanelCache;
+import eu.ydp.empiria.player.client.module.media.MediaControllerFactory;
+import eu.ydp.empiria.player.client.module.media.fullscreen.VideoFullScreenHelper;
+import eu.ydp.empiria.player.client.module.object.template.ObjectTemplateParser;
+import eu.ydp.empiria.player.client.resources.StyleNameConstants;
 import eu.ydp.empiria.player.client.util.SchedulerImpl;
 import eu.ydp.empiria.player.client.util.events.bus.EventsBus;
 import eu.ydp.empiria.player.client.util.events.bus.PlayerEventsBus;
 import eu.ydp.empiria.player.client.util.scheduler.Scheduler;
+import eu.ydp.gwtutil.client.ui.GWTPanelFactory;
+import eu.ydp.gwtutil.junit.mock.GWTConstantsMock;
 import eu.ydp.gwtutil.test.AbstractTestModule;
+import eu.ydp.gwtutil.test.mock.GwtPanelFactoryMock;
 
-public class TestGuiceModule extends AbstractTestModule{
+public class TestGuiceModule extends AbstractTestModule {
+	public TestGuiceModule() {
+	}
 
-	public TestGuiceModule(Class<?>... classToOmit){
+	public TestGuiceModule(Class<?>... classToOmit) {
 		super(classToOmit);
 	}
 
@@ -18,6 +31,33 @@ public class TestGuiceModule extends AbstractTestModule{
 	public void configure() {
 		bind(Scheduler.class).to(SchedulerImpl.class).in(Singleton.class);
 		bind(EventsBus.class).to(PlayerEventsBus.class).in(Singleton.class);
+	//	bind(GWTPanelFactory.class).to(GwtPanelFactoryMock.class).in(Singleton.class);
+		bind(VideoFullScreenHelper.class).in(Singleton.class);
+		bind(PanelCache.class);
+	}
+
+	@Provides
+	public StyleNameConstants getNameConstants() {
+		return GWTConstantsMock.mockAllStringMethods(mock(StyleNameConstants.class));
+	}
+
+	@Provides
+	public MediaControllerFactory getMediaControllerFactory() {
+		MediaControllerFactory factory = mock(MediaControllerFactory.class);
+		return factory;
+	}
+
+
+	@Provides
+	public ObjectTemplateParser getObjectTemplateParser(MediaControllerFactory factory) {
+		ObjectTemplateParser<?> parser = new ObjectTemplateParser();
+		parser.setFactory(factory);
+		return parser;
+	}
+
+	@Provides
+	public GWTPanelFactory gwtPanelFactory(){
+		return new GwtPanelFactoryMock();
 	}
 
 }
