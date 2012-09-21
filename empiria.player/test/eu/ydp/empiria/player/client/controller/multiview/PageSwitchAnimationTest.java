6320f8b97a192e5451c27eccc800d42466774aa8
plelakowski <plelakowski@ydp.com.pl>
2012-09-21 09:59:58 +0000

    test for page switch animation and remove unused class bug 71512
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@118632 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- empiria.player/test/eu/ydp/empiria/player/client/controller/multiview/PageSwitchAnimationTest.java
+++ empiria.player/test/eu/ydp/empiria/player/client/controller/multiview/PageSwitchAnimationTest.java
@@ -1,8 +1,11 @@
 package eu.ydp.empiria.player.client.controller.multiview;
 
 import com.google.gwt.dom.client.Style.Position;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.user.client.ui.FlowPanel;
+
+import eu.ydp.empiria.player.client.controller.multiview.animation.PageSwitchAnimation;
 
 @SuppressWarnings("PMD")
 public class PageSwitchAnimationTest extends GWTTestCase {
@@ -10,7 +13,8 @@ public class PageSwitchAnimationTest extends GWTTestCase {
 	public void testAnimation() {
 		final FlowPanel panel = new FlowPanel();
 		panel.getElement().getStyle().setPosition(Position.ABSOLUTE);
-		PageSwitchAnimation animation = new PageSwitchAnimation(panel, 100, 200){
+		panel.getElement().getStyle().setLeft(100, Unit.PCT);
+		PageSwitchAnimation animation = new PageSwitchAnimation(panel, 200){
 			@Override
 			protected void onComplete() {
 				super.onComplete();
