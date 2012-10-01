39ed96053125398aa0a79486fe7d08012515d36e
pmurawski <pmurawski@ydp.com.pl>
2012-10-01 11:24:33 +0000

    Test for math text width measurement. bug 72214
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk@119522 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ empiria.player/test/com/mathplayer/player/model/TokenTest.java
@@ -0,0 +1,61 @@
+package com.mathplayer.player.model;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import gwt.g2d.client.graphics.Surface;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import com.google.gwt.junit.GWTMockUtilities;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.mathplayer.player.geom.Font;
+import com.mathplayer.player.geom.Size;
+import com.mathplayer.player.interaction.InteractionSocket;
+
+public class TokenTest extends TestCase {
+    @Test
+	public void testCanvasForTextWidthMeasureAddedAndRemoved() {
+    	GWTMockUtilities.disarm(); 
+		
+		Token token = new Token() {
+			@Override
+			public String toString() {
+				return null;
+			}
+			
+			@Override
+			public String toMathML() {
+				return null;
+			}
+			
+			@Override
+			public Size measure(InteractionSocket socket) {
+				return null;
+			}
+			
+			@Override
+			public Surface getCanvas() {
+				return mock(Surface.class);
+			}
+			
+			@Override
+			public Boolean isStackAndroidBrowser() {
+				return false;
+			}
+		};
+		
+		AbsolutePanel mockPanel = mock(AbsolutePanel.class);
+		
+		String content = "";
+		Font font = new Font(10, "Arial", false, false);
+		double margin = 0;
+		
+		token.getTextWidth(content, font, margin, mockPanel);
+		verify(mockPanel).add(any(Surface.class));
+		verify(mockPanel).remove(any(Surface.class));
+		
+		GWTMockUtilities.restore();
+	}
+}
