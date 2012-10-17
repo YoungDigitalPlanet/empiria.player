45212969f377a8f644d93b97d16bad98e9332da6
rrybacki <rrybacki@ydp.com.pl>
2012-10-17 11:40:46 +0000

    text entry max length bug 73371
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@121359 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/controller/extensions/internal/bookmark/BookmarkPopupContents.java
+++ src/eu/ydp/empiria/player/client/controller/extensions/internal/bookmark/BookmarkPopupContents.java
@@ -4,7 +4,6 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
-import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
