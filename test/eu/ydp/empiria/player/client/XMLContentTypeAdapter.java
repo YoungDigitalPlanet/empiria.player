1896a2e2c3e65580970128501756cbe9d6f1dc25
plelakowski <plelakowski@ydp.com.pl>
2012-10-23 06:55:01 +0000

    Xml adapter for mapping module xml to jaxb module attribute bug 72518
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@121920 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ test/eu/ydp/empiria/player/client/XMLContentTypeAdapter.java
@@ -0,0 +1,18 @@
+package eu.ydp.empiria.player.client;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import com.peterfranza.gwt.jaxb.client.parser.utils.XMLContent;
+
+public class XMLContentTypeAdapter extends XmlAdapter<String,  XMLContent> {// implements DomHandler<ElementT, Result> {
+
+	@Override
+	public XMLContent unmarshal(String v) throws Exception {
+		return null;
+	}
+
+	@Override
+	public String marshal(XMLContent v) throws Exception {
+		return null;
+	}
+}
