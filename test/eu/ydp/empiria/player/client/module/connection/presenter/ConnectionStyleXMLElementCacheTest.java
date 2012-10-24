548f81d7a5687f67c64ee4a40d15717612603813
plelakowski <plelakowski@ydp.com.pl>
2012-10-24 09:53:20 +0000

    Connection style cache test bug 72518
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@122114 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ test/eu/ydp/empiria/player/client/module/connection/presenter/ConnectionStyleXMLElementCacheTest.java
@@ -0,0 +1,56 @@
+package eu.ydp.empiria.player.client.module.connection.presenter;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.Element;
+
+import eu.ydp.empiria.player.client.module.components.multiplepair.MultiplePairModuleConnectType;
+import eu.ydp.empiria.player.client.resources.StyleNameConstants;
+import eu.ydp.gwtutil.client.xml.XMLParser;
+import eu.ydp.gwtutil.junit.mock.GWTConstantsMock;
+
+public class ConnectionStyleXMLElementCacheTest {
+
+	private ConnectionStyleXMLElementCache cache;
+	private StyleNameConstants styleNames;
+	private final Map<MultiplePairModuleConnectType, String> styles = new HashMap<MultiplePairModuleConnectType, String>();
+
+	@Before
+	public void before() {
+		XMLParser parser = mock(XMLParser.class);
+		styleNames = GWTConstantsMock.mockAllStringMethods(mock(StyleNameConstants.class), StyleNameConstants.class);
+		styles.put(MultiplePairModuleConnectType.WRONG, styleNames.QP_CONNECTION_WRONG());
+		styles.put(MultiplePairModuleConnectType.CORRECT, styleNames.QP_CONNECTION_CORRECT());
+		styles.put(MultiplePairModuleConnectType.NONE, styleNames.QP_CONNECTION_NONE());
+		styles.put(MultiplePairModuleConnectType.NORMAL, styleNames.QP_CONNECTION_NORMAL());
+		when(parser.parse(Mockito.anyString())).then(new Answer<Document>() {
+			@Override
+			public Document answer(InvocationOnMock invocation) throws Throwable {
+				return eu.ydp.gwtutil.xml.XMLParser.parse((String) invocation.getArguments()[0]);
+			}
+		});
+		cache = new ConnectionStyleXMLElementCache(styleNames, parser);
+	}
+
+	@Test
+	public void testGet() {
+		for (MultiplePairModuleConnectType type : MultiplePairModuleConnectType.values()) {
+			Element element = cache.get(type);
+			assertEquals(styles.get(type), element.getAttribute("class"));
+			assertEquals("connection", element.getNodeName());
+		}
+	}
+
+}
