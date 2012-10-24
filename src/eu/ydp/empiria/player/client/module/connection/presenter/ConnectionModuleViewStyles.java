feb719e0028afe7628b0aeb3002680d23e395de7
plelakowski <plelakowski@ydp.com.pl>
2012-10-24 09:50:17 +0000

    Connection style cache bug 72518
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@122110 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/module/connection/presenter/ConnectionModuleViewStyles.java
@@ -0,0 +1,22 @@
+package eu.ydp.empiria.player.client.module.connection.presenter;
+
+import java.util.Map;
+
+import eu.ydp.empiria.player.client.module.components.multiplepair.MultiplePairModuleConnectType;
+import eu.ydp.empiria.player.client.resources.StyleNameConstants;
+import eu.ydp.empiria.player.client.style.StyleSocket;
+import eu.ydp.gwtutil.client.xml.XMLParser;
+
+public class ConnectionModuleViewStyles {
+	private final StyleSocket styleSocket;
+	private final ConnectionStyleXMLElementCache cache;
+
+	public ConnectionModuleViewStyles(StyleSocket styleSocket, StyleNameConstants styleNames, XMLParser xmlParser) {
+		this.styleSocket = styleSocket;
+		cache = new ConnectionStyleXMLElementCache(styleNames, xmlParser);
+	}
+
+	public Map<String, String> getStyles(MultiplePairModuleConnectType type) {
+		return styleSocket.getStyles(cache.get(type));
+	}
+}
---
+++ src/eu/ydp/empiria/player/client/module/connection/presenter/ConnectionStyleXMLElementCache.java
@@ -0,0 +1,39 @@
+package eu.ydp.empiria.player.client.module.connection.presenter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gwt.xml.client.Element;
+
+import eu.ydp.empiria.player.client.module.components.multiplepair.MultiplePairModuleConnectType;
+import eu.ydp.empiria.player.client.resources.StyleNameConstants;
+import eu.ydp.empiria.player.client.view.player.AbstractCache;
+import eu.ydp.gwtutil.client.xml.XMLParser;
+
+public class ConnectionStyleXMLElementCache extends AbstractCache<MultiplePairModuleConnectType, Element> {
+
+	Map<MultiplePairModuleConnectType, String> styles = new HashMap<MultiplePairModuleConnectType, String>();
+	private final XMLParser xmlParser;
+
+	public ConnectionStyleXMLElementCache(StyleNameConstants styleNames, XMLParser xmlParser) {
+		styles.put(MultiplePairModuleConnectType.WRONG, styleNames.QP_CONNECTION_WRONG());
+		styles.put(MultiplePairModuleConnectType.CORRECT, styleNames.QP_CONNECTION_CORRECT());
+		styles.put(MultiplePairModuleConnectType.NONE, styleNames.QP_CONNECTION_NONE());
+		styles.put(MultiplePairModuleConnectType.NORMAL, styleNames.QP_CONNECTION_NORMAL());
+		this.xmlParser = xmlParser;
+	}
+
+	String template = "<root><connection class=\"$class\" /></root>";
+	private String getTemplate(String className){
+		return template.replaceAll("\\$class", className);
+	}
+	private Element getXMLElement(String className){
+		return (Element) xmlParser.parse(getTemplate(className)).getDocumentElement().getFirstChild();
+	}
+
+	@Override
+	protected Element getElement(MultiplePairModuleConnectType index) {
+		return getXMLElement(styles.get(index));
+	}
+
+}
