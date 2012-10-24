3dd52ee6302caf428e1e1a65532d71a7f6b73e79
plelakowski <plelakowski@ydp.com.pl>
2012-10-24 12:21:23 +0000

    restore matchinteraction bug 72518
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@122160 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/module/ModuleTagName.java
+++ src/eu/ydp/empiria/player/client/module/ModuleTagName.java
@@ -14,7 +14,7 @@ public enum ModuleTagName {
 	MEDIA_PROGRESS_BAR("mediaProgressBar"),MEDIA_VOLUME_BAR("mediaVolumeBar"),MEDIA_FULL_SCREEN_BUTTON("mediaFullScreenButton"),
 	MEDIA_POSITION_IN_STREAM("mediaPositinInStream"),MEDIA_CURRENT_TIME("mediaCurrentTime"),MEDIA_TOTAL_TIME("mediaTotalTime"),
 	MEDIA_TITLE("mediaTitle"), MEDIA_DESCRIPTION("mediaDescription"), MEDIA_SCREEN("mediaScreen"), SIMULATION_PLAYER("simulationPlayer"), MEDIA_TEXT_TRACK("mediaTextTrack"),
-	MATH_GAP_TEXT_ENTRY_TYPE("gap_text-entry"), MATH_GAP_INLINE_CHOICE_TYPE("gap_inline-choice");
+	MATH_GAP_TEXT_ENTRY_TYPE("gap_text-entry"), MATH_GAP_INLINE_CHOICE_TYPE("gap_inline-choice"),MATCH_INTERACTION("matchInteraction");
 	String name = null;
 	private ModuleTagName(String name){
 		this.name = name;
@@ -32,16 +32,16 @@ public enum ModuleTagName {
 		}
 		return null;
 	}
-	
+
 	public static String getTagNameWithType(String tagName, String type) {
 		String tagNameWithType = "";
-		
+
 		if ( tagName.equals("gap") && type.equals("text-entry") ) {
 			tagNameWithType = MATH_GAP_TEXT_ENTRY_TYPE.toString();
 		} else if ( tagName.equals("gap") && type.equals("inline-choice") ) {
 			tagNameWithType = MATH_GAP_INLINE_CHOICE_TYPE.toString();
 		}
-		
+
 		return tagNameWithType;
 	}
 
