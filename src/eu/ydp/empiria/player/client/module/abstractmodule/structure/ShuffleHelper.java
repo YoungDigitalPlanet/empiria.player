d2f356be29dace4a56f3264bde136c7c1e9b2923
plelakowski <plelakowski@ydp.com.pl>
2012-11-06 10:21:44 +0000

    shuffle helper bug 74194
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@123309 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/module/abstractmodule/structure/ShuffleHelper.java
@@ -0,0 +1,47 @@
+package eu.ydp.empiria.player.client.module.abstractmodule.structure;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import eu.ydp.gwtutil.client.collections.RandomizedSet;
+
+public class ShuffleHelper {
+
+	public <B extends ModuleBean & HasShuffle, I extends HasFixed> List<I> randomizeChoices(B bean, List<I> associableChoices) {
+		List<I> resultList = associableChoices;
+		if (bean.isShuffle()) {
+			resultList = new ArrayList<I>(associableChoices.size());
+			List<I> copyList = new ArrayList<I>(associableChoices);
+			Map<Integer, I> fixedMap = new TreeMap<Integer, I>();
+			TreeSet<Integer> reverseOrder = new TreeSet<Integer>(Collections.reverseOrder());
+			for (int x = 0; x < associableChoices.size(); ++x) {
+				I item = associableChoices.get(x);
+				if (item.isFixed()) {
+					fixedMap.put(x, item);
+					reverseOrder.add(x);
+				}
+			}
+
+
+			for (Integer index : reverseOrder) {
+				copyList.remove(index.intValue());
+			}
+
+			if (!copyList.isEmpty()) {
+				RandomizedSet<I> randomSet = new RandomizedSet<I>(copyList);
+				while (randomSet.hasMore()) {
+					resultList.add(randomSet.pull());
+				}
+			}
+			for (Map.Entry<Integer, I> fixedEntry : fixedMap.entrySet()) {
+				resultList.add(fixedEntry.getKey(), fixedEntry.getValue());
+			}
+
+		}
+		return resultList;
+	}
+}
---
+++ test/eu/ydp/empiria/player/client/module/abstractmodule/structure/ShuffleHelperTest.java
@@ -0,0 +1,128 @@
+package eu.ydp.empiria.player.client.module.abstractmodule.structure;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+@SuppressWarnings("PMD")
+public class ShuffleHelperTest {
+
+	private static class BeanItem implements HasFixed {
+		private final int index;
+		private final boolean shuffle;
+
+		public BeanItem(int index, boolean shuffle) {
+			this.index = index;
+			this.shuffle = shuffle;
+		}
+
+		@Override
+		public boolean isFixed() {
+			return shuffle;
+		}
+
+
+		@Override
+		public String toString() {
+			return "index=" + index + ", shuffle=" + shuffle;
+		}
+
+	}
+
+	public static class Bean implements HasShuffle, ModuleBean {
+		@Override
+		public boolean isShuffle() {
+			return false;
+		}
+	}
+
+	private final List<BeanItem> testList = new ArrayList<ShuffleHelperTest.BeanItem>();
+
+	@Before
+	public void before() {
+		for (int x = 0; x < 9; ++x) {
+			testList.add(new BeanItem(x, x % 2 == 0));
+		}
+	}
+
+	@Test
+	public void shuffleEmptyListTest() {
+		ShuffleHelper instance = new ShuffleHelper();
+		Bean bean = Mockito.mock(Bean.class);
+		when(bean.isShuffle()).thenReturn(true);
+		testList.clear();
+		List<BeanItem> randomizeChoices = instance.randomizeChoices(bean, testList);
+		assertTrue(testList.size() == randomizeChoices.size());
+	}
+
+	@Test
+	public void sizeShuffleTrueTest() {
+		ShuffleHelper instance = new ShuffleHelper();
+		Bean bean = Mockito.mock(Bean.class);
+		when(bean.isShuffle()).thenReturn(true);
+		List<BeanItem> randomizeChoices = instance.randomizeChoices(bean, testList);
+		assertTrue(testList.size() == randomizeChoices.size());
+	}
+
+	@Test
+	public void sizeShuffleFalseTest() {
+		ShuffleHelper instance = new ShuffleHelper();
+		Bean bean = Mockito.mock(Bean.class);
+		when(bean.isShuffle()).thenReturn(false);
+		List<BeanItem> randomizeChoices = instance.randomizeChoices(bean, testList);
+		assertTrue(testList.size() == randomizeChoices.size());
+	}
+
+	@Test
+	public void shuffleTrueTest() {
+		ShuffleHelper instance = new ShuffleHelper();
+		Bean bean = Mockito.mock(Bean.class);
+		when(bean.isShuffle()).thenReturn(true);
+		List<BeanItem> randomizeChoices = instance.randomizeChoices(bean, testList);
+		assertNotSame(testList, randomizeChoices);
+	}
+
+	@Test
+	public void shuffleFalseTest() {
+		ShuffleHelper instance = new ShuffleHelper();
+		Bean bean = Mockito.mock(Bean.class);
+		when(bean.isShuffle()).thenReturn(false);
+		List<BeanItem> randomizeChoices = instance.randomizeChoices(bean, testList);
+		assertEquals(testList, randomizeChoices);
+	}
+
+	@Test
+	public void fixedWithShuffleTrueTest() {
+		ShuffleHelper instance = new ShuffleHelper();
+		Bean bean = Mockito.mock(Bean.class);
+		when(bean.isShuffle()).thenReturn(true);
+		List<BeanItem> randomizeChoices = instance.randomizeChoices(bean, testList);
+		for (int x = 0; x < testList.size(); ++x) {
+			if (testList.get(x).isFixed()) {
+				assertEquals(testList.get(x), randomizeChoices.get(x));
+			}
+		}
+	}
+
+	@Test
+	public void fixedWithShuffleFalseTest() {
+		ShuffleHelper instance = new ShuffleHelper();
+		Bean bean = Mockito.mock(Bean.class);
+		when(bean.isShuffle()).thenReturn(false);
+		List<BeanItem> randomizeChoices = instance.randomizeChoices(bean, testList);
+		for (int x = 0; x < testList.size(); ++x) {
+			if (testList.get(x).isFixed()) {
+				assertEquals(testList.get(x), randomizeChoices.get(x));
+			}
+		}
+	}
+
+}
