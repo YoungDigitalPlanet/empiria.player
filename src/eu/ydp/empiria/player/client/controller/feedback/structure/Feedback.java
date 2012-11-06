11cf36fc2df6ccf3dd92c173be0fd86bc1e7cddd
mkaldonek <mkaldonek@ydp.com.pl>
2012-11-06 08:29:10 +0000

    feedbacks caused compile error issue 70470
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@123269 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/controller/feedback/structure/Feedback.java
+++ src/eu/ydp/empiria/player/client/controller/feedback/structure/Feedback.java
@@ -1,11 +1,8 @@
 package eu.ydp.empiria.player.client.controller.feedback.structure;
 
-import java.util.List;
 
 public interface Feedback {
 	
-	List<FeedbackAction> getActions();
 	
-	FeedbackCriterion getCriterion();
 	
 }
