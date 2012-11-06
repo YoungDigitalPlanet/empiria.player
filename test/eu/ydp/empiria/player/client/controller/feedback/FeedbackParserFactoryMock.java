852596f7194d0c013c0ac8248b8a14025189db8c
mkaldonek <mkaldonek@ydp.com.pl>
2012-11-06 07:39:38 +0000

    common parser impl issue 70475
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@123252 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/client/controller/feedback/FeedbackParserFactoryMock.java
+++ test/eu/ydp/empiria/player/client/controller/feedback/FeedbackParserFactoryMock.java
@@ -2,22 +2,13 @@ package eu.ydp.empiria.player.client.controller.feedback;
 
 import com.peterfranza.gwt.jaxb.client.parser.JAXBParser;
 
-import eu.ydp.empiria.player.client.AbstractJAXBTestBase;
 import eu.ydp.empiria.player.client.controller.feedback.structure.FeedbackBean;
+import eu.ydp.empiria.player.client.jaxb.JAXBParserImpl;
 
-public class FeedbackParserFactoryMock extends AbstractJAXBTestBase<FeedbackBean> implements FeedbackParserFactory {
+public class FeedbackParserFactoryMock implements FeedbackParserFactory {
 
 	@Override
 	public JAXBParser<FeedbackBean> create() {
-		return new JAXBParser<FeedbackBean>() {
-
-			@Override
-			public FeedbackBean parse(String xml) {
-				createUnmarshaller();
-				return createBeanFromXMLString(xml);
-			}
-			
-		};
+		return new JAXBParserImpl<FeedbackBean>(FeedbackParserFactory.class);
 	}
-
 }
