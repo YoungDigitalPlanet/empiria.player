0fc59ac35f361115ae666b497b09ee752070b8e6
mkaldonek <mkaldonek@ydp.com.pl>
2012-11-13 10:58:20 +0000

    getting feedback property name by string issue 70475
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@124044 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/controller/feedback/FeedbackPropertyName.java
+++ src/eu/ydp/empiria/player/client/controller/feedback/FeedbackPropertyName.java
@@ -1,7 +1,31 @@
 package eu.ydp.empiria.player.client.controller.feedback;
 
-
-
 public enum FeedbackPropertyName{
-	RESULT, OK, WRONG, ALL_OK, TEXT;
+	RESULT("result"), OK("ok"), WRONG("wrong"), ALL_OK("allOk"), TEXT("text"), DEFAULT("default");
+	
+	private String name;
+	
+	private FeedbackPropertyName(String name){
+		this.name = name;
+	}
+	
+	/**
+	 * Searches for FeedbackPropertyName by given string name
+	 * @param value string name for which FeedbackPropertyName will be searched
+	 * @return retruns instance of FeedbackPropertyName which string name
+	 * is equal to geven in 'value' paremeter. If it is not found then
+	 * FeedbackPropertyName.DEFAULT is returned.
+	 */
+	public static FeedbackPropertyName getPropertyName(String value){
+		FeedbackPropertyName searchedName = DEFAULT;
+		
+		for(FeedbackPropertyName feedbackPropertyName: values()){
+			if(feedbackPropertyName.name.equals(value)){
+				searchedName = feedbackPropertyName;
+				break;
+			}
+		}
+		
+		return searchedName;
+	}
 }
