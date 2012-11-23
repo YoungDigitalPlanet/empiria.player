18d4d1881236a50331075d08f882101420633f6c
pmurawski <pmurawski@ydp.com.pl>
2012-11-23 14:18:01 +0000

    sound feedback - sources for many file types issue YPUB-4623
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@125445 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/controller/feedback/structure/action/ShowUrlActionSource.java
@@ -0,0 +1,38 @@
+package eu.ydp.empiria.player.client.controller.feedback.structure.action;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name="source")
+public class ShowUrlActionSource {
+	
+	@XmlAttribute(name="src")
+	private String src;
+	
+	@XmlAttribute(name="type")
+	private String type;
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "ShowUrlActionSource [src=" + src + ", type=" + type + "]";
+	}
+}
