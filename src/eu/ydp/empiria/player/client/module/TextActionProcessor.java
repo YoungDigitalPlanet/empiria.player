0a2ab148e0fb1ab4c255d14ccbfb8484abcc229b
mkaldonek <mkaldonek@ydp.com.pl>
2012-11-23 08:54:12 +0000

    added textactionprocessor issue YPUB-4859
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@125323 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/module/TextActionProcessor.java
@@ -0,0 +1,33 @@
+package eu.ydp.empiria.player.client.module;
+
+import java.util.List;
+
+import eu.ydp.empiria.player.client.controller.feedback.processor.ActionProcessorHelper;
+import eu.ydp.empiria.player.client.controller.feedback.processor.FeedbackActionProcessor;
+import eu.ydp.empiria.player.client.controller.feedback.structure.action.ActionProcessorTarget;
+import eu.ydp.empiria.player.client.controller.feedback.structure.action.FeedbackAction;
+
+public class TextActionProcessor implements FeedbackActionProcessor, ActionProcessorTarget {
+
+	private ActionProcessorHelper helper;
+	
+	public TextActionProcessor(){
+		helper = new ActionProcessorHelper(this);
+	}
+	
+	@Override
+	public List<FeedbackAction> processActions(List<FeedbackAction> actions) {
+		return helper.processActions(actions);
+	}
+
+	@Override
+	public boolean canProcessAction(FeedbackAction action) {
+		return false;
+	}
+
+	@Override
+	public void processSingleAction(FeedbackAction action) {
+		
+	}
+
+}
