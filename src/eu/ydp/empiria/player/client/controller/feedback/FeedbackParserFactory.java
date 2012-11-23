e2e07e05173da3549da690120505e72afe8b1f00
pmurawski <pmurawski@ydp.com.pl>
2012-11-23 14:45:50 +0000

    sound feedback - sources for many file types issue YPUB-4623
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@125451 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/controller/feedback/FeedbackParserFactory.java
+++ src/eu/ydp/empiria/player/client/controller/feedback/FeedbackParserFactory.java
@@ -7,6 +7,7 @@ import eu.ydp.empiria.player.client.controller.feedback.structure.FeedbackBean;
 import eu.ydp.empiria.player.client.controller.feedback.structure.action.FeedbackActionBean;
 import eu.ydp.empiria.player.client.controller.feedback.structure.action.ShowTextAction;
 import eu.ydp.empiria.player.client.controller.feedback.structure.action.ShowUrlAction;
+import eu.ydp.empiria.player.client.controller.feedback.structure.action.ShowUrlActionSource;
 import eu.ydp.empiria.player.client.controller.feedback.structure.condition.AndConditionBean;
 import eu.ydp.empiria.player.client.controller.feedback.structure.condition.CountConditionBean;
 import eu.ydp.empiria.player.client.controller.feedback.structure.condition.FeedbackConditionBean;
@@ -15,7 +16,7 @@ import eu.ydp.empiria.player.client.controller.feedback.structure.condition.OrCo
 import eu.ydp.empiria.player.client.controller.feedback.structure.condition.PropertyConditionBean;
 
 @JAXBBindings(value=FeedbackBean.class, objects={FeedbackConditionBean.class, FeedbackActionBean.class, ShowTextAction.class, ShowUrlAction.class,
-	PropertyConditionBean.class, CountConditionBean.class, AndConditionBean.class, OrConditionBean.class, NotConditionBean.class})
+	ShowUrlActionSource.class, PropertyConditionBean.class, CountConditionBean.class, AndConditionBean.class, OrConditionBean.class, NotConditionBean.class})
 public interface FeedbackParserFactory extends JAXBParserFactory<FeedbackBean> {
 
 }
