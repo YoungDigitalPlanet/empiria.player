45e8e70b49d01bedbb3e488c2614c654b3989052
plelakowski <plelakowski@ydp.com.pl>
2012-12-03 13:02:35 +0000

    add repaint action when size of view change issue YPUB-4841
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@126272 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/util/events/multiplepair/PairConnectEventTypes.java
+++ src/eu/ydp/empiria/player/client/util/events/multiplepair/PairConnectEventTypes.java
@@ -1,5 +1,5 @@
 package eu.ydp.empiria.player.client.util.events.multiplepair;
 
 public enum PairConnectEventTypes {
-	CONNECTED, DISCONNECTED, WRONG_CONNECTION
+	CONNECTED, DISCONNECTED, WRONG_CONNECTION, REPAINT_VIEW
 }
