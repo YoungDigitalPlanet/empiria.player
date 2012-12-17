f5ebd7099cf51b329241308339d20370a6201a3b
mkaldonek <mkaldonek@ydp.com.pl>
2012-12-17 08:11:37 +0000

    removed empty test bug YPUB-4865
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@127796 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/client/controller/extensions/internal/jsonreport/AssessmentReportExtensionTest.java
+++
@@ -1,8 +0,0 @@
-package eu.ydp.empiria.player.client.controller.extensions.internal.jsonreport;
-
-import eu.ydp.empiria.player.client.AbstractEmpiriaPlayerGWTTestCase;
-
-public class AssessmentReportExtensionTest extends AbstractEmpiriaPlayerGWTTestCase {
-	
-	
-}
