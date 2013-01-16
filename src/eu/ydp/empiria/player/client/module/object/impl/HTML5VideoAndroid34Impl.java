d056a5c0cda04398b21dbb864cb9de67987520e5
plelakowski <plelakowski@ydp.com.pl>
2013-01-16 12:31:32 +0000

    restore hack for video stop before end. android 3,4 issue YPUB-4989
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@130101 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/module/object/impl/HTML5VideoAndroid34Impl.java
+++ src/eu/ydp/empiria/player/client/module/object/impl/HTML5VideoAndroid34Impl.java
@@ -1,5 +1,7 @@
 package eu.ydp.empiria.player.client.module.object.impl;
 
+import com.google.gwt.media.client.MediaBase;
+
 import eu.ydp.empiria.player.client.module.media.MediaWrapper;
 import eu.ydp.empiria.player.client.util.events.media.MediaEvent;
 import eu.ydp.empiria.player.client.util.events.media.MediaEventHandler;
@@ -8,37 +10,64 @@ import eu.ydp.empiria.player.client.util.events.scope.CurrentPageScope;
 
 public class HTML5VideoAndroid34Impl extends HTML5VideoAndroidImpl {
 	private boolean videoReady = false;
+	// zatrzymujemy 300ms przed koncem na androidach wystepuje error dla niektorych filmow
+	private static final int STOP_TIME = 300;
 
-	@Override
-	public void setEventBusSourceObject(MediaWrapper<?> object) {
-		super.setEventBusSourceObject(object);
-		eventsBus.addHandlerToSource(MediaEvent.getType(MediaEventTypes.ON_TIME_UPDATE), getEventBusSourceObject(), new MediaEventHandler() {
-			int stopTime = 300; // zatrzymujemy 300ms przed koncem niestety na
+	protected void stopVideo() {
+		MediaBase video = (MediaBase) ((MediaWrapper<?>) getEventBusSourceObject()).getMediaObject();
+		video.pause();
+		video.setCurrentTime(0);
+	}
 
-			// galaxytabie ponizej nie zalapie
-			@Override
-			public void onMediaEvent(MediaEvent event) {
-				// 300ms przed koncem filmu
-				if (videoReady) {
-					double currentTime = video.getCurrentTime() * 1000;
-					double duration = video.getDuration() * 1000;
+	protected boolean isAudioShouldBeStopped() {
+		MediaWrapper<?> mediaWrapper = getEventBusSourceObject();
+		double currentTime = mediaWrapper.getCurrentTime() * 1000;
+		double duration = mediaWrapper.getDuration() * 1000;
+		boolean stop = false;
+		if (currentTime >= 1000 && currentTime > duration - STOP_TIME) {
+			stop = true;
+		}
+		return stop;
+	}
 
-					if (currentTime >= 1000 && currentTime > duration - stopTime) {
-						video.pause();
-						video.setCurrentTime(0);
-					}
-				}
+	protected boolean isVideoReady() {
+		return videoReady;
+	}
 
-			}
-		}, new CurrentPageScope());
+	protected void setVideoReady(boolean videoReady) {
+		this.videoReady = videoReady;
+	}
 
+	protected void addHandlerToVideoOnDurationChange() {
 		// czekamy na informacje na temat dlugosci utworu
 		eventsBus.addHandlerToSource(MediaEvent.getType(MediaEventTypes.ON_DURATION_CHANGE), getEventBusSourceObject(), new MediaEventHandler() {
 
 			@Override
 			public void onMediaEvent(MediaEvent event) {
-				videoReady = true;
+				setVideoReady(true);
 			}
 		}, new CurrentPageScope());
 	}
+
+	protected void addHandlerToVideoOnTimeUpdate() {
+		eventsBus.addHandlerToSource(MediaEvent.getType(MediaEventTypes.ON_TIME_UPDATE), getEventBusSourceObject(), new MediaEventHandler() {
+			// galaxytabie ponizej nie zalapie
+			@Override
+			public void onMediaEvent(MediaEvent event) {
+				// 300ms przed koncem filmu
+				if (isVideoReady() && isAudioShouldBeStopped()) {
+					stopVideo();
+				}
+			}
+		}, new CurrentPageScope());
+	}
+
+	@Override
+	public void setEventBusSourceObject(MediaWrapper<?> object) {
+		super.setEventBusSourceObject(object);
+		addHandlerToVideoOnTimeUpdate();
+		addHandlerToVideoOnDurationChange();
+
+	}
+
 }
