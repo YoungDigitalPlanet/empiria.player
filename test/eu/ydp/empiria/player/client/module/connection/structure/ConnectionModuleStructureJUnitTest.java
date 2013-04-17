cd56401b6011d05fad6f2e21957105462c44609f
anicos <anicos@ydp.com.pl>
2013-04-17 13:56:31 +0000

    YPUB-5230 connection - zapamiętywanie stanu elementów issue YPUB-5230
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@138143 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/client/module/connection/structure/ConnectionModuleStructureJUnitTest.java
+++ test/eu/ydp/empiria/player/client/module/connection/structure/ConnectionModuleStructureJUnitTest.java
@@ -1,20 +1,22 @@
 package eu.ydp.empiria.player.client.module.connection.structure;
 
+import org.junit.Test;
 
-public class ConnectionModuleStructureJUnitTest { // extends
-// AbstractModuleStructureTestBase<ConnectionModuleStructure,
-// MatchInteractionBean, ConnectionModuleJAXBParser> {
-//
-// @Override
-// protected ConnectionModuleStructure createModuleStructure() {
-// return new ConnectionModuleStructureMock();
-// }
-//
-// @Test
-// public void shouldInitializeStructure() {
-// MatchInteractionBean bean =
-// createFromXML(ConnectionModuleStructureMock.CONNECTION_XML,
-// Mockito.mock(Optional.class));
-// assertThat(bean.getId(), is(equalTo("dummy1")));
-// }
+import eu.ydp.empiria.player.client.AbstractModuleStructureTestBase;
+
+public class ConnectionModuleStructureJUnitTest extends
+		AbstractModuleStructureTestBase<ConnectionModuleStructure, MatchInteractionBean, ConnectionModuleJAXBParser> {
+
+	@Override
+	protected ConnectionModuleStructure createModuleStructure() {
+		return new ConnectionModuleStructureMock();
+	}
+
+	@Test
+	public void shouldInitializeStructure() {
+		// MatchInteractionBean bean =
+		// createFromXML(ConnectionModuleStructureMock.CONNECTION_XML,
+		// Mockito.mock(Optional.class));
+		// assertThat(bean.getId(), is(equalTo("dummy1")));
+	}
 }
