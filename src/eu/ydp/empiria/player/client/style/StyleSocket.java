da2fe258c8eb7c4670d8d1e2094e4842c60120ae
rrybacki <rrybacki@ydp.com.pl>
2013-04-18 12:58:44 +0000

    add comment issue YPUB-5136
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@138299 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/style/StyleSocket.java
+++ src/eu/ydp/empiria/player/client/style/StyleSocket.java
@@ -7,7 +7,13 @@ import com.google.gwt.xml.client.Element;
 import eu.ydp.empiria.player.client.controller.communication.PageReference;
 
 public interface StyleSocket {
+	/**
+	 * @return styles in lower case. 
+	 */
 	public Map<String,String> getStyles( Element element );
+	/**
+	 * @return styles in unchanged letter case. 
+	 */
 	public Map<String,String> getOrgStyles( Element element );
 	public void setCurrentPages( PageReference pr );
 }
