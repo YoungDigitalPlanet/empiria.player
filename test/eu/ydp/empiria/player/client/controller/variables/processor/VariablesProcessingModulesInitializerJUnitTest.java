bf5c99aaf11478f2234075fc2a243aae13defc71
anicos <anicos@ydp.com.pl>
2013-04-23 11:04:29 +0000

    YPUB-5281 Po ponownym wejściu do lekcji, resetowana do 0 jest liczba mistakesow issue YPUB-5281
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@138623 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/client/controller/variables/processor/VariablesProcessingModulesInitializerJUnitTest.java
+++ test/eu/ydp/empiria/player/client/controller/variables/processor/VariablesProcessingModulesInitializerJUnitTest.java
@@ -1,10 +1,7 @@
 package eu.ydp.empiria.player.client.controller.variables.processor;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
 import java.util.Map;
 
 import org.junit.After;
@@ -16,13 +13,11 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.google.gwt.thirdparty.guava.common.collect.Maps;
-import com.google.inject.Inject;
 
 import eu.ydp.empiria.player.client.controller.variables.objects.outcome.Outcome;
 import eu.ydp.empiria.player.client.controller.variables.objects.response.Response;
 import eu.ydp.empiria.player.client.controller.variables.processor.module.ModulesVariablesProcessor;
 import eu.ydp.empiria.player.client.controller.variables.processor.module.grouped.GroupedAnswersManager;
-import eu.ydp.empiria.player.client.gin.scopes.page.PageScoped;
 
 @RunWith(MockitoJUnitRunner.class)
 public class VariablesProcessingModulesInitializerJUnitTest {
@@ -56,40 +51,5 @@ public class VariablesProcessingModulesInitializerJUnitTest {
 		inOrder.verify(groupedAnswersManager).initialize(responses);
 		inOrder.verify(modulesVariablesProcessor).initialize(responses);
 		inOrder.verify(mistakesInitializer).initialize(outcomes);
-	}
-
-	@Test
-	public void validateAllConstructorFieldsAnnotatedWithPageScoped() throws Exception {
-		Constructor<?>[] constructors = VariablesProcessingModulesInitializer.class.getConstructors();
-		Constructor<?> constructor = getInjectableConstructor(constructors);
-
-		Annotation[][] parametersAnnotations = constructor.getParameterAnnotations();
-		for (Annotation[] parameterAnnotations : parametersAnnotations) {
-			assertThatParameterIsAnnotatedWithPageScoped(parameterAnnotations);
-		}
-	}
-
-	private void assertThatParameterIsAnnotatedWithPageScoped(Annotation[] parameterAnnotations) {
-		boolean isPageScopedAnnotated = containsAnnotation(parameterAnnotations, PageScoped.class);
-		String errorMessage = "Parameter is not annotated with: " + PageScoped.class.getName();
-		assertEquals(errorMessage, true, isPageScopedAnnotated);
-	}
-
-	private boolean containsAnnotation(Annotation[] parameterAnnotations, Class<PageScoped> annotationClass) {
-		for (Annotation annotation : parameterAnnotations) {
-			if (annotation.annotationType().equals(annotationClass)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private Constructor<?> getInjectableConstructor(Constructor<?>[] constructors) {
-		for (Constructor<?> constructor : constructors) {
-			if (constructor.isAnnotationPresent(Inject.class)) {
-				return constructor;
-			}
-		}
-		throw new IllegalArgumentException("There is no constructor annotated with inject!");
 	}
 }
