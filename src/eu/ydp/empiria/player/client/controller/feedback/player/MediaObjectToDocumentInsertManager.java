d82e42e2c1317392e5aa389d9f8fdeb09488817f
mszalomski <mszalomski@ydp.com.pl>
2013-04-26 19:35:08 +0000

    Clean code - sweeping issue YPUB-5224
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@138979 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/controller/feedback/player/MediaObjectToDocumentInsertManager.java
+++ src/eu/ydp/empiria/player/client/controller/feedback/player/MediaObjectToDocumentInsertManager.java
@@ -1,9 +1,12 @@
 package eu.ydp.empiria.player.client.controller.feedback.player;
 
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.dom.client.Style.Visibility;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import eu.ydp.empiria.player.client.module.media.MediaWrapper;
 
@@ -14,30 +17,35 @@ import eu.ydp.empiria.player.client.module.media.MediaWrapper;
  */
 public class MediaObjectToDocumentInsertManager {
 
-	public void addMediaToDocumentAndHideControls(Object object) {
-		FlowPanel hiddenPanel = createPanel();
-		hidePanel(hiddenPanel);
-		addMediaObjectToPanel(object, hiddenPanel);
-		addPanelToRootPanel(hiddenPanel);
+	public void addMediaToDocumentAndHideControls(Object mediaWrapperObject) {
+		FlowPanel panel = createPanel();
+		hidePanel(panel);
+		addMediaObjectToPanel(mediaWrapperObject, panel);
+		addPanelToRootPanel(panel);
 	}
 
 	private void addPanelToRootPanel(FlowPanel hiddenPanel) {
 		RootPanel.get().add(hiddenPanel);
 	}
 
-	private void addMediaObjectToPanel(Object object, FlowPanel hiddenPanel) {
-		MediaWrapper<?> mediaWrapper = (MediaWrapper<?>) object;
-		hiddenPanel.add(mediaWrapper.getMediaObject());
+	private void addMediaObjectToPanel(Object mediaWrapperObject, FlowPanel panel) {
+		MediaWrapper<?> mediaWrapper = (MediaWrapper<?>) mediaWrapperObject;
+		Object mediaObject = mediaWrapper.getMediaObject();
+		Widget mediaWidget = (Widget) mediaObject;
+		panel.add(mediaWidget);
 	}
 
 	private FlowPanel createPanel() {
 		return new FlowPanel();
 	}
 
-	private void hidePanel(FlowPanel hiddenPanel) {
-		hiddenPanel.getElement().getStyle().setWidth(1d, Unit.PX);
-		hiddenPanel.getElement().getStyle().setHeight(1d, Unit.PX);
-		hiddenPanel.getElement().getStyle().setVisibility(Visibility.VISIBLE);
+	private void hidePanel(FlowPanel panel) {
+		Element element = panel.getElement();
+		Style style = element.getStyle();
+		
+		style.setWidth(1d, Unit.PX);
+		style.setHeight(1d, Unit.PX);
+		style.setVisibility(Visibility.HIDDEN);
 	}
 
 }
