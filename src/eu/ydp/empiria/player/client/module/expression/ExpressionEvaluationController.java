aca022b3b1d5adcc2da39ebdb3a225b59bb48625
aglowacki <aglowacki@ydp.com.pl>
2013-05-07 12:17:39 +0000

    Added required classes with api, and empty body that will be implemented in task YPUB-5323 issue YPUB-5323
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@139229 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/module/expression/ExpressionEvaluationController.java
@@ -0,0 +1,13 @@
+package eu.ydp.empiria.player.client.module.expression;
+
+import eu.ydp.empiria.player.client.module.expression.model.ExpressionBean;
+import eu.ydp.empiria.player.client.module.expression.model.ExpressionEvaluationResult;
+
+public class ExpressionEvaluationController {
+
+	public ExpressionEvaluationResult evaluateExpression(ExpressionBean expression){
+		//TODO: To implement as task YPUB-5322
+		return ExpressionEvaluationResult.VALUES_NOT_SET;
+	}
+	
+}
---
+++ src/eu/ydp/empiria/player/client/module/expression/model/ExpressionEvaluationResult.java
@@ -0,0 +1,7 @@
+package eu.ydp.empiria.player.client.module.expression.model;
+
+public enum ExpressionEvaluationResult {
+
+	VALUES_NOT_SET, CORRECT, WRONG
+	
+}
