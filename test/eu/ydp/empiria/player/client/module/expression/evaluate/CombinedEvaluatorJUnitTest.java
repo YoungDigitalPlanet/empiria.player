3d544f862f2845c29e4dd8198dae2efbd282261e
rrybacki <rrybacki@ydp.com.pl>
2013-05-16 14:12:28 +0000

    add result test for combined evaluator issue YPUB-5354
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@139966 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/client/module/expression/evaluate/CombinedEvaluatorJUnitTest.java
+++ test/eu/ydp/empiria/player/client/module/expression/evaluate/CombinedEvaluatorJUnitTest.java
@@ -1,17 +1,23 @@
 package eu.ydp.empiria.player.client.module.expression.evaluate;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.stub;
 import static org.mockito.Mockito.verify;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import eu.ydp.empiria.player.client.module.expression.model.ExpressionBean;
 
+@RunWith(JUnitParamsRunner.class)
 public class CombinedEvaluatorJUnitTest {
 
 	private CombinedEvaluator evaluator;
@@ -26,7 +32,7 @@ public class CombinedEvaluatorJUnitTest {
 	}
 	
 	@Test
-	public void expressionIsWrong() {
+	public void checkCommutation_expressionIsWrong() {
 		// given
 		ExpressionBean bean = mock(ExpressionBean.class);
 		stub(expressionEvaluator.evaluate(any(ExpressionBean.class))).toReturn(false);
@@ -40,7 +46,7 @@ public class CombinedEvaluatorJUnitTest {
 	}
 	
 	@Test
-	public void expressionIsCorrect() {
+	public void checkCommutation_expressionIsCorrect() {
 		// given
 		ExpressionBean bean = mock(ExpressionBean.class);
 		stub(expressionEvaluator.evaluate(any(ExpressionBean.class))).toReturn(true);
@@ -51,6 +57,35 @@ public class CombinedEvaluatorJUnitTest {
 		// then
 		verify(expressionEvaluator).evaluate(eq(bean));
 		verify(commutationEvaluator).evaluate(eq(bean));
+	}
+	
+	@Test
+	public void result_correct() {
+		// given
+		ExpressionBean bean = mock(ExpressionBean.class);
+		stub(expressionEvaluator.evaluate(any(ExpressionBean.class))).toReturn(true);
+		stub(commutationEvaluator.evaluate(any(ExpressionBean.class))).toReturn(true);
+		
+		// when
+		boolean result = evaluator.evaluate(bean);
+		
+		// then
+		assertThat(result, equalTo(true));
+	}
+	
+	@Test
+	@Parameters({"false, true", "true, false", "false, false"})
+	public void result_wrong(boolean expressionResult, boolean commutationResult) {
+		// given
+		ExpressionBean bean = mock(ExpressionBean.class);
+		stub(expressionEvaluator.evaluate(any(ExpressionBean.class))).toReturn(expressionResult);
+		stub(commutationEvaluator.evaluate(any(ExpressionBean.class))).toReturn(commutationResult);
+		
+		// when
+		boolean result = evaluator.evaluate(bean);
+		
+		// then
+		assertThat(result, equalTo(false));
 	}
 
 }
