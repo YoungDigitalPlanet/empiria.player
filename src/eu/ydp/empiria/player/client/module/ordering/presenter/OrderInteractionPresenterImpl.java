31a59495bef7ce77dc9a0a877b0e4ee78de9edc8
aglowacki <aglowacki@ydp.com.pl>
2013-05-16 09:35:49 +0000

    Replaced HashMap implementation issue YPUB-5345
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@139902 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- src/eu/ydp/empiria/player/client/module/ordering/presenter/OrderInteractionPresenterImpl.java
+++ src/eu/ydp/empiria/player/client/module/ordering/presenter/OrderInteractionPresenterImpl.java
@@ -1,9 +1,11 @@
 package eu.ydp.empiria.player.client.module.ordering.presenter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.gwt.dev.util.collect.HashMap;
+import com.google.gwt.thirdparty.guava.common.collect.Lists;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.peterfranza.gwt.jaxb.client.parser.utils.XMLContent;
@@ -18,11 +20,15 @@ import eu.ydp.empiria.player.client.module.ordering.model.OrderingItem;
 import eu.ydp.empiria.player.client.module.ordering.structure.OrderInteractionBean;
 import eu.ydp.empiria.player.client.module.ordering.structure.SimpleOrderChoiceBean;
 import eu.ydp.empiria.player.client.module.ordering.view.OrderInteractionView;
+import eu.ydp.empiria.player.client.module.selection.model.UserAnswerType;
 
 public class OrderInteractionPresenterImpl implements OrderInteractionPresenter {
 
 	@Inject
 	private OrderInteractionView interactionView;
+	
+	@Inject
+	private ItemsOrderByAnswersFinder itemsOrderByAnswersFinder;
 	
 	private ModuleSocket socket;
 	private OrderInteractionModuleModel model;
@@ -77,25 +83,107 @@ public class OrderInteractionPresenterImpl implements OrderInteractionPresenter 
 
 	@Override
 	public void setLocked(boolean locked) {
-		// TODO Auto-generated method stub
+		for(OrderingItem orderingItem : orderingItemsMap.values()){
+			orderingItem.setLocked(locked);
+		}
+		updateAllItemsStyles();
+	}
 
+	private void updateAllItemsStyles() {
+		for(OrderingItem orderingItem : orderingItemsMap.values()){
+			interactionView.setChildStyles(orderingItem);
+		}
 	}
 
 	@Override
 	public void markAnswers(MarkAnswersType type, MarkAnswersMode mode) {
-		// TODO Auto-generated method stub
+		List<OrderingItem> orderingItems = getItemsByEvaluationType(type);
+		
+		if(mode == MarkAnswersMode.MARK){
+			markItems(orderingItems, type);
+		}else if(mode == MarkAnswersMode.UNMARK){
+			unmarkItems(orderingItems);
+		}
+		
+		updateAllItemsStyles();
+	}
+	
+	private void markItems(List<OrderingItem> orderingItems, MarkAnswersType type) {
+		if(type == MarkAnswersType.CORRECT){
+			markItemsAsCorrect(orderingItems);
+		}else if (type == MarkAnswersType.WRONG){
+			markItemsAsWrong(orderingItems);
+		}
+	}
 
+	private void markItemsAsWrong(List<OrderingItem> orderingItems) {
+		for (OrderingItem orderingItem : orderingItems) {
+			orderingItem.setAnswerType(UserAnswerType.WRONG);
+		}
+	}
+
+	private void markItemsAsCorrect(List<OrderingItem> orderingItems) {
+		for (OrderingItem orderingItem : orderingItems) {
+			orderingItem.setAnswerType(UserAnswerType.CORRECT);
+		}
+	}
+
+	private void unmarkItems(List<OrderingItem> orderingItems) {
+		for (OrderingItem orderingItem : orderingItems) {
+			orderingItem.setAnswerType(UserAnswerType.NONE);
+		}
+	}
+
+	public List<OrderingItem> getItemsByEvaluationType(MarkAnswersType type){
+		List<Boolean> answerEvaluations = socket.evaluateResponse(model.getResponse());
+		List<String> currentItemsOrder = getCurrentAnswersOrder();
+		
+		List<OrderingItem> fittingOrderingItems = Lists.newArrayList();
+		
+		for(int i=0; i<answerEvaluations.size(); i++){
+			String itemId = currentItemsOrder.get(i);
+			boolean evaluationResult = answerEvaluations.get(i);
+			
+			if(isItemFittingEvaluationType(evaluationResult, type)){
+				OrderingItem orderingItem = orderingItemsMap.get(itemId);
+				fittingOrderingItems.add(orderingItem);
+			}
+			
+		}
+		
+		return fittingOrderingItems;
+	}
+
+	private boolean isItemFittingEvaluationType(boolean evaluationResult, MarkAnswersType type) {
+		if(evaluationResult && type == MarkAnswersType.CORRECT){
+			return true;
+		}else if( !evaluationResult && type == MarkAnswersType.WRONG){
+			return true;
+		}
+		
+		return false;
+	}
+
+	private List<String> getCurrentAnswersOrder() {
+		List<String> currentAnswers = model.getCurrentAnswers();
+		List<OrderingItem> items = new ArrayList<OrderingItem>(orderingItemsMap.values());
+		return itemsOrderByAnswersFinder.findCorrectItemsOrderByAnswers(currentAnswers, items);
 	}
 
 	@Override
 	public void showAnswers(ShowAnswersType mode) {
-		// TODO Auto-generated method stub
-
+		List<String> correctAnswers = model.getCorrectAnswers();
+		List<OrderingItem> items = new ArrayList<OrderingItem>(orderingItemsMap.values());
+		List<String> correctItemsOrder = itemsOrderByAnswersFinder.findCorrectItemsOrderByAnswers(correctAnswers, items);
+		
+		interactionView.setChildrenOrder(correctItemsOrder);
 	}
 
 	@Override
 	public void itemClicked(String itemId) {
 		// TODO Auto-generated method stub
-		
+		//case 1. pierwy element clickniety
+		//case 2. drugi element clickniety
+		//case 3. element odclikniety
 	}
 }
