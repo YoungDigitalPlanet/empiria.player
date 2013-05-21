078a897cc9aa79f5cfb0e7e4bc81e23a3152eee2
plelakowski <plelakowski@ydp.com.pl>
2013-05-21 06:28:57 +0000

    Response manager for item issue YPUB-5368
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@140198 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

---
+++ src/eu/ydp/empiria/player/client/controller/item/ItemResponseManager.java
@@ -0,0 +1,22 @@
+package eu.ydp.empiria.player.client.controller.item;
+
+import com.google.gwt.xml.client.Node;
+import com.google.gwt.xml.client.NodeList;
+
+import eu.ydp.empiria.player.client.controller.variables.IVariableCreator;
+import eu.ydp.empiria.player.client.controller.variables.manager.VariableManager;
+import eu.ydp.empiria.player.client.controller.variables.objects.response.Response;
+import eu.ydp.empiria.player.client.controller.variables.objects.response.ResponseNodeParser;
+
+public class ItemResponseManager extends VariableManager<Response> {
+
+	public ItemResponseManager(NodeList responseDeclarationNodes, final ResponseNodeParser responseNodeParser) {
+		super(responseDeclarationNodes, new IVariableCreator<Response>() {
+			@Override
+			public Response createVariable(Node node) {
+				return  responseNodeParser.parseResponseFromNode(node.toString());
+			}
+		});
+	}
+
+}
