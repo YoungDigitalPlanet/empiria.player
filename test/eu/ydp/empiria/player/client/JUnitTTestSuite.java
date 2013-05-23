c83bb348bf88172fa205e8c9a9d959823f0fb69b
plelakowski <plelakowski@ydp.com.pl>
2013-05-23 12:11:49 +0000

    code clean up
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@140515 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/client/JUnitTTestSuite.java
+++ test/eu/ydp/empiria/player/client/JUnitTTestSuite.java
@@ -11,27 +11,52 @@ import org.junit.runner.RunWith;
 import org.junit.runners.AllTests;
 import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.Scanner;
 import org.reflections.scanners.SubTypesScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 import org.reflections.util.FilterBuilder;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 
 import eu.ydp.gwtutil.junit.runners.ExMockRunner;
 
 @RunWith(AllTests.class)
+@SuppressWarnings("PMD")
 public class JUnitTTestSuite {
-	public static TestSuite suite() {
-		TestSuite junitSuite = new TestSuite();
 
-		for (Class<?> clazz : getAllTestsClass()) {
+	private final static Predicate<Class<?>> filterUnsupportedClazz = new Predicate<Class<?>>() {
+		@Override
+		public boolean apply(Class<?> clazz) {
 			if (clazz.isAnnotationPresent(RunWith.class)) {
 				if (clazz.getAnnotation(RunWith.class).value().isAssignableFrom(ExMockRunner.class)) {
-					continue;
+					return false;
 				}
 			}
+			return true;
+		}
+	};
+
+
+	private final static Predicate<String> onlyMethodWithTestAnnotation = new Predicate<String>() {
+		@Override
+		public boolean apply(String name) {
+			return name.contains("org.junit.Test");
+		}
+	};
+
+	private final static Predicate<String> onlyClassEndsWithTest = new Predicate<String>() {
+		@Override
+		public boolean apply(String name) {
+			return name.endsWith("Test.class");
+		}
+	};
+	public static TestSuite suite() {
+		TestSuite junitSuite = new TestSuite();
+		Iterable<Class<?>> filteredClasses = Iterables.filter(getAllTestsClass(), filterUnsupportedClazz);
+		for (Class<?> clazz :  filteredClasses) {
 			JUnit4TestAdapter adapter = new JUnit4TestAdapter(clazz);
 			junitSuite.addTest(adapter);
 		}
@@ -41,18 +66,12 @@ public class JUnitTTestSuite {
 
 	public static Set<Class<?>> getAllTestsClass() {
 
-		Reflections reflections = new Reflections(new ConfigurationBuilder().filterInputsBy(new FilterBuilder().add(new Predicate<String>() {
-			@Override
-			public boolean apply(String name) {
-				return name.endsWith("Test.class");
-			}
-		})).setUrls(ClasspathHelper.forPackage("eu.ydp.empiria"))
-				.setScanners(new SubTypesScanner(), new MethodAnnotationsScanner().filterResultsBy(new Predicate<String>() {
-					@Override
-					public boolean apply(String name) {
-						return name.contains("org.junit.Test");
-					}
-				})));
+		ConfigurationBuilder inputFilterByName = new ConfigurationBuilder().filterInputsBy(new FilterBuilder().add(onlyClassEndsWithTest));
+		Scanner methodFilterByTestAnnotation = new MethodAnnotationsScanner().filterResultsBy(onlyMethodWithTestAnnotation);
+
+		Reflections reflections = new Reflections(inputFilterByName.
+				 setUrls(ClasspathHelper.forPackage("eu.ydp.empiria"))
+				.setScanners(new SubTypesScanner(), methodFilterByTestAnnotation));
 
 		Set<Class<?>> classes = Sets.newHashSet();
 		Set<Method> methodsAnnotatedWith = reflections.getMethodsAnnotatedWith(Test.class);
