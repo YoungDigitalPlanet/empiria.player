2e644dce2a0109cf1de257b1443615aa2bda2761
plelakowski <plelakowski@ydp.com.pl>
2013-06-07 08:02:58 +0000

    temporary change that move test with ExMockRunner outside the JunitSuite
    git-svn-id: https://svn.intranet.ydp/ydp/projects/empiria/player/trunk/empiria.player@141345 3e83ccf1-c6ce-0310-9054-8fdb33cf0640

--- test/eu/ydp/empiria/player/UnsupportedTestClassForSuiteFilter.java
+++ test/eu/ydp/empiria/player/UnsupportedTestClassForSuiteFilter.java
@@ -1,12 +1,23 @@
 package eu.ydp.empiria.player;
 
+import org.junit.runner.RunWith;
+
 import com.google.common.base.Predicate;
+
+import eu.ydp.gwtutil.junit.runners.ExMockRunner;
 
 public class UnsupportedTestClassForSuiteFilter implements Predicate<Class<?>> {
 
 	@Override
 	public boolean apply(final Class<?> clazz) {
-		return !clazz.isAnnotationPresent(RunOutsideTestSuite.class);
+		return !(clazz.isAnnotationPresent(RunOutsideTestSuite.class) || isRunByExMockRunner(clazz));
+	}
+
+	public boolean isRunByExMockRunner(Class<?> clazz){
+		if(clazz.isAnnotationPresent(RunWith.class)){
+			return ExMockRunner.class.isAssignableFrom(clazz.getAnnotation(RunWith.class).value());
+		}
+		return false;
 	}
 
 }
