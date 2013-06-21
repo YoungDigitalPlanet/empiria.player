ea34e2c7a5e236602d51c37e66836f887ff71e27
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2013-06-21 12:52:05 +0200

    Mega refactor

--- src/eu/ydp/empiria/player/client/module/dragdrop/SourcelistClient.java
+++ src/eu/ydp/empiria/player/client/module/dragdrop/SourcelistClient.java
@@ -2,9 +2,9 @@ package eu.ydp.empiria.player.client.module.dragdrop;
 
 public interface SourcelistClient {
 
+	String getDragItemId();
+
 	void setDragItem(String itemId);
 
 	void removeDragItem();
-
-	String getDragItemId();
 }
