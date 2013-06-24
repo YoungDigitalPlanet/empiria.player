610a286df3f808861b17066982a5096500892ca1
Arkadiusz G³owacki <aglowacki@ydp.com.pl>
2013-06-24 16:07:41 +0200

    YPUB-5450
    Gapa Drag - widok

--- src/eu/ydp/empiria/player/client/module/draggap/view/DragGapView.java
+++ src/eu/ydp/empiria/player/client/module/draggap/view/DragGapView.java
@@ -2,10 +2,17 @@ package eu.ydp.empiria.player.client.module.draggap.view;
 
 import com.google.gwt.user.client.ui.IsWidget;
 
+import eu.ydp.empiria.player.client.module.selection.model.UserAnswerType;
+
 public interface DragGapView extends IsWidget {
+	
 	void setContent(String content);
+
 	void removeContent();
-	void markAnswers(boolean mark);
-	void showCorrectAnswers(boolean show);
+
 	void lock(boolean lock);
+
+	void setDragDisabled(boolean disabled);
+
+	void updateStyle(UserAnswerType answerType);
 }
