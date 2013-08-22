495e0243495013bb6fc6c6cc1190f5c14091fd87
plelakowski <plelakowski@ydp.com.pl>
2013-08-22 10:43:12 +0200

    EMPCP-366: move animation from player to utils

---
+++ test/org/mockito/configuration/MockitoConfiguration.java
@@ -0,0 +1,15 @@
+package org.mockito.configuration;
+
+public class MockitoConfiguration extends DefaultMockitoConfiguration {
+
+	private static boolean enabledClassCache = true;
+
+	public static void setenableClassCache(final boolean enabled){
+		enabledClassCache = enabled;
+	}
+
+	@Override
+	public boolean enableClassCache() {
+		return enabledClassCache;
+	}
+}
