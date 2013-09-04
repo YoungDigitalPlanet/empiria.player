c05bd49771257dd0e7d1b0dc9afd5950c8a038b4
plelakowski <plelakowski@ydp.com.pl>
2013-09-04 14:10:04 +0200

    YPUB-5677 : safari crash in lms when return from empiria course

--- src/eu/ydp/empiria/player/client/style/IOSModuleStyle.java
+++ src/eu/ydp/empiria/player/client/style/IOSModuleStyle.java
@@ -3,6 +3,8 @@ package eu.ydp.empiria.player.client.style;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.base.Objects;
+
 public class IOSModuleStyle extends HashMap<String, String> implements ModuleStyle {
 	private static final long serialVersionUID = 1L;
 
@@ -16,7 +18,7 @@ public class IOSModuleStyle extends HashMap<String, String> implements ModuleSty
 	@Override
 	public String get(Object key) {
 		for (Map.Entry<String, String> entry : entrySet()) {
-			if (key.equals(entry.getKey())) {
+			if (Objects.equal(key,entry.getKey())) {
 				return entry.getValue();
 			}
 		}
