b495482203d5cb9d5ed1449fa7f48efa0ce670bd
rrybacki <rrybacki@ydp.com.pl>
2013-09-30 12:09:44 +0200

    YPUB-5720 add swiffy runtime to iframe - prevent memory leaks
    (cherry picked from commit 46f8ecd9e7b660f3e9dbac7c936c18c252b35811)
    
    Conflicts:
    	src/eu/ydp/empiria/player/client/gin/module/BonusGinModule.java

--- src/eu/ydp/empiria/player/client/components/animation/swiffy/SwiffyObject.java
+++ src/eu/ydp/empiria/player/client/components/animation/swiffy/SwiffyObject.java
@@ -1,40 +1,30 @@
 package eu.ydp.empiria.player.client.components.animation.swiffy;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.IsWidget;
-import com.google.inject.Inject;
-
-import eu.ydp.empiria.player.client.gin.binding.UniqueId;
 
 public class SwiffyObject {
-
-	private final SwiffyRuntimeLoadHandler runtimeLoadHandler = new SwiffyRuntimeLoadHandler() {
-		@Override
-		public void onLoad() {
-			startAnimation();
-		}
-	};
-
+	
+	private static final String SWIFFY_RUNTIME_JS = "swiffy/runtime.js";
 	private String animationJsUrl;
 	private FlowPanel container = new FlowPanel();
-	@Inject @UniqueId private String iframeId;
-	@Inject private SwiffyRuntimeLoader runtimeLoader;
+	private String iframeId;
+	private boolean animationLoaded;
+	private boolean runtimeLoaded;
 
 	void setAnimationUrl(String animationUrl) {
 		this.animationJsUrl = animationUrl;
 	}
 
 	public void start() {
-		runtimeLoader.addRuntimeLoadHandler(runtimeLoadHandler);
-	}
-
-	private void startAnimation() {
-		appendScriptElement(iframeId, animationJsUrl);
+		iframeId = Document.get().createUniqueId();
+		appendScriptsInIframe(iframeId, animationJsUrl, GWT.getModuleBaseURL() + SWIFFY_RUNTIME_JS);
 	}
 
 	public void destroy() {
-		runtimeLoader.removeRuntimeLoadHandler(runtimeLoadHandler);
 		container.removeFromParent();
 		swiffyDestroy(iframeId);
 		container = null;
@@ -45,31 +35,57 @@ public class SwiffyObject {
 	}
 
 	private void onAnimationJsLoaded() {
+		animationLoaded = true;
+		if (runtimeLoaded){
+			startAnimation();
+		}
+	}
+
+	private void onRuntimeJsLoaded() {
+		runtimeLoaded = true;
+		if (animationLoaded){
+			startAnimation();
+		}
+	}
+
+	private void startAnimation() {
 		createSwiffyObject(iframeId, container.getElement());
 		swiffyStart(iframeId);
 	}
 
-	private native void appendScriptElement(String iframeId, String animationJsUrl)/*-{
+	private native void appendScriptsInIframe(String iframeId, String animationJsUrl, String runtimeUrl)/*-{
+		var createScript = function (doc, url, callback){
+			var scriptElement = doc.createElement('script');
+			scriptElement.setAttribute('src', url);
+			scriptElement.onload = function() {
+				callback();
+				scriptElement.onload = null;
+			}
+			return scriptElement;
+		}
+		
 		var iframe = $doc.createElement("IFRAME");
 		$doc.body.appendChild(iframe);
 		iframe.setAttribute("id", iframeId);
 		iframe.setAttribute("style","position: absolute; width: 0px; height: 0px; border: medium none; left: -1000px; top: -1000px;");
 		var ifrDoc = iframe.contentWindow.document;
 		ifrDoc.open();
-		ifrDoc.write('<html><head><script src="'+ animationJsUrl+ '" type="text/javascript"></script><\/head><body><\/body><\/html>');
+		ifrDoc.write('<html><head><\/head><body><\/body><\/html>');
 		ifrDoc.close();
+
 		var that = this;
-		var scriptElement = ifrDoc.getElementsByTagName("head")[0].childNodes[0];
-		scriptElement.onload = function() {
-			that.@eu.ydp.empiria.player.client.components.animation.swiffy.SwiffyObject::onAnimationJsLoaded()();
-			scriptElement.onload = null;
-		}
+		
+		var scriptElementRt = createScript(ifrDoc, runtimeUrl, function() { that.@eu.ydp.empiria.player.client.components.animation.swiffy.SwiffyObject::onRuntimeJsLoaded()() } );
+		var scriptElementAnim = createScript(ifrDoc, animationJsUrl, function() { that.@eu.ydp.empiria.player.client.components.animation.swiffy.SwiffyObject::onAnimationJsLoaded()() } );
+		
+		ifrDoc.getElementsByTagName("head")[0].appendChild(scriptElementRt);
+		ifrDoc.getElementsByTagName("head")[0].appendChild(scriptElementAnim);
 	}-*/;
 
 	private native void createSwiffyObject(String iframeId, Element container)/*-{
 		var iframe = $doc.getElementById(iframeId);
 		iframe.contentWindow.swiffyObject = iframe.contentWindow.getSwiffyObject();
-	    iframe.contentWindow.swiffyStage = new window.swiffy.Stage(container, iframe.contentWindow.swiffyObject);
+	    iframe.contentWindow.swiffyStage = new iframe.contentWindow.swiffy.Stage(container, iframe.contentWindow.swiffyObject);
 	}-*/;
 
 	private native void swiffyStart(String iframeId)/*-{
