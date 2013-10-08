cee4799023a578c4c44605aa1691f4189055e999
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2013-10-08 10:56:37 +0200

    PorgressAssetProvider simplified. YPUB-5801

--- test/eu/ydp/empiria/player/client/module/progressbonus/ProgressAssetProviderTest.java
+++ test/eu/ydp/empiria/player/client/module/progressbonus/ProgressAssetProviderTest.java
@@ -1,13 +1,12 @@
 package eu.ydp.empiria.player.client.module.progressbonus;
 
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.mock;
+import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Map;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -15,56 +14,70 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
-import eu.ydp.empiria.player.client.controller.extensions.internal.bonusprogress.ProgressAward;
-import eu.ydp.empiria.player.client.controller.extensions.internal.bonusprogress.ProgressBonusConfig;
+import eu.ydp.empiria.player.client.module.model.image.ShowImageDTO;
 import eu.ydp.gwtutil.client.util.RandomWrapper;
+import eu.ydp.gwtutil.client.util.geom.Size;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProgressAssetProviderTest {
 
 	@InjectMocks
 	private ProgressAssetProvider assetProvider;
-
-	@Mock
-	private ProgressBonusConfig progressBonusConfig;
 	@Mock
 	private RandomWrapper random;
 	@Mock
-	private ProgressAwardResolver awardResolver;
-
-	@Before
-	public void setUp() throws Exception {
-	}
+	private ProgressConfigResolver configResolver;
 
 	@Test
 	public void shouldResolveAwardsForGivenId() {
 		// given
-		ProgressAward award1 = mock(ProgressAward.class);
-		ProgressAward award2 = mock(ProgressAward.class);
-		ProgressAward award3 = mock(ProgressAward.class);
-		List<ProgressAward> awards = Lists.newArrayList(award1, award2, award3);
-		when(progressBonusConfig.getAwards()).thenReturn(awards);
+		final int ID = 1;
+		Map<Integer, List<ShowImageDTO>> map = Maps.newHashMap();
+		ShowImageDTO dto0_1 = createShowImageDTO("PATH0_1");
+		ShowImageDTO dto100_1 = createShowImageDTO("PATH100_1");
+		ShowImageDTO dto100_2 = createShowImageDTO("PATH100_2");
+		ShowImageDTO dto100_3 = createShowImageDTO("PATH100_3");
+		map.put(0, Lists.newArrayList(dto0_1));
+		map.put(100, Lists.newArrayList(dto100_1, dto100_2, dto100_3));
+		when(configResolver.resolveProgressConfig()).thenReturn(map);
+
 		// when
-		assetProvider.createFrom(1);
+		ProgressAsset asset = assetProvider.createFrom(ID);
 
 		// then
-		verify(awardResolver).createProgressAsset(award2);
+		assertThat(asset.getId()).isEqualTo(ID);
+		assertThat(asset.getImageForProgress(0)).isEqualTo(dto0_1);
+		assertThat(asset.getImageForProgress(100)).isEqualTo(dto100_2);
 	}
 
 	@Test
 	public void shouldResolveAwardsForRandomId() {
 		// given
-		ProgressAward award1 = mock(ProgressAward.class);
-		ProgressAward award2 = mock(ProgressAward.class);
-		ProgressAward award3 = mock(ProgressAward.class);
-		List<ProgressAward> awards = Lists.newArrayList(award1, award2, award3);
-		when(progressBonusConfig.getAwards()).thenReturn(awards);
-		when(random.nextInt(anyInt())).thenReturn(2);
+		final int ID = 1;
+		Map<Integer, List<ShowImageDTO>> map = Maps.newHashMap();
+		ShowImageDTO dto0_1 = createShowImageDTO("PATH0_1");
+		ShowImageDTO dto100_1 = createShowImageDTO("PATH100_1");
+		ShowImageDTO dto100_2 = createShowImageDTO("PATH100_2");
+		ShowImageDTO dto100_3 = createShowImageDTO("PATH100_3");
+		map.put(0, Lists.newArrayList(dto0_1));
+		map.put(100, Lists.newArrayList(dto100_1, dto100_2, dto100_3));
+		when(configResolver.resolveProgressConfig()).thenReturn(map);
+		
+		when(random.nextInt(3)).thenReturn(ID);
+
 		// when
-		assetProvider.createRandom();
+		ProgressAsset asset = assetProvider.createRandom();
 
 		// then
-		verify(awardResolver).createProgressAsset(award3);
+		verify(random).nextInt(3);
+		assertThat(asset.getId()).isEqualTo(ID);
+		assertThat(asset.getImageForProgress(0)).isEqualTo(dto0_1);
+		assertThat(asset.getImageForProgress(100)).isEqualTo(dto100_2);
+	}
+
+	private ShowImageDTO createShowImageDTO(String path) {
+		return new ShowImageDTO(path, new Size(200, 300));
 	}
 }
