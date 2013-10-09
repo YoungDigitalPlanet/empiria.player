33d3d9e38a8063b1f40a8fb1a921e750a9f0d2fd
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2013-10-09 08:53:39 +0200

    Forgotten binding. YPUB-5804

--- src/eu/ydp/empiria/player/client/gin/module/ProgressBonusGinModule.java
+++ src/eu/ydp/empiria/player/client/gin/module/ProgressBonusGinModule.java
@@ -3,6 +3,7 @@ package eu.ydp.empiria.player.client.gin.module;
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.inject.Singleton;
 
+import eu.ydp.empiria.player.client.controller.extensions.internal.bonusprogress.ProgressBonusService;
 import eu.ydp.empiria.player.client.module.progressbonus.ProgressCalculator;
 import eu.ydp.empiria.player.client.module.progressbonus.view.ProgressBonusView;
 import eu.ydp.empiria.player.client.module.progressbonus.view.ProgressBonusViewImpl;
@@ -13,5 +14,6 @@ public class ProgressBonusGinModule extends AbstractGinModule {
 	protected void configure() {
 		bind(ProgressBonusView.class).to(ProgressBonusViewImpl.class);
 		bind(ProgressCalculator.class).in(Singleton.class);
+		bind(ProgressBonusService.class).in(Singleton.class);
 	}
 }
