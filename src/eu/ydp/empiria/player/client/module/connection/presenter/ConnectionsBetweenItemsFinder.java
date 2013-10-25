65b2d589088643e74eb48dca1a0aa3bf5f4168d4
Mateusz Haligowski <mhaligowski@ydp.eu>
2013-10-25 16:12:08 +0200

    wyciągnięcie predykatu do osobnej klasy

--- src/eu/ydp/empiria/player/client/module/connection/presenter/ConnectionsBetweenItemsFinder.java
+++ src/eu/ydp/empiria/player/client/module/connection/presenter/ConnectionsBetweenItemsFinder.java
@@ -1,7 +1,6 @@
 package eu.ydp.empiria.player.client.module.connection.presenter;
 
 import com.google.common.base.Optional;
-import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.user.client.ui.IsWidget;
@@ -14,19 +13,6 @@ import eu.ydp.empiria.player.client.util.position.Point;
 import eu.ydp.empiria.player.client.util.position.PositionHelper;
 
 public class ConnectionsBetweenItemsFinder {
-	private final class PredicateImplementation implements Predicate<ConnectionItem> {
-		private final Point clickPoint;
-
-		private PredicateImplementation(Point clickPoint) {
-			this.clickPoint = clickPoint;
-		}
-
-		@Override
-		public boolean apply(ConnectionItem item) {
-			return isItemOnPosition(item, clickPoint);
-		}
-	}
-
 	@Inject
 	private PositionHelper positionHelper;
 
@@ -47,29 +33,8 @@ public class ConnectionsBetweenItemsFinder {
 	}
 
 	private Optional<ConnectionItem> findItemOnPosition(final Point clickPoint, ConnectionItems connectionItems) {
-		return Iterables.tryFind(connectionItems.getAllItems(), new PredicateImplementation(clickPoint));
+		return Iterables.tryFind(connectionItems.getAllItems(), new IsClickInConnectionItemPredicate(clickPoint));
 	}
 
-	private boolean isItemOnPosition(ConnectionItem item, Point point) {
-		final int xPos = point.getX();
-		final int yPos = point.getY();
-		return xPos >= leftBorder(item) && xPos <= rightBorder(item) && yPos >= topBorder(item) && yPos <= bottomBorder(item);
-	}
-
-	private int topBorder(ConnectionItem item) {
-		return item.getOffsetTop();
-	}
-
-	private int leftBorder(ConnectionItem item) {
-		return item.getOffsetLeft();
-	}
-
-	private int bottomBorder(ConnectionItem item) {
-		return topBorder(item) + item.getHeight();
-	}
-
-	private int rightBorder(ConnectionItem item) {
-		return leftBorder(item) + item.getWidth();
-	}
 
 }
