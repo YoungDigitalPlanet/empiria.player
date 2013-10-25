f86344f0d063a0759000664b5e003caa5ad06e30
Mateusz Haligowski <mhaligowski@ydp.eu>
2013-10-25 11:23:20 +0200

    Przejście z pętli na predykaty

--- src/eu/ydp/empiria/player/client/module/connection/presenter/ConnectionsBetweenItemsFinder.java
+++ src/eu/ydp/empiria/player/client/module/connection/presenter/ConnectionsBetweenItemsFinder.java
@@ -1,5 +1,8 @@
 package eu.ydp.empiria.player.client.module.connection.presenter;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.inject.Inject;
@@ -10,35 +13,45 @@ import eu.ydp.empiria.player.client.util.position.Point;
 import eu.ydp.empiria.player.client.util.position.PositionHelper;
 
 public class ConnectionsBetweenItemsFinder {
+	private final class PredicateImplementation implements Predicate<ConnectionItem> {
+		private final Point clickPoint;
+
+		private PredicateImplementation(Point clickPoint) {
+			this.clickPoint = clickPoint;
+		}
+
+		@Override
+		public boolean apply(ConnectionItem item) {
+			return isItemOnPosition(item, clickPoint);
+		}
+	}
 
 	@Inject
 	private PositionHelper positionHelper;
 
 	@Inject
 	private NativeEventWrapper nativeEvent;
-	
+
 	public ConnectionItem findConnectionItemForEventOnWidget(NativeEvent event, IsWidget widget, ConnectionItems connectionItems) {
 		Point clickPoint = positionHelper.getPoint(event, widget);
 
-		ConnectionItem item = findItemOnPosition(clickPoint, connectionItems);
+		Optional<ConnectionItem> item = findItemOnPosition(clickPoint, connectionItems);
 
-		if (item != null) {
+		if (item.isPresent()) {
 			nativeEvent.preventDefault(event);
+			return item.get();
+		} else {
+			return null;
 		}
-		
-		return item;
 	}
 
-	private ConnectionItem findItemOnPosition(Point clickPoint, ConnectionItems connectionItems) {
-		for (ConnectionItem item : connectionItems.getAllItems()) {
-			if (isItemOnPosition(item, clickPoint.getX(), clickPoint.getY())) {
-				return item;
-			}
-		}
-		return null;
+	private Optional<ConnectionItem> findItemOnPosition(final Point clickPoint, ConnectionItems connectionItems) {
+		return Iterables.tryFind(connectionItems.getAllItems(), new PredicateImplementation(clickPoint));
 	}
 
-	private boolean isItemOnPosition(ConnectionItem item, int xPos, int yPos) {
+	private boolean isItemOnPosition(ConnectionItem item, Point point) {
+		final int xPos = point.getX();
+		final int yPos = point.getY();
 		return xPos >= leftBorder(item) && xPos <= rightBorder(item) && yPos >= topBorder(item) && yPos <= bottomBorder(item);
 	}
 
