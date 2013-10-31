a455170e6954b7bd419f717ca22758dd501fd554
anicos <adrian.nicos@gmail.com>
2013-10-31 15:16:31 +0100

    YPUB-5877
    [Kosmikus][EC][chrome] przy przeciąganiu nie widac literek

--- src/eu/ydp/empiria/player/client/gin/factory/DragDropObjectFactory.java
+++ src/eu/ydp/empiria/player/client/gin/factory/DragDropObjectFactory.java
@@ -5,12 +5,8 @@ import com.google.inject.assistedinject.Assisted;
 
 import eu.ydp.empiria.player.client.util.dom.drag.DragDropType;
 import eu.ydp.empiria.player.client.util.dom.drag.emulate.EmulatedDragDrop;
-import eu.ydp.empiria.player.client.util.dom.drag.html5.HTML5DragDrop;
 
 public interface DragDropObjectFactory {
-	public HTML5DragDrop<Widget> getHTML5DragDrop(@Assisted("widget") Widget widget, @Assisted("type") DragDropType type,
-			@Assisted("disableAutoBehavior") boolean disableAutoBehavior);
-
 	public EmulatedDragDrop<Widget> getEmulatedDragDrop(@Assisted("widget") Widget widget, @Assisted("type") DragDropType type,
 			@Assisted("disableAutoBehavior") boolean disableAutoBehavior);
 }
--- src/eu/ydp/empiria/player/client/util/dom/drag/DragDropHelperImpl.java
+++ src/eu/ydp/empiria/player/client/util/dom/drag/DragDropHelperImpl.java
@@ -1,26 +1,19 @@
 package eu.ydp.empiria.player.client.util.dom.drag;
 
-import com.google.gwt.event.dom.client.DragEvent;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
 import eu.ydp.empiria.player.client.gin.factory.DragDropObjectFactory;
-import eu.ydp.gwtutil.client.util.UserAgentChecker;
 
 public class DragDropHelperImpl implements DragDropHelper {
 
 	@Inject
 	protected DragDropObjectFactory obFactory;
 
-	private final static boolean isNativeDragSupported =    DragEvent.isSupported()
-														&& !UserAgentChecker.isIE()
-														&& !UserAgentChecker.isMobileUserAgent();
-
 	@SuppressWarnings("unchecked")
 	@Override
 	public DraggableObject<Widget> enableDragForWidget(Widget widget) {
-		return (isNativeDragSupported) ? obFactory.getHTML5DragDrop(widget, DragDropType.DRAG, false)
-										 : obFactory.getEmulatedDragDrop(widget, DragDropType.DRAG, false);
+		return obFactory.getEmulatedDragDrop(widget, DragDropType.DRAG, false);
 	}
 
 	@Override
@@ -30,10 +23,8 @@ public class DragDropHelperImpl implements DragDropHelper {
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public <W extends Widget> DroppableObject<W> enableDropForWidget(W widget,boolean disableAutoBehavior) {
-		return (DroppableObject) ((isNativeDragSupported)
-									? obFactory.getHTML5DragDrop(widget, DragDropType.DROP, disableAutoBehavior)
-								 	: obFactory.getEmulatedDragDrop(widget, DragDropType.DROP,disableAutoBehavior));
+	public <W extends Widget> DroppableObject<W> enableDropForWidget(W widget, boolean disableAutoBehavior) {
+		return (DroppableObject) obFactory.getEmulatedDragDrop(widget, DragDropType.DROP, disableAutoBehavior);
 	}
 
 }
