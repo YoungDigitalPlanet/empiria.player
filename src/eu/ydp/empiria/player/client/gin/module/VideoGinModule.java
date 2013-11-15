967cee60bb5d9d9dfd67d2f87c47471488ba3d2b
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2013-11-15 08:33:43 +0100

    VideoPlayer and VideoPlayerImpl merged. YPUB-5966

--- src/eu/ydp/empiria/player/client/gin/module/VideoGinModule.java
+++ src/eu/ydp/empiria/player/client/gin/module/VideoGinModule.java
@@ -8,8 +8,6 @@ import eu.ydp.empiria.player.client.module.video.view.SourceElementWrapperProvid
 import eu.ydp.empiria.player.client.module.video.view.SourceElementWrapper;
 import eu.ydp.empiria.player.client.module.video.view.VideoElementWrapperProvider;
 import eu.ydp.empiria.player.client.module.video.view.VideoElementWrapper;
-import eu.ydp.empiria.player.client.module.video.view.VideoPlayer;
-import eu.ydp.empiria.player.client.module.video.view.VideoPlayerImpl;
 import eu.ydp.empiria.player.client.module.video.view.VideoPlayerNative;
 import eu.ydp.empiria.player.client.module.video.view.VideoPlayerNativeImpl;
 import eu.ydp.empiria.player.client.module.video.view.VideoView;
@@ -23,6 +21,6 @@ public class VideoGinModule extends AbstractGinModule {
 		bind(SourceElementWrapper.class).toProvider(SourceElementWrapperProvider.class);
 		bind(VideoElementWrapper.class).toProvider(VideoElementWrapperProvider.class);
 		bind(VideoPlayerNative.class).to(VideoPlayerNativeImpl.class);
-		install(new GinFactoryModuleBuilder().implement(VideoPlayer.class, VideoPlayerImpl.class).build(VideoModuleFactory.class));
+		install(new GinFactoryModuleBuilder().build(VideoModuleFactory.class));
 	}
 }
--- src/eu/ydp/empiria/player/client/module/video/view/VideoPlayer.java
+++ src/eu/ydp/empiria/player/client/module/video/view/VideoPlayer.java
@@ -1,22 +1,39 @@
 package eu.ydp.empiria.player.client.module.video.view;
 
-import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
 
-public interface VideoPlayer extends IsWidget {
+public class VideoPlayer extends Widget {
 
-//	void setSkinName(String skinName);
-//
-//	void setControls(boolean controls);
-//
-//	void setPreload(String preload);
-//
-//	void addSource(SourceBean source);
-//
-//	void addSources(Collection<SourceBean> sources);
-//
-//	void setPoster(String poster);
-//
-//	void setWidth(int width);
-//
-//	void setHeight(int height);
+	private final VideoPlayerNative nativePlayer;
+	private VideoElementWrapper videoElementWrapper;
+
+	private boolean isLoaded = false;
+
+	@Inject
+	public VideoPlayer(@Assisted VideoElementWrapper videoElementWrapper, VideoPlayerNative nativePlayer) {
+		this.nativePlayer = nativePlayer;
+		this.videoElementWrapper = videoElementWrapper;
+		setElement(Document.get().createDivElement());
+	}
+
+	@Override
+	protected void onLoad() {
+		if (!isLoaded) {
+			getElement().appendChild(videoElementWrapper.asNode());
+
+			String playerId = videoElementWrapper.getId();
+			nativePlayer.initPlayer(playerId);
+
+			isLoaded = true;
+		}
+	}
+
+	@Override
+	protected void onUnload() {
+		super.onUnload();
+		nativePlayer.unload();
+	}
 }
