fe68471e472394432bcf941146b297b6831c95a3
Mateusz Haligowski <mhaligowski@ydp.eu>
2013-12-03 10:05:48 +0100

    [YPUB-6012] Uzupełnienie testu

--- test/eu/ydp/empiria/player/client/module/video/presenter/VideoPlayerBuilderTest.java
+++ test/eu/ydp/empiria/player/client/module/video/presenter/VideoPlayerBuilderTest.java
@@ -11,8 +11,12 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
+import com.google.gwt.event.logical.shared.AttachEvent;
 import com.google.gwt.event.logical.shared.AttachEvent.Handler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.junit.GWTMockUtilities;
 
 import eu.ydp.empiria.player.client.module.video.VideoPlayerControl;
@@ -36,13 +40,16 @@ public class VideoPlayerBuilderTest {
 	private VideoPlayerFactory factory;
 	@Mock
 	private VideoBean videoBean;
-	
+
 	@Mock
 	private VideoView view;
 	@Mock
 	private EventsBus eventsBus;
-	
-	
+	@Mock
+	private HandlerRegistration handlerRegistration;
+
+	private Handler handler;
+
 	@BeforeClass
 	public static void before() {
 		GWTMockUtilities.disarm();
@@ -67,6 +74,59 @@ public class VideoPlayerBuilderTest {
 		// then
 		verify(eventsBus).addHandler(eq(PlayerEvent.getType(PlayerEventTypes.PAGE_CHANGE)), any(VideoPlayerPauseOnPageChangeHandler.class));
 		verify(videoPlayer).addAttachHandler(any(Handler.class));
+
+	}
+
+	@Test
+	public void shouldRemoveTheHandlerOnDetach() {
+		// given
+		VideoPlayer videoPlayer = mock(VideoPlayer.class);
+		when(eventsBus.addHandler(eq(PlayerEvent.getType(PlayerEventTypes.PAGE_CHANGE)), any(VideoPlayerPauseOnPageChangeHandler.class))).thenReturn(
+				handlerRegistration);
+		when(factory.create(videoBean)).thenReturn(videoPlayer);
+		doAnswer(answer()).when(videoPlayer).addAttachHandler(any(Handler.class));
+
+		AttachEvent event = mock(AttachEvent.class);
+		when(event.isAttached()).thenReturn(Boolean.FALSE);
+
+		// when
+		testObj.buildVideoPlayer();
+		handler.onAttachOrDetach(event);
+
+		// then
+		verify(handlerRegistration).removeHandler();
+	}
+
+	@Test
+	public void shouldNotRemoveTheHandlerOnDetach() {
+		// given
+		VideoPlayer videoPlayer = mock(VideoPlayer.class);
+		when(eventsBus.addHandler(eq(PlayerEvent.getType(PlayerEventTypes.PAGE_CHANGE)), any(VideoPlayerPauseOnPageChangeHandler.class))).thenReturn(
+				handlerRegistration);
+		when(factory.create(videoBean)).thenReturn(videoPlayer);
+		doAnswer(answer()).when(videoPlayer).addAttachHandler(any(Handler.class));
+
+		AttachEvent event = mock(AttachEvent.class);
+		when(event.isAttached()).thenReturn(Boolean.TRUE);
+
+		// when
+		testObj.buildVideoPlayer();
+		handler.onAttachOrDetach(event);
+
+		// then
+		verify(handlerRegistration, never()).removeHandler();
+	}
+
+	private Answer<Void> answer() {
+		return new Answer<Void>() {
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				handler = (Handler) invocation.getArguments()[0];
+				return null;
+			}
+
+		};
+
 	}
 
 	@AfterClass
