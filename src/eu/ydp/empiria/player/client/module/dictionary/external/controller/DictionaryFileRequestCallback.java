ad16397c6fb46b0896628359f2d9ca27a178b062
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2014-04-18 08:07:59 +0200

    [YPUB-6301] ExplanationListener injected without provider

--- src/eu/ydp/empiria/player/client/module/dictionary/external/controller/DictionaryFileRequestCallback.java
+++ src/eu/ydp/empiria/player/client/module/dictionary/external/controller/DictionaryFileRequestCallback.java
@@ -1,7 +1,6 @@
 package eu.ydp.empiria.player.client.module.dictionary.external.controller;
 
 import com.google.inject.Inject;
-import com.google.inject.Provider;
 import com.google.inject.assistedinject.Assisted;
 
 import eu.ydp.empiria.player.client.module.dictionary.external.model.Entry;
@@ -17,17 +16,17 @@ public class DictionaryFileRequestCallback implements FileRequestCallback {
 	private static final int ENTRIES_PER_FILE = 50;
 	private final int index;
 	private final boolean playSound;
-	private final Provider<ExplanationListener> listenerProvider;
+	private final ExplanationListener explanationListener;
 	private final Logger logger;
 	private final EntryFactory entryFactory;
 
 	@Inject
-	public DictionaryFileRequestCallback(@Assisted int index, @Assisted boolean playSound, Provider<ExplanationListener> listenerProvider,
-			IXMLParser xmlParser, EntryFactory entryFactory, Logger logger) {
+	public DictionaryFileRequestCallback(@Assisted int index, @Assisted boolean playSound, ExplanationListener explanationListener, IXMLParser xmlParser,
+			EntryFactory entryFactory, Logger logger) {
 
 		this.index = index;
 		this.playSound = playSound;
-		this.listenerProvider = listenerProvider;
+		this.explanationListener = explanationListener;
 		this.entryFactory = entryFactory;
 		this.logger = logger;
 	}
@@ -36,7 +35,7 @@ public class DictionaryFileRequestCallback implements FileRequestCallback {
 	public void onResponseReceived(FileRequest request, FileResponse response) {
 		Entry entry = createEntryFromResponse(response);
 
-		listenerProvider.get().onEntryLoaded(entry, playSound);
+		explanationListener.onEntryLoaded(entry, playSound);
 	}
 
 	private Entry createEntryFromResponse(FileResponse response) {
