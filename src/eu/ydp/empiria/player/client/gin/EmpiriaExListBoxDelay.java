ed736c6c516c1f2eeebee6a9d503329fe73d5418
Arkadiusz Kaczyński <akaczynski@ydp.pl>
2014-06-17 14:49:32 +0200

    [YPUB-6402] - customized delay

---
+++ src/eu/ydp/empiria/player/client/gin/EmpiriaExListBoxDelay.java
@@ -0,0 +1,17 @@
+package eu.ydp.empiria.player.client.gin;
+
+import eu.ydp.gwtutil.client.components.exlistbox.ExListBoxDelays;
+
+public class EmpiriaExListBoxDelay implements ExListBoxDelays {
+
+	@Override
+	public int getAutoHideDelay() {
+		return 0;
+	}
+
+	@Override
+	public int getCloseDelay() {
+		return 0;
+	}
+
+}
