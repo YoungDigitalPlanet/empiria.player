45112bf609e93559c49d7b1c400e1fd647c31b47
Arkadiusz Kaczyński <akaczynski@ydp.pl>
2014-06-18 11:26:53 +0200

    [YPUB-6402] - interface containing one delay method

--- src/eu/ydp/empiria/player/client/gin/EmpiriaExListBoxDelay.java
+++ src/eu/ydp/empiria/player/client/gin/EmpiriaExListBoxDelay.java
@@ -5,11 +5,6 @@ import eu.ydp.gwtutil.client.components.exlistbox.ExListBoxDelays;
 public class EmpiriaExListBoxDelay implements ExListBoxDelays {
 
 	@Override
-	public int getAutoHideDelay() {
-		return 0;
-	}
-
-	@Override
 	public int getCloseDelay() {
 		return 0;
 	}
