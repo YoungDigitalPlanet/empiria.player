a942eef764ff812532c60438ddd67888f7c28725
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2014-07-14 13:58:24 +0200

    [YPUB-6443] Ignoring points from ignored modules

--- src/eu/ydp/empiria/player/client/controller/variables/processor/VariableProcessingAdapter.java
+++ src/eu/ydp/empiria/player/client/controller/variables/processor/VariableProcessingAdapter.java
@@ -3,14 +3,11 @@ package eu.ydp.empiria.player.client.controller.variables.processor;
 import com.google.inject.Inject;
 import eu.ydp.empiria.player.client.controller.variables.objects.outcome.Outcome;
 import eu.ydp.empiria.player.client.controller.variables.objects.response.Response;
-import eu.ydp.empiria.player.client.controller.variables.processor.global.GlobalVariablesProcessor;
 import eu.ydp.empiria.player.client.controller.variables.processor.module.ModulesVariablesProcessor;
 import eu.ydp.empiria.player.client.controller.variables.processor.results.ModulesProcessingResults;
 import eu.ydp.empiria.player.client.controller.variables.processor.results.ProcessingResultsToOutcomeMapConverterFacade;
-import eu.ydp.empiria.player.client.controller.variables.processor.results.model.DtoModuleProcessingResult;
 import eu.ydp.empiria.player.client.controller.variables.processor.results.model.GlobalVariables;
 import eu.ydp.empiria.player.client.gin.scopes.page.PageScoped;
-import eu.ydp.empiria.player.client.module.IIgnored;
 import eu.ydp.empiria.player.client.module.IUniqueModule;
 
 import java.util.Map;
@@ -18,38 +15,24 @@ import java.util.Map;
 public class VariableProcessingAdapter {
 
 	private final ModulesVariablesProcessor modulesVariablesProcessor;
-	private final GlobalVariablesProcessor globalVariablesProcessor;
 	private final AnswerEvaluationSupplier answerEvaluationProvider;
 	private final ProcessingResultsToOutcomeMapConverterFacade mapConverterFacade;
+	private final GlobalVariablesProvider globalVariablesProvider;
 
 	@Inject
 	public VariableProcessingAdapter(@PageScoped ModulesVariablesProcessor modulesVariablesProcessor,
-			@PageScoped AnswerEvaluationSupplier answerEvaluationProvider, GlobalVariablesProcessor globalVariablesProcessor,
-			ProcessingResultsToOutcomeMapConverterFacade mapConverterFacade) {
+			@PageScoped AnswerEvaluationSupplier answerEvaluationProvider, ProcessingResultsToOutcomeMapConverterFacade mapConverterFacade,
+			GlobalVariablesProvider globalVariablesProvider) {
 		this.modulesVariablesProcessor = modulesVariablesProcessor;
-		this.globalVariablesProcessor = globalVariablesProcessor;
 		this.mapConverterFacade = mapConverterFacade;
 		this.answerEvaluationProvider = answerEvaluationProvider;
+		this.globalVariablesProvider = globalVariablesProvider;
 	}
 
 	public void processResponseVariables(Map<String, Response> responses, Map<String, Outcome> outcomes, ProcessingMode processingMode, IUniqueModule sender) {
 		ModulesProcessingResults modulesProcessingResults = modulesVariablesProcessor.processVariablesForResponses(responses, processingMode);
-		GlobalVariables globalVariables = getGlobalVariables(responses, modulesProcessingResults, sender);
+		GlobalVariables globalVariables = globalVariablesProvider.retrieveGlobalVariables(modulesProcessingResults, responses, sender);
 		mapConverterFacade.convert(outcomes, modulesProcessingResults, globalVariables);
 		answerEvaluationProvider.updateModulesProcessingResults(modulesProcessingResults);
-	}
-
-	private GlobalVariables getGlobalVariables(Map<String, Response> responses, ModulesProcessingResults modulesProcessingResults, IUniqueModule sender) {
-		if (sender instanceof IIgnored) {
-			if (((IIgnored) sender).isIgnored()) {
-				return GlobalVariables.createEmpty();
-			}
-		}
-		return getGlobalVariablesFromResponses(responses, modulesProcessingResults);
-	}
-
-	private GlobalVariables getGlobalVariablesFromResponses(Map<String, Response> responses, ModulesProcessingResults modulesProcessingResults) {
-		Map<String, DtoModuleProcessingResult> mapOfProcessingResults = modulesProcessingResults.getMapOfProcessingResults();
-		return globalVariablesProcessor.calculateGlobalVariables(mapOfProcessingResults, responses);
 	}
 }
---
+++ test/eu/ydp/empiria/player/client/controller/variables/processor/VariableProcessingAdapterTest.java
@@ -0,0 +1,81 @@
+package eu.ydp.empiria.player.client.controller.variables.processor;
+
+import com.google.common.collect.ImmutableMap;
+import eu.ydp.empiria.player.client.controller.variables.objects.outcome.Outcome;
+import eu.ydp.empiria.player.client.controller.variables.objects.response.Response;
+import eu.ydp.empiria.player.client.controller.variables.processor.module.ModulesVariablesProcessor;
+import eu.ydp.empiria.player.client.controller.variables.processor.results.ModulesProcessingResults;
+import eu.ydp.empiria.player.client.controller.variables.processor.results.ProcessingResultsToOutcomeMapConverterFacade;
+import eu.ydp.empiria.player.client.controller.variables.processor.results.model.DtoModuleProcessingResult;
+import eu.ydp.empiria.player.client.controller.variables.processor.results.model.GlobalVariables;
+import eu.ydp.empiria.player.client.module.IUniqueModule;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VariableProcessingAdapterTest {
+
+	@InjectMocks
+	private VariableProcessingAdapter testObj;
+
+	@Mock
+	private ModulesVariablesProcessor modulesVariablesProcessor;
+	@Mock
+	private AnswerEvaluationSupplier answerEvaluationProvider;
+	@Mock
+	private ProcessingResultsToOutcomeMapConverterFacade resultsToOutcomeMapConverterFacade;
+	@Mock
+	private GlobalVariablesProvider globalVariablesProvider;
+
+	@Mock
+	private ModulesProcessingResults modulesProcessingResults;
+	@Mock
+	private Map<String, Response> responses;
+	@Mock
+	private Map<String, Outcome> outcomes;
+	@Mock
+	private IUniqueModule sender;
+	
+	private final ProcessingMode processingMode = ProcessingMode.USER_INTERACT;
+
+	@Before
+	public void setUp() throws Exception {
+		when(modulesVariablesProcessor.processVariablesForResponses(responses, processingMode)).thenReturn(modulesProcessingResults);
+	}
+
+	@Test
+	public void shouldProcessResponsesAndReturnResultConvertedToOldMap() throws Exception {
+		// given
+		final String ID = "ID";
+
+		Map<String, DtoModuleProcessingResult> processingResults = ImmutableMap.of(ID, DtoModuleProcessingResult.fromDefaultVariables());
+		when(modulesProcessingResults.getMapOfProcessingResults()).thenReturn(processingResults);
+
+		GlobalVariables globalVariables = mock(GlobalVariables.class);
+		when(globalVariablesProvider.retrieveGlobalVariables(modulesProcessingResults, responses, sender)).thenReturn(globalVariables);
+
+		// when
+		testObj.processResponseVariables(responses, outcomes, processingMode, sender);
+
+		// then
+		verify(modulesVariablesProcessor).processVariablesForResponses(responses, processingMode);
+		verify(resultsToOutcomeMapConverterFacade).convert(outcomes, modulesProcessingResults, globalVariables);
+	}
+
+	@Test
+	public void shouldUpdateAnswerEvaluationProvider() throws Exception {
+		// when
+		testObj.processResponseVariables(responses, outcomes, processingMode, sender);
+
+		// then
+		verify(answerEvaluationProvider).updateModulesProcessingResults(modulesProcessingResults);
+	}
+}
