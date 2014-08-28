44644be09a57fb433e3016081cb8a64cea2d8086
aleksanderkotbury <AKotynski-Buryla@ydp.pl>
2014-08-28 13:46:35 +0200

    UnitTests

---
+++ test/eu/ydp/empiria/player/client/util/events/dom/emulate/handlers/pointer/PointerDownHandlerImplTest.java
@@ -0,0 +1,47 @@
+package eu.ydp.empiria.player.client.util.events.dom.emulate.handlers.pointer;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwtmockito.GwtMockitoTestRunner;
+
+import eu.ydp.empiria.player.client.util.events.dom.emulate.handlers.touchon.TouchOnStartHandler;
+import eu.ydp.empiria.player.client.util.events.dom.emulate.iepointer.events.PointerDownEvent;
+
+@RunWith(GwtMockitoTestRunner.class)
+public class PointerDownHandlerImplTest {
+
+	private PointerDownHandlerImpl testObj;
+
+	@Mock
+	private TouchOnStartHandler touchOnStartHandler;
+
+	@Mock
+	private PointerDownEvent pointerDownEvent;
+
+	@Mock
+	private NativeEvent nativeEvent;
+
+	@Before
+	public void setUp() {
+		testObj = new PointerDownHandlerImpl(touchOnStartHandler);
+	}
+
+	@Test
+	public void shouldCallOnStart() {
+		// given
+		when(pointerDownEvent.getNativeEvent()).thenReturn(nativeEvent);
+		when(pointerDownEvent.isTouchEvent()).thenReturn(true);
+
+		// when
+		testObj.onPointerDown(pointerDownEvent);
+
+		// then
+		verify(touchOnStartHandler).onStart(nativeEvent);
+	}
+}
