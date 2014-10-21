955a05200072f74ec9ba0da9e37dbcaa748b4527
aleksanderkotbury <AKotynski-Buryla@ydp.pl>
2014-10-21 14:51:17 +0200

    [YPUB-6614] getSoundInstance added

--- src/eu/ydp/empiria/player/client/module/simulation/soundjs/SoundJsNative.java
+++ src/eu/ydp/empiria/player/client/module/simulation/soundjs/SoundJsNative.java
@@ -19,32 +19,35 @@ public class SoundJsNative {
 	}
 
 	private native void nativesInit()/*-{
-										var instance = this;
-										$wnd.empiriaSoundJsBeginPlaying = function (src) {
-										instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::play(Ljava/lang/String;)(src);
-										}
-										$wnd.empiriaSoundJsBeginPlayingLooped = function(src) {
-											instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::playLooped(Ljava/lang/String;)(src);
-										}
-										$wnd.empiriaSoundJsInit = function (soundInstance, src) {
-										instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::preload(Lcom/google/gwt/core/client/JavaScriptObject;Ljava/lang/String;)(soundInstance, src);
-										}
-										$wnd.empiriaSoundJsStop = function (src) {
-										instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::stop(Ljava/lang/String;)(src);
-										}
-										$wnd.empiriaSoundJsPause = function (src) {
-										instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::pause(Ljava/lang/String;)(src);
-										}
-										$wnd.empiriaSoundJsResume = function (src) {
-										instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::resume(Ljava/lang/String;)(src);
-										}
-										}-*/;
+		var instance = this;
+		$wnd.empiriaSoundJsBeginPlaying = function(src) {
+			instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::play(Ljava/lang/String;)(src);
+		}
+		$wnd.empiriaSoundJsBeginPlayingLooped = function(src) {
+			instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::playLooped(Ljava/lang/String;)(src);
+		}
+		$wnd.empiriaSoundJsInit = function(soundInstance, src) {
+			instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::preload(Lcom/google/gwt/core/client/JavaScriptObject;Ljava/lang/String;)(soundInstance, src);
+		}
+		$wnd.empiriaSoundJsStop = function(src) {
+			instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::stop(Ljava/lang/String;)(src);
+		}
+		$wnd.empiriaSoundJsPause = function(src) {
+			instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::pause(Ljava/lang/String;)(src);
+		}
+		$wnd.empiriaSoundJsResume = function(src) {
+			instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::resume(Ljava/lang/String;)(src);
+		}
+		$wnd.empiriaSoundJsGetSoundInstance = function(src) {
+			return instance.@eu.ydp.empiria.player.client.module.simulation.soundjs.SoundJsNative::getSoundInstance(Ljava/lang/String;)(src);
+		}
+	}-*/;
 
 	private native void onComplete(JavaScriptObject soundInstance)/*-{
-																	if (typeof soundInstance.onComplete == 'function') {
-																	soundInstance.onComplete(soundInstance);
-																	}
-																	}-*/;
+		if (typeof soundInstance.onComplete == 'function') {
+			soundInstance.onComplete(soundInstance);
+		}
+	}-*/;
 
 	public void onComplete(String str) {
 		onComplete(soundInstances.get(str));
@@ -73,5 +76,9 @@ public class SoundJsNative {
 
 	private void resume(String src) {
 		api.resume(src);
+	}
+
+	private JavaScriptObject getSoundInstance(String src) {
+		return soundInstances.get(src);
 	}
 }
