3542c6ad4273dcf5ed32d7800688336e630f808f
aleksanderkotbury <AKotynski-Buryla@ydp.pl>
2015-05-08 11:29:46 +0200

    [YPUB-7123] test fix

--- src/test/java/eu/ydp/empiria/player/client/module/external/ExternalInteractionPathsTest.java
+++ src/test/java/eu/ydp/empiria/player/client/module/external/ExternalInteractionPathsTest.java
@@ -8,12 +8,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
 import static org.fest.assertions.api.Assertions.assertThat;
-import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,22 +24,11 @@ public class ExternalInteractionPathsTest {
 	private ExternalInteractionModuleStructure structure;
 	@Mock
 	private ExternalInteractionModuleBean bean;
-	private String src = "src";
-	private String media = "media";
-
-	private Answer<String> pathAnswer = new Answer<String>() {
-		@Override
-		public String answer(InvocationOnMock invocation) throws Throwable {
-			return media + "/" + invocation.getArguments()[0];
-		}
-	};
 
 	@Before
 	public void init() {
 		when(structure.getBean()).thenReturn(bean);
-		when(bean.getSrc()).thenReturn(src);
-
-		when(empiriaPaths.getMediaFilePath(anyString())).thenAnswer(pathAnswer);
+		when(bean.getSrc()).thenReturn("src");
 	}
 
 	@Test
@@ -50,6 +36,7 @@ public class ExternalInteractionPathsTest {
 		// given
 		String fileName = "file";
 		String expectedPath = "media/src/file";
+		when(empiriaPaths.getMediaFilePath("src/file")).thenReturn(expectedPath);
 
 		// when
 		String pathToFile = testObj.getExternalFilePath(fileName);
@@ -62,6 +49,7 @@ public class ExternalInteractionPathsTest {
 	public void shouldReturnPathToEntryPoint() {
 		// given
 		String expectedPath = "media/src/index.html";
+		when(empiriaPaths.getMediaFilePath("src/index.html")).thenReturn(expectedPath);
 
 		// when
 		String pathToEntryPoint = testObj.getExternalEntryPointPath();
