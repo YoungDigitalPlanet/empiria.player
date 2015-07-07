30349069e71740a7aab55c3a99d230f706c95f92
Jacek Jackowiak <jjackowiak@ydp.com.pl>
2015-07-07 11:53:05 +0200

    [YPUB-7212] Restoring on end callbacks

--- src/test/java/eu/ydp/empiria/player/client/module/external/common/sound/ExternalSoundInstanceCreatorTest.java
+++ src/test/java/eu/ydp/empiria/player/client/module/external/common/sound/ExternalSoundInstanceCreatorTest.java
@@ -1,5 +1,6 @@
 package eu.ydp.empiria.player.client.module.external.common.sound;
 
+import com.google.common.base.Optional;
 import com.google.gwt.user.client.ui.Widget;
 import eu.ydp.empiria.player.client.gin.factory.ExternalInteractionModuleFactory;
 import eu.ydp.empiria.player.client.media.MediaWrapperCreator;
@@ -22,41 +23,42 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class ExternalSoundInstanceCreatorTest {
 
-	@InjectMocks
-	private ExternalSoundInstanceCreator testObj;
-	@Mock
-	private ExternalPaths paths;
-	@Mock
-	private MediaWrapperCreator mediaWrapperCreator;
-	@Mock
-	private ExternalInteractionModuleFactory moduleFactory;
-	@Mock
-	private ExternalSoundInstanceCallback callback;
-	@Mock
-	private MediaWrapper<Widget> audioWrapper;
-	@Mock
-	private ExternalSoundInstance soundInstance;
-	@Captor
-	private ArgumentCaptor<CallbackReceiver<MediaWrapper<Widget>>> argumentCaptor;
+    @InjectMocks
+    private ExternalSoundInstanceCreator testObj;
+    @Mock
+    private ExternalPaths paths;
+    @Mock
+    private MediaWrapperCreator mediaWrapperCreator;
+    @Mock
+    private ExternalInteractionModuleFactory moduleFactory;
+    @Mock
+    private ExternalSoundInstanceCallback callback;
+    @Mock
+    private MediaWrapper<Widget> audioWrapper;
+    @Mock
+    private ExternalSoundInstance soundInstance;
+    @Captor
+    private ArgumentCaptor<CallbackReceiver<MediaWrapper<Widget>>> argumentCaptor;
+    private Optional<OnEndCallback> onEndCallback = Optional.absent();
 
-	@Before
-	public void init() {
-		when(paths.getExternalFilePath("ok.mp3")).thenReturn("external/ok.mp3");
-		when(moduleFactory.getExternalSoundInstance(audioWrapper)).thenReturn(soundInstance);
-	}
+    @Before
+    public void init() {
+        when(paths.getExternalFilePath("ok.mp3")).thenReturn("external/ok.mp3");
+        when(moduleFactory.getExternalSoundInstance(audioWrapper, onEndCallback)).thenReturn(soundInstance);
+    }
 
-	@Test
-	public void shouldCreateSound() {
-		// given
-		String src = "ok.mp3";
+    @Test
+    public void shouldCreateSound() {
+        // given
+        String src = "ok.mp3";
 
-		// when
-		testObj.createSound(src, callback);
-		verify(mediaWrapperCreator).createMediaWrapper(eq("external/ok.mp3"), argumentCaptor.capture());
-		CallbackReceiver<MediaWrapper<Widget>> callbackReceiver = argumentCaptor.getValue();
-		callbackReceiver.setCallbackReturnObject(audioWrapper);
+        // when
+        testObj.createSound(src, callback, onEndCallback);
+        verify(mediaWrapperCreator).createMediaWrapper(eq("external/ok.mp3"), argumentCaptor.capture());
+        CallbackReceiver<MediaWrapper<Widget>> callbackReceiver = argumentCaptor.getValue();
+        callbackReceiver.setCallbackReturnObject(audioWrapper);
 
-		// then
-		verify(callback).onSoundCreated(soundInstance, src);
-	}
+        // then
+        verify(callback).onSoundCreated(soundInstance, src);
+    }
 }
