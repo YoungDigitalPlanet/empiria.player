4170450a3c79e67ca76dbcf77c9457f6bb02c373
Marcin Różycki <mrozycki@ydp.eu>
2015-08-11 09:44:23 +0200

    Fixes

---
+++ src/main/java/eu/ydp/empiria/player/client/module/inlinechoice/InlineChoiceStyleNameConstants.java
@@ -0,0 +1,39 @@
+package eu.ydp.empiria.player.client.module.inlinechoice;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface InlineChoiceStyleNameConstants extends Constants {
+
+    @DefaultStringValue("qp-text")
+    String QP_TEXT();
+
+    @DefaultStringValue("qp-text-choice-popup")
+    String QP_TEXT_CHOICE_POPUP();
+
+    @DefaultStringValue("qp-text-choice-popup-correct")
+    String QP_TEXT_CHOICE_POPUP_CORRECT();
+
+    @DefaultStringValue("qp-text-choice-popup-disabled")
+    String QP_TEXT_CHOICE_POPUP_DISABLED();
+
+    @DefaultStringValue("qp-text-choice-popup-none")
+    String QP_TEXT_CHOICE_POPUP_NONE();
+
+    @DefaultStringValue("qp-text-choice-popup-option-empty")
+    String QP_TEXT_CHOICE_POPUP_OPTION_EMPTY();
+
+    @DefaultStringValue("qp-text-choice-popup-wrong")
+    String QP_TEXT_CHOICE_POPUP_WRONG();
+
+    @DefaultStringValue("qp-text-editor")
+    String QP_TEXT_EDITOR();
+
+    @DefaultStringValue("qp-text-editor-locked")
+    String QP_TEXT_EDITOR_LOCKED();
+
+    @DefaultStringValue("qp-text-inline")
+    String QP_TEXT_INLINE();
+
+    @DefaultStringValue("qp-math-choice-popup-option-empty")
+    String QP_MATH_CHOICE_POPUP_OPTION_EMPTY();
+}
