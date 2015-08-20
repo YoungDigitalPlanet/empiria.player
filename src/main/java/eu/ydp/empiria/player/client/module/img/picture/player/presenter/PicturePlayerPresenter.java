3e9288b5c622fab906347b89749633d27ebde719
BMachnik <machnik.barlomiej@gmail.com>
2015-08-20 09:47:05 +0200

    [YPUB-7302] Added mechanism to set alt properly

--- src/main/java/eu/ydp/empiria/player/client/module/img/picture/player/presenter/PicturePlayerPresenter.java
+++ src/main/java/eu/ydp/empiria/player/client/module/img/picture/player/presenter/PicturePlayerPresenter.java
@@ -3,6 +3,7 @@ package eu.ydp.empiria.player.client.module.img.picture.player.presenter;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import eu.ydp.empiria.player.client.controller.body.InlineBodyGeneratorSocket;
+import eu.ydp.empiria.player.client.module.img.picture.player.structure.PictureAltProvider;
 import eu.ydp.empiria.player.client.module.img.picture.player.structure.PicturePlayerBean;
 import eu.ydp.empiria.player.client.module.img.picture.player.structure.PictureTitleProvider;
 import eu.ydp.empiria.player.client.module.img.picture.player.view.PicturePlayerView;
@@ -14,24 +15,23 @@ public class PicturePlayerPresenter {
     private PicturePlayerBean bean;
     private InlineBodyGeneratorSocket inlineBodyGeneratorSocket;
     private PictureTitleProvider pictureTitleProvider;
+    private PictureAltProvider pictureAltProvider;
 
     private boolean template = false;
 
     @Inject
-    public PicturePlayerPresenter(PicturePlayerView view, PicturePlayerFullscreenController fullscreenController, PictureTitleProvider pictureTitleProvider) {
+    public PicturePlayerPresenter(PicturePlayerView view, PicturePlayerFullscreenController fullscreenController, PictureTitleProvider pictureTitleProvider, PictureAltProvider pictureAltProvider) {
         this.view = view;
         this.fullscreenController = fullscreenController;
         this.pictureTitleProvider = pictureTitleProvider;
+        this.pictureAltProvider = pictureAltProvider;
     }
 
     public void init(PicturePlayerBean bean, InlineBodyGeneratorSocket inlineBodyGeneratorSocket) {
         this.bean = bean;
         this.inlineBodyGeneratorSocket = inlineBodyGeneratorSocket;
         view.setPresenter(this);
-
-        String titleXmlString = pictureTitleProvider.getPictutreTitleString(bean);
-
-        view.setImage(titleXmlString, bean.getSrc());
+        view.setImage(pictureAltProvider.getPictureAltString(bean), bean.getSrc());
         initFullScreenMediaButton(bean);
     }
 
