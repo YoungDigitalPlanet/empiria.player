fd0601f3be106d2800b20564fa84ab487870640f
Marcin Różycki <mrozycki@ydp.eu>
2015-11-13 09:48:10 +0100

    [YPUB-7409] AudioTemplateParser refactor

--- src/main/java/eu/ydp/empiria/player/client/module/object/template/AudioTemplateParser.java
+++ src/main/java/eu/ydp/empiria/player/client/module/object/template/AudioTemplateParser.java
@@ -1,20 +1,14 @@
 package eu.ydp.empiria.player.client.module.object.template;
 
 import com.google.common.collect.Sets;
-import com.google.gwt.dom.client.Style;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.xml.client.Element;
 import com.google.gwt.xml.client.Node;
 import com.google.inject.Inject;
-import eu.ydp.empiria.player.client.media.texttrack.TextTrackKind;
 import eu.ydp.empiria.player.client.module.ModuleTagName;
 import eu.ydp.empiria.player.client.module.media.MediaControllerFactory;
 import eu.ydp.empiria.player.client.module.media.MediaWrapper;
 import eu.ydp.empiria.player.client.module.media.button.MediaController;
-import eu.ydp.empiria.player.client.module.media.button.MediaControllerWrapper;
 import eu.ydp.empiria.player.client.util.AbstractTemplateParser;
-import eu.ydp.gwtutil.client.xml.XMLUtils;
 
 import java.util.Set;
 
@@ -37,18 +31,7 @@ public class AudioTemplateParser extends AbstractTemplateParser {
             CONTROLLERS.add(ModuleTagName.MEDIA_VOLUME_BAR.tagName());
             CONTROLLERS.add(ModuleTagName.MEDIA_CURRENT_TIME.tagName());
             CONTROLLERS.add(ModuleTagName.MEDIA_TOTAL_TIME.tagName());
-            CONTROLLERS.add(ModuleTagName.MEDIA_TEXT_TRACK.tagName());
-            CONTROLLERS.add(ModuleTagName.MEDIA_SCREEN.tagName());
         }
-    }
-
-    protected Widget getMediaObject() {
-        Widget mediaObjectWidget;
-        FlowPanel videoContainer = new FlowPanel();
-        videoContainer.add(mediaWrapper.getMediaObject());
-        videoContainer.getElement().getStyle().setPosition(Style.Position.RELATIVE);
-        mediaObjectWidget = videoContainer;
-        return mediaObjectWidget;
     }
 
     @Override
@@ -72,20 +55,7 @@ public class AudioTemplateParser extends AbstractTemplateParser {
 
     @Override
     protected MediaController getMediaControllerNewInstance(String moduleName, Node node) {
-        MediaController controller;
-        if (ModuleTagName.MEDIA_TEXT_TRACK.tagName().equals(moduleName)) {
-            String kind = XMLUtils.getAttributeAsString((Element) node, "kind", TextTrackKind.SUBTITLES.name());
-            TextTrackKind trackKind = TextTrackKind.SUBTITLES;
-            try {
-                trackKind = TextTrackKind.valueOf(kind.toUpperCase()); // NOPMD
-            } catch (IllegalArgumentException exception) { // NOPMD
-            }
-            controller = factory.get(ModuleTagName.MEDIA_TEXT_TRACK, trackKind);
-        } else if (ModuleTagName.MEDIA_SCREEN.tagName().equals(moduleName)) {
-            controller = new MediaControllerWrapper(getMediaObject());
-        } else {
-            controller = factory.get(ModuleTagName.getTag(moduleName));
-        }
+        MediaController controller = factory.get(ModuleTagName.getTag(moduleName));
         if (controller != null) {
             controller.setMediaDescriptor(mediaWrapper);
         }
