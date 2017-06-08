40b0bad3f4567e2717e4858da53f5547d228ea3a
Adrian Nicos <adrian.nicos@gmail.com>
2017-06-08 11:57:33 +0200

    update licence

--- src/main/java/eu/ydp/empiria/player/public/video/AC_RunActiveContent.js
+++ src/main/java/eu/ydp/empiria/player/public/video/AC_RunActiveContent.js
@@ -1,7 +1,18 @@
-//v1.7
-// Flash Player Version Detection
-// Detect Client Browser type
-// Copyright 2005-2007 Adobe Systems Incorporated.  All rights reserved.
+/*
+ * Copyright 2017 Young Digital Planet S.A.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 var isIE  = (navigator.appVersion.indexOf("MSIE") != -1) ? true : false;
 var isWin = (navigator.appVersion.toLowerCase().indexOf("win") != -1) ? true : false;
 var isOpera = (navigator.userAgent.indexOf("Opera") != -1) ? true : false;
